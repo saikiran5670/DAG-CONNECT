5d1ffabed810c90a33efc22c39936377c425d4a7
Ram Pratap <ram.pratap@atos.net>
2021-01-06 19:43:42 +0530

    Identity services changes

---
+++ dotnet/businessservices/net.atos.daf.ct2.identityservice/Controllers/AuthenticatorController.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using DAF.IdentityLogic;
+using DAF.Entity;
+
+namespace DAF.IdentityService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticatorController : ControllerBase
+    {
+        private readonly IAutheticator _autheticator;
+        private readonly IOptions<GlobalConfiguration> _globalConfiguration;
+        public AuthenticatorController(IAutheticator autheticator,IOptions<GlobalConfiguration> globalConfiguration)
+        {
+            _autheticator=autheticator;
+            _globalConfiguration=globalConfiguration;
+        }
+
+        /// <summary>
+        ///  This action will used to obtian the access token for a user
+        /// </summary>
+        /// <param name="user"> User model that will have username and password as an input</param>
+        /// <returns>Authentication token as a JSON, error message if any</returns>
+        [HttpPost]        
+        [Route("Token")]
+        public async Task<IActionResult> GenerateToken([FromBody] User user)
+        {
+            string result = string.Empty;
+            try 
+            {
+                    if(string.IsNullOrEmpty(user.UserName))
+                    {
+                        return StatusCode(401,"invalid_grant: The username is Empty.");
+                    }
+                    else if(string.IsNullOrEmpty(user.Password))
+                    {
+                        return StatusCode(401,"invalid_grant: The password is Empty.");
+                    }
+                    else
+                    {
+                        Response response = await _autheticator.AccessToken(user);
+                        if(response!=null && response.Result==null)
+                            return Ok(result);
+                        else 
+                            return StatusCode((int) response.StatusCode,response.Result);
+
+                    }
+            }
+            catch(Exception ex)
+            {
+                    var p = ex.Message;
+                    return StatusCode(500,"Internal Server Error.");
+            }            
+        }
+        
+        [HttpGet]
+        public string Get()
+        {
+            User user =new User();
+            user.UserName="testuser5@atos.net";
+            user.Password="123456";
+            return _globalConfiguration.Value.BaseUrlKeycloak;
+            // return _autheticator.getURL(user);;
+        }
+
+    }
+}
---
+++ dotnet/businessservices/net.atos.daf.ct2.identityservice/Services/AuthenticationService.cs
@@ -0,0 +1,62 @@
+using System;
+using Grpc.Core;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using DAF.IdentityService.Protos;
+using DAF.IdentityLogic;
+using DAF.Entity;
+
+namespace DAF.IdentityService
+{
+    public class AuthenticationService : AuthService.AuthServiceBase
+    {
+        private readonly IAutheticator _autheticator;
+        public AuthenticationService (IAutheticator autheticator)
+        {
+            _autheticator=autheticator;
+            // _logger = logger;
+        }   
+
+        public async override Task<TokenResponse> GenerateToken(UserRequest request, ServerCallContext context)
+        {
+             var grpcresponse = new TokenResponse();
+
+             var user = new User
+            {
+                UserName = request.Username,
+                Password= request.Password
+            };
+            try 
+            {
+                var response = await _autheticator.AccessToken(user);                
+                Token token = JsonConvert.DeserializeObject<Token>(Convert.ToString(response.Result));
+                if(!string.IsNullOrEmpty(token.Error))
+                {
+                    grpcresponse.Error=token.Error;
+                }
+                grpcresponse.Accesstoken=token.access_token;
+                grpcresponse.Expiresin=token.expires_in;
+                grpcresponse.Refreshexpiresin=token.refresh_expires_in;
+                grpcresponse.Refreshtoken=token.refresh_token;
+                grpcresponse.Tokentype=token.token_type;
+                grpcresponse.Notbeforepolicy=token.not_before_policy;
+                grpcresponse.Sessionstate=token.session_state;
+                grpcresponse.Scope=token.scope;
+            
+            }
+            catch(Exception ex)
+            {
+                if(!string.IsNullOrEmpty(grpcresponse.Error))
+                {
+                    grpcresponse.Error=grpcresponse.Error +" "+ex.StackTrace;
+                }
+                else
+                {
+                    grpcresponse.Error = ex.StackTrace;
+                }
+            }
+            return grpcresponse;
+        }
+
+    }
+}
