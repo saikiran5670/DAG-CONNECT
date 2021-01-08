cb8401b77c72f4a779828d8c30fa60460ab27f98
Gaurav Gadewar <gaurav.ratnakargadewar@atos.net>
2021-01-08 17:48:41 +0530

    Added TCU provisioning data service

---
+++ dotnet/dataservices/net.atos.daf.ct2.customerdataservice.tcuprovisioning/TCUProvisioning/TCUProvisioning/TCUDataSend/TCURegistrationEvents.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCUSend
+{
+    class TCURegistrationEvents
+    {
+        private List<TCURegistrationEvent> tcuRegistrationEvent;
+
+        public TCURegistrationEvents(List<TCURegistrationEvent> tcuRegistrationEvent)
+        {
+            this.tcuRegistrationEvent = tcuRegistrationEvent;
+        }
+
+        public List<TCURegistrationEvent> TcuRegistrationEvent { get => tcuRegistrationEvent; set => tcuRegistrationEvent = value; }
+    }
+}
