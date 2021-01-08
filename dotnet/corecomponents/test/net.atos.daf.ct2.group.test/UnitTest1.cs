de1be42e6d62918def8725bd16f1f1461fa532ec
Viranjay Singh <viranjay.kumar@gmail.com>
2021-01-08 12:02:05 +0530

    test project for group added

--- dotnet/corecomponents/test/net.atos.daf.ct2.group.test/UnitTest1.cs
+++ dotnet/corecomponents/test/net.atos.daf.ct2.group.test/UnitTest1.cs
@@ -1,13 +1,28 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using net.atos.daf.ct2.group;
 namespace net.atos.daf.ct2.group.test
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+       [TestMethod]
+        public void CreateGroup()
         {
+            // Group group = new Group();
+            // string connectionString = "Server = 127.0.0.1; Port = 5432; Database = DAFCT; User Id = postgres; Password = Admin@1978; CommandTimeout = 90; ";
+            // dataAccess = new PgSQLDataAccess(connectionString);
+            // GroupRepository repo = new GroupRepository();
+            // group.ObjType = ObjectType.VehicleGroup;
+            // group.GroupType = GroupType.Group;
+            // group.Argument = "Truck";
+            // group.FunctionEnum = FunctionEnum.None;
+            // group.OrganizationId = 1;
+            // group.RefId = null;
+            // group.Name = "Account Group3";
+            // group.Description = "Account Group Test3 ";
+            // var result = repo.Create(group).Result;
+            // Assert.IsTrue(result != null && result.Id > 0);
         }
+        
     }
 }
