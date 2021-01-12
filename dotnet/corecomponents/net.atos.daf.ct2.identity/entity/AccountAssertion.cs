1fcd41c51b9d3dd00057d8475f518f910414e9d1
Shailesh Rajput <shailesh-nathusing.rajput@atos.net>
2021-01-12 11:42:30 +0530

    commit

---
+++ dotnet/corecomponents/net.atos.daf.ct2.identity/entity/AccountAssertion.cs
@@ -0,0 +1,14 @@
+using System.Net.Http;
+
+namespace net.atos.daf.ct2.identity.entity
+{
+   public class AccountAssertion
+    {
+        public long Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string SessionState { get; set; }
+        public string AccountId { get; set; }
+        public string CreatedAt { get; set; }
+    }
+}
