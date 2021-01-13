d82d29bdfe0626944088d271875ffcccf7876f94
Saurabh.Komawar@sthlvblvd.onmicrosoft.com <saurabh.komawar@atos.net>
2021-01-13 15:09:40 +0530

    vehicle service changes

--- dotnet/corecomponents/net.atos.daf.ct2.vehicle/entity/VehicleProperty.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.vehicle/entity/VehicleProperty.cs
@@ -10,7 +10,7 @@ namespace net.atos.daf.ct2.vehicle.entity
         public string Classification_Make { get; set; }
         public string Classification_Series { get; set; }
         public string Classification_Model { get; set; }
-        public string Classification_Type { get; set; }
+        public VehicleType Classification_Type { get; set; }
         public int Tank_Nr { get; set; }
         public int Tank_Volume { get; set; }
         public string Chasis_Id { get; set; }
@@ -20,14 +20,14 @@ namespace net.atos.daf.ct2.vehicle.entity
         public string Engine_ID { get; set; }
         public EngineType Engine_Type { get; set; }
         public int Engine_Power { get; set; }
-        public string Engine_Coolant { get; set; }
-        public string Engine_EmissionLevel { get; set; }
+        public EngineCoolantType Engine_Coolant { get; set; }
+        public EngineEmissionLevelType Engine_EmissionLevel { get; set; }
         public string GearBox_Id { get; set; }
         public GearBoxType GearBox_Type { get; set; }
         public string DriverLine_AxleConfiguration { get; set; }
         public decimal DriverLine_Wheelbase { get; set; }
         public string DriverLine_Tire_Size { get; set; }
-        public string DriverLine_FrontAxle_Position { get; set; }
+        public int DriverLine_FrontAxle_Position { get; set; }
         public int DriverLine_FrontAxle_Load { get; set; }
         public int DriverLine_RearAxle_Position { get; set; }
         public int DriverLine_RearAxle_Load { get; set; }
