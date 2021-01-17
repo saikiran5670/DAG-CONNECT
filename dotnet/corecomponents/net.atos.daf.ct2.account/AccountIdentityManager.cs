702d46ca0e7e4b08280cca187ed05f583f314bdc
Shailesh Rajput <shailesh-nathusing.rajput@atos.net>
2021-01-17 15:22:48 +0530

    Added authentication service changes

--- dotnet/corecomponents/net.atos.daf.ct2.account/AccountIdentityManager.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.account/AccountIdentityManager.cs
@@ -2,49 +2,52 @@ using System;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using net.atos.daf.ct2.identity;
-using net.atos.daf.ct2.identity.entity;
+using IdentityComponent= net.atos.daf.ct2.identity;
+using IdentityEntity=net.atos.daf.ct2.identity.entity;
 using net.atos.daf.ct2.account.entity;
+using net.atos.daf.ct2.accountpreference;
+using net.atos.daf.ct2.account.ENUM;
 
 namespace net.atos.daf.ct2.account
 {
     public class AccountIdentityManager : IAccountIdentityManager
     {
-        ITokenManager tokenManager;
-        IAccountAuthenticator autheticator;
+        IdentityComponent.ITokenManager tokenManager;
+        IdentityComponent.IAccountAuthenticator autheticator;
         IAccountManager accountManager;
-        IAccountPreference accountPreference;        
-
-        public AccountIdentityManager(ITokenManager _tokenManager, IAccountAuthenticator _autheticator,IAccountPreference _accountPreference)
+        IPreferenceManager preferenceManager;        
+        
+        public AccountIdentityManager(IdentityComponent.ITokenManager _tokenManager, IdentityComponent.IAccountAuthenticator _autheticator,IPreferenceManager _preferenceManager,IAccountManager _accountManager)
         {
             autheticator = _autheticator;
             tokenManager = _tokenManager;
-            accountPreference=_accountPreference;
+            preferenceManager = _preferenceManager;
+            accountManager=_accountManager;
         }
-        public async Task<AccountIdentity> Login(Identity user)
+        public async Task<AccountIdentity> Login(IdentityEntity.Identity user)
         {
             AccountIdentity accIdentity = new AccountIdentity();
 
-            Response idpResponse = await autheticator.AccessToken(user);
+            IdentityEntity.Response idpResponse = await autheticator.AccessToken(user);
             if(idpResponse.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                IDPToken token = JsonConvert.DeserializeObject<IDPToken>(Convert.ToString(idpResponse.Result));
-                AccountIDPClaim accIDPclaims= tokenManager.DecodeToken(token.access_token);
+                IdentityEntity.IDPToken token = JsonConvert.DeserializeObject<IdentityEntity.IDPToken>(Convert.ToString(idpResponse.Result));
+                IdentityEntity.AccountIDPClaim accIDPclaims= tokenManager.DecodeToken(token.access_token);
 
-                AccountToken accToken = tokenManager.CreateToken(accIDPclaims);
+                IdentityEntity.AccountToken accToken = tokenManager.CreateToken(accIDPclaims);
                 accIdentity.AccountToken=accToken;
-                int accountId=GetAccountByEmail(user.UserName);
+                int accountId= GetAccountByEmail(user.UserName);
                 if(accountId>0)
                 {
                     AccountPreferenceFilter filter=new AccountPreferenceFilter();
-                    filter.Ref_id=PreferenceType.Ref_id;
+                    filter.Ref_Id=accountId;
                     filter.PreferenceType=PreferenceType.Account;
-                    // IEnumerable<AccountPreference> preferences=accountPreference.Get(filter);
-                    // foreach(var pref in preferences) 
-                    // {
-                    //     accIdentity.AccountPreference=pref;
-                    //     break; //get only first preference
-                    // }
+                    IEnumerable<AccountPreference> preferences = preferenceManager.Get(filter).Result;
+                    foreach(var pref in preferences) 
+                    {
+                        accIdentity.AccountPreference=pref;
+                        break; //get only first preference
+                    }
                 }
             }
             return await Task.FromResult(accIdentity);
@@ -59,13 +62,13 @@ namespace net.atos.daf.ct2.account
         {
             int accountid=0;
             AccountFilter filter = new AccountFilter();     
-            filter.Name =email;
-        //    filter.AccountType = AccountType.None;            
+            filter.Email =email;
+            filter.AccountType = AccountType.PortalAccount;            
             IEnumerable<Account> result = accountManager.Get(filter).Result;
             foreach(var account in result) 
             {
                 accountid=account.Id;
-                //get only first account id
+                break;//get only first account id
             }
             return accountid;
         }
