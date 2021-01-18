1e51ef6d976650d54baff46d35c4498161c171b8
Ram Pratap <ram.pratap@atos.net>
2021-01-18 16:48:49 +0530

    Customer interface changes

--- dotnet/dataservices/net.atos.daf.ct2.customerdataservice/Startup.cs
+++ dotnet/dataservices/net.atos.daf.ct2.customerdataservice/Startup.cs
@@ -30,7 +30,7 @@ namespace net.atos.daf.ct2.customerdataservice
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            var connectionString="Server=dafct-dev0-dta-cdp-pgsql.postgres.database.azure.com;Database=dafconnectmasterdatabase;Port=5432;User Id=pgadmin@dafct-dev0-dta-cdp-pgsql;Password=W%PQ1AI}Y\\97;Ssl Mode=Require;";
+            var connectionString="Server=dafct-dev0-dta-cdp-pgsql.postgres.database.azure.com;Database=dafconnectmasterdatabase;Port=5432;User Id=pgadmin@dafct-dev0-dta-cdp-pgsql;Password=W%PQ1AI}Y97;Ssl Mode=Require;";
             IDataAccess dataAccess = new PgSQLDataAccess(connectionString);           
             services.AddSingleton(dataAccess); 
             services.AddTransient<IAuditTraillib,AuditTraillib>(); 
