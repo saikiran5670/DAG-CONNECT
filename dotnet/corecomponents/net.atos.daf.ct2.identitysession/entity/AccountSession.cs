f720e06f2e6e8d9504e018087e19c762a0487fbb
Jayshree Ahire <jayshree.ahire@atos.net>
2021-01-18 21:14:04 +0530

    Created identity session management template

---
+++ dotnet/corecomponents/net.atos.daf.ct2.identitysession/entity/AccountSession.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+namespace net.atos.daf.ct2.identitysession.entity
+{
+    public class AccountSession
+    {
+    }
+}
---
+++ dotnet/corecomponents/net.atos.daf.ct2.identitysession/repository/IAccountTokenRepository.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using net.atos.daf.ct2.identitysession.entity;
+
+namespace net.atos.daf.ct2.identitysession.repository
+{
+   public interface IAccountTokenRepository
+    {
+    }
+}
