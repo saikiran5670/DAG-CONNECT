9fcd6971d555bbf6e3f66a75ec169194007b43ad
Innani, Arun <arun.innani@atos.net>
2021-01-23 10:47:52 +0000

    Updated Startup.cs

--- dotnet/dataservices/net.atos.daf.ct2.customerdataservice/Startup.cs
+++ dotnet/dataservices/net.atos.daf.ct2.customerdataservice/Startup.cs
@@ -100,14 +100,14 @@ namespace net.atos.daf.ct2.customerdataservice
 
             app.UseSwagger(c =>
             {
-                c.RouteTemplate = "customer-data/swagger/v1/swagger.json";
+                c.RouteTemplate = "customer-data/swagger/{documentName}/swagger.json";
             });
 
 
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/customer-data/swagger/v1/swagger.json", "Customer Data Service V1");
-                //c.RoutePrefix = "customer-data/swagger";
+                c.RoutePrefix = "customer-data";
             });
         }
     }
