314905fff9420a29da6338766ab331c754a4f042
Chennareddy <sunitha.2.chennareddy@atos.net>
2021-01-23 19:58:22 +0530

    Included Postgre functionality

---
+++ java/commoncomponent/daf.common/src/main/java/net/atos/daf/common/ct2/util/DAFConstants.java
@@ -0,0 +1,8 @@
+package net.atos.daf.common.ct2.util;
+
+public class DAFConstants {
+
+	//Postgre Sql
+	public static final String POSTGRE_SQL_MAXIMUM_CONNECTION ="maximum_connection";
+	public static final String POSTGRE_SQL_SSL_MODE = "&sslmode=require";
+}
--- java/commoncomponent/daf.common/src/test/java/net/atos/daf/common/ct2/utc/TimeFormaterTest.java
+++ java/commoncomponent/daf.common/src/test/java/net/atos/daf/common/ct2/utc/TimeFormaterTest.java
@@ -1,56 +1,64 @@
 package net.atos.daf.common.ct2.utc;
 
-import java.util.Arrays;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import junit.framework.Assert;
 
 public class TimeFormaterTest {
-	
-	TimeFormatter obj = new TimeFormatter();
-	
+
 	@Test
-	public void testCurrentUtilTime() throws Exception {
-		
-		System.out.println("hello");
-		System.out.println(obj.getCurrentUTCTime());
-		System.out.println(obj.getUTCStringFromEpochMilli(1610345862505L));
-		//System.out.println(" ts :: "+ obj.convertDateStrToTimestamp("2020-08-06T16:42:19.000Z", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
-		
-		//Testing of Trip fields
-		System.out.println("start time :: "+obj.getUTCStringFromEpochMilli(1596775285000L));
-		System.out.println("end time :: "+obj.getUTCStringFromEpochMilli(  1596775380000L));
-		System.out.println(" tim ediff  time :: "+obj.subMilliSecFromUTCTime(1596775285000L,1596775380000L ));
-		System.out.println(" Adding time :: "+obj.addMilliSecToUTCTime(1596775285000L,95000L ));
-		
-		Long addMilli = obj.addMilliSecToUTCTime(1610345862505L, 60000);
-		System.out.println(addMilli);
-		System.out.println(obj.getUTCStringFromEpochMilli(addMilli));
-		
-		Long subMilli = obj.subMilliSecFromUTCTime(1610345862505L, 60000L);
-		System.out.println(subMilli);
-		System.out.println(obj.getUTCStringFromEpochMilli(subMilli));
-		
-		
-		
-		System.out.println("fue :: "+(38421 * (8.5 /10000000)));
-		
-		
-		//Assert.assertEquals(ZonedDateTime.now().toInstant().toEpochMilli(), obj.getCurrentUTCTime());
-		Assert.assertEquals(Long.valueOf(1596732139000L), obj.convertUTCToEpochMilli("2020-08-06T16:42:19.000Z", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
-	//long aa =obj.testutc();
-	//	System.out.println(aa);
+	public void testGetCurrentUtcTime() throws Exception {
+		assertTrue("Current UTC time greater than previous time :: ",
+				TimeFormatter.getCurrentUTCTime() > 1611055667622L);
 	}
-	
-	
-	/*public static long testutc()
-	{
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-		final String dateTime= "2020-08-06T16:42:19.000Z";
-		
-		System.out.println("time =" + ZonedDateTime.parse(dateTime, formatter.withZone(ZoneId.of("UTC"))).toInstant().toEpochMilli());
-		return ZonedDateTime.parse(dateTime, formatter.withZone(ZoneId.of("UTC"))).toInstant().toEpochMilli();
-		
-	}*/
+
+	@Test
+	public void testSubMilliSecFromUTCTime() throws Exception {
+		Assert.assertEquals(1611064206919L, TimeFormatter.subMilliSecFromUTCTime(1611064208919L, 2000));
+	}
+
+	@Test
+	public void testSubMSFromUTCTime() throws Exception {
+		Assert.assertEquals(1611064206919L, TimeFormatter.subMilliSecFromUTCTime(1611064208919L, 2000L));
+	}
+
+	@Test
+	public void testSubPastUtcTmFrmCurrentUtcTm() throws Exception {
+		Assert.assertEquals(2000L, TimeFormatter.subPastUtcTmFrmCurrentUtcTm(1611064206919L, 1611064208919L));
+	}
+
+	@Test
+	public void testSubSecondsFromUTCTime() throws Exception {
+		Assert.assertEquals(1611064206919L, TimeFormatter.subSecondsFromUTCTime(1611064208919L, 2L));
+	}
+
+	@Test
+	public void testAddMilliSecToUTCTime() throws Exception {
+		Assert.assertEquals(1611150608919L, TimeFormatter.addMilliSecToUTCTime(1611064208919L, 86400000L));
+	}
+
+	@Test
+	public void addMSToUTCTime() throws Exception {
+		Assert.assertEquals(1611064209919L, TimeFormatter.addMilliSecToUTCTime(1611064208919L, 1000L));
+	}
+
+	@Test
+	public void testAddSecondsToUTCTime() throws Exception {
+		Assert.assertEquals(1611064210919L, TimeFormatter.addSecondsToUTCTime(1611064208919L, 2L));
+	}
+
+	@Test
+	public void testConvertUTCToEpochMilli() throws Exception {
+		Assert.assertEquals(1611064208919L,
+				TimeFormatter.convertUTCToEpochMilli("2021-01-19T13:50:08.919Z", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+	}
+
+	@Test
+	public void testGetTimeDiffInMilli() throws Exception {
+		Assert.assertEquals(2000L, TimeFormatter.getTimeDiffInMilli("2021-01-19T13:50:06.919Z",
+				"2021-01-19T13:50:08.919Z", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"));
+	}
+
 }
