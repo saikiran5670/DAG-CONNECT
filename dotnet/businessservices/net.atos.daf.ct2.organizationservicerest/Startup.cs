62db125054fdc727f988615b91e37f430e453e2c
Ram Pratap <ram.pratap@atos.net>
2021-01-23 12:04:59 +0530

    Organization rest service changes

--- dotnet/businessservices/net.atos.daf.ct2.organizationservicerest/Startup.cs
+++ dotnet/businessservices/net.atos.daf.ct2.organizationservicerest/Startup.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using net.atos.daf.ct2.data;
+using net.atos.daf.ct2.audit;
+using net.atos.daf.ct2.organization.repository;
+using Microsoft.Extensions.Configuration;
+using net.atos.daf.ct2.organization;
+using net.atos.daf.ct2.audit.repository;  
+using net.atos.daf.ct2.accountpreference;
+using net.atos.daf.ct2.vehicle;
+using  net.atos.daf.ct2.vehicle.repository;
+using Microsoft.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using AccountComponent = net.atos.daf.ct2.account;
+using Identity = net.atos.daf.ct2.identity;
+using AccountPreference = net.atos.daf.ct2.accountpreference;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
+
 
 namespace net.atos.daf.ct2.organizationservicerest
 {
@@ -20,12 +33,43 @@ namespace net.atos.daf.ct2.organizationservicerest
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+       public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+             services.AddControllers();
+
+            // var connectionString = Configuration.GetConnectionString("ConnectionString");
+            // IDataAccess dataAccess = new PgSQLDataAccess(connectionString);
+            //services.AddControllers();
+            var connectionString="Server=dafct-dev0-dta-cdp-pgsql.postgres.database.azure.com;Database=dafconnectmasterdatabase;Port=5432;User Id=pgadmin@dafct-dev0-dta-cdp-pgsql;Password=W%PQ1AI}Y97;Ssl Mode=Require;";
+           IDataAccess dataAccess = new PgSQLDataAccess(connectionString);           
+           services.Configure<Identity.IdentityJsonConfiguration>(Configuration.GetSection("IdentityConfiguration")); 
+           
+            services.AddSingleton(dataAccess); 
+            services.AddTransient<IAuditTraillib,AuditTraillib>(); 
+            services.AddTransient<IAuditLogRepository, AuditLogRepository>(); 
+            services.AddTransient<IOrganizationManager,OrganizationManager>();
+            services.AddTransient<IOrganizationRepository, OrganizationRepository>();
+            services.AddTransient<IPreferenceManager,PreferenceManager>();
+            services.AddTransient<IAccountPreferenceRepository, AccountPreferenceRepository>();
+            services.AddTransient<IVehicleRepository, VehicleRepository>();
+            services.AddTransient<IVehicleManager,VehicleManager>();
+            //services.AddTransient<IVehicleManagerRepository, VehicleManagerRepository>();
+
+             services.AddTransient<Identity.IAccountManager,Identity.AccountManager>();
+            services.AddTransient<Identity.ITokenManager,Identity.TokenManager>();
+            services.AddTransient<Identity.IAccountAuthenticator,Identity.AccountAuthenticator>();            
+            services.AddTransient<AccountComponent.IAccountIdentityManager,AccountComponent.AccountIdentityManager>();            
+            services.AddTransient<AccountPreference.IPreferenceManager,AccountPreference.PreferenceManager>();
+            services.AddTransient<AccountPreference.IAccountPreferenceRepository, AccountPreference.AccountPreferenceRepository>();
+            services.AddTransient<AccountComponent.IAccountRepository,AccountComponent.AccountRepository>();
+            services.AddTransient<AccountComponent.IAccountManager,AccountComponent.AccountManager>();   
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            
+            services.AddSwaggerGen(c =>
+            {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Customer Data Service", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +89,12 @@ namespace net.atos.daf.ct2.organizationservicerest
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+               c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer Data Service V1");
             });
         }
     }
