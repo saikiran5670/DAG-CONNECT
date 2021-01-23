8f3680a0fe9e0df2dae811bd9897634a11e1f8e2
tushar.gawande@sthlvblvd.onmicrosoft.com <tushar.gawande@atos.net>
2021-01-23 16:12:43 +0530

    Con string

--- dotnet/commonservices/net.atos.daf.ct2.auditservicerest/Startup.cs
+++ dotnet/commonservices/net.atos.daf.ct2.auditservicerest/Startup.cs
@@ -33,8 +33,8 @@ namespace net.atos.daf.ct2.auditservicerest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            // var connectionString = Configuration.GetConnectionString("ConnectionString");
-            var connectionString= "Server=dafct-dev0-dta-cdp-pgsql.postgres.database.azure.com;Database=dafconnectmasterdatabase;Port=5432;User Id=pgadmin@dafct-dev0-dta-cdp-pgsql;Password=W%PQ1AI}Y97;Ssl Mode=Require;";
+            var connectionString = Configuration.GetConnectionString("ConnectionString");
+            // var connectionString= "Server=dafct-dev0-dta-cdp-pgsql.postgres.database.azure.com;Database=dafconnectmasterdatabase;Port=5432;User Id=pgadmin@dafct-dev0-dta-cdp-pgsql;Password=W%PQ1AI}Y97;Ssl Mode=Require;";
             IDataAccess dataAccess = new PgSQLDataAccess(connectionString);
             services.AddSingleton(dataAccess); 
             services.AddTransient<IAuditTraillib,AuditTraillib>();
