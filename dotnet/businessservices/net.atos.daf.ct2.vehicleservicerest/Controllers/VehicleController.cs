be7aa59fbb96963c7d2e7efebfed8e6d43806520
Viranjay Singh <viranjay.kumar@gmail.com>
2021-01-23 17:44:09 +0530

    Vehicle Service Build Issues Fix.

--- dotnet/businessservices/net.atos.daf.ct2.vehicleservicerest/Controllers/VehicleController.cs
+++ dotnet/businessservices/net.atos.daf.ct2.vehicleservicerest/Controllers/VehicleController.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace net.atos.daf.ct2.vehicleservicerest.Controllers
 {
-    public class VehicleController : controllerBase
+    public class VehicleController : ControllerBase
     {
 
+        [HttpPost]
+        [Route("Test")]
+        public async Task<IActionResult> Test(string request)
+        {
+            try
+            {   
+                return Ok("You Name" + request);
+            }
+            catch (Exception ex)
+            {
+                
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }
     }
 }
