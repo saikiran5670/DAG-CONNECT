7771df6162fabc82a0f3e606fa325af253b929bc
Shailesh Rajput <shailesh-nathusing.rajput@atos.net>
2021-01-28 22:26:39 +0530

    Added Organization_Id while returning role on login

---
+++ dotnet/corecomponents/net.atos.daf.ct2.account/entity/AccountOrgRole.cs
@@ -0,0 +1,9 @@
+namespace net.atos.daf.ct2.account.entity
+{
+    public class AccountOrgRole
+    {
+         public int Id { get; set; }
+         public string Name { get; set; }  
+         public int Organization_Id { get; set; }
+    }
+}
