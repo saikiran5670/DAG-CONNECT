49b8efee9af26cec5311782e31a749d9a31eb49a
Shailesh Vijaivargia <shailesh.vijaivargia@atos.net>
2021-01-29 05:37:57 +0000

    Updated user from pgrdm_dbcreator_tst1 to pgrdm_dbcreator_dev2

--- dbscripts/postgresql/datamart/v0.01/_dev02/04-privilege.sql
+++ dbscripts/postgresql/datamart/v0.01/_dev02/04-privilege.sql
@@ -1,5 +1,5 @@
-ALTER USER pgrdm_dbcreator_tst1 WITH REPLICATION;
-GRANT USAGE ON SCHEMA MASTER TO pgrdm_dbcreator_tst1;
-GRANT ALL PRIVILEGES ON TABLE master.vehicle TO pgrdm_dbcreator_tst1;
+ALTER USER pgrdm_dbcreator_dev2 WITH REPLICATION;
+GRANT USAGE ON SCHEMA MASTER TO pgrdm_dbcreator_dev2;
+GRANT ALL PRIVILEGES ON TABLE master.vehicle TO pgrdm_dbcreator_dev2;
 CREATE PUBLICATION dbz_publication FOR TABLE master.vehicle WITH (publish = 'insert, update'); 
 
