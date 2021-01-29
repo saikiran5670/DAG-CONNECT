a009fab8dd8502d4ff522e0964b64431aaaa7998
Shailesh Vijaivargia <shailesh.vijaivargia@atos.net>
2021-01-29 05:47:25 +0000

    Added privileges

--- dbscripts/postgresql/datamart/v0.01/_test01/04-privilege.sql
+++ dbscripts/postgresql/datamart/v0.01/_test01/04-privilege.sql
@@ -1,5 +1,16 @@
 ALTER USER pgrdm_dbcreator_tst1 WITH REPLICATION;
-GRANT USAGE ON SCHEMA MASTER TO pgrdm_dbcreator_tst1;
+GRANT USAGE ON SCHEMA master TO pgrdm_dbcreator_tst1;
 GRANT ALL PRIVILEGES ON TABLE master.vehicle TO pgrdm_dbcreator_tst1;
 CREATE PUBLICATION dbz_publication FOR TABLE master.vehicle WITH (publish = 'insert, update'); 
 
+GRANT USAGE ON SCHEMA master TO pgrdm_dbcreator_tst1;
+GRANT USAGE ON SCHEMA tripdetail TO pgrdm_dbcreator_tst1;
+GRANT USAGE ON SCHEMA livefleet TO pgrdm_dbcreator_tst1;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA master TO pgrdm_dbcreator_tst1;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA tripdetail TO pgrdm_dbcreator_tst1;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA livefleet TO pgrdm_dbcreator_tst1;
+
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA master TO pgrdm_dbcreator_tst1;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA tripdetail TO pgrdm_dbcreator_tst1;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA livefleet TO pgrdm_dbcreator_tst1;
