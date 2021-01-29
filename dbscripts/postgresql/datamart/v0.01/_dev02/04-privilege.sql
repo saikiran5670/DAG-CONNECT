c916d58c8725c1f060d98890707097943c7f2765
Shailesh Vijaivargia <shailesh.vijaivargia@atos.net>
2021-01-29 05:46:15 +0000

    Added privileges

--- dbscripts/postgresql/datamart/v0.01/_dev02/04-privilege.sql
+++ dbscripts/postgresql/datamart/v0.01/_dev02/04-privilege.sql
@@ -3,3 +3,14 @@ GRANT USAGE ON SCHEMA MASTER TO pgrdm_dbcreator_dev2;
 GRANT ALL PRIVILEGES ON TABLE master.vehicle TO pgrdm_dbcreator_dev2;
 CREATE PUBLICATION dbz_publication FOR TABLE master.vehicle WITH (publish = 'insert, update'); 
 
+GRANT USAGE ON SCHEMA master TO pgrdm_dbcreator_dev2;
+GRANT USAGE ON SCHEMA tripdetail TO pgrdm_dbcreator_dev2;
+GRANT USAGE ON SCHEMA livefleet TO pgrdm_dbcreator_dev2;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA master TO pgrdm_dbcreator_dev2;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA tripdetail TO pgrdm_dbcreator_dev2;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA livefleet TO pgrdm_dbcreator_dev2;
+
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA master TO pgrdm_dbcreator_dev2;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA tripdetail TO pgrdm_dbcreator_dev2;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA livefleet TO pgrdm_dbcreator_dev2;
