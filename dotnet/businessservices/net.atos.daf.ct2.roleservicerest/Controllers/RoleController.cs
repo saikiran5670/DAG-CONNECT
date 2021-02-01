78d9cb84a361680ba8f13c1a6d96de8ee0de9d2d
Viranjay Singh <viranjay.kumar@gmail.com>
2021-02-01 21:17:31 +0530

    Role Description should be EMPTY or null

--- dotnet/businessservices/net.atos.daf.ct2.roleservicerest/Controllers/RoleController.cs
+++ dotnet/businessservices/net.atos.daf.ct2.roleservicerest/Controllers/RoleController.cs
@@ -58,7 +58,7 @@ namespace net.atos.daf.ct2.roleservicerest.Controllers
                     ObjRole.Organization_Id =roleMaster.OrganizationId;
                     ObjRole.Name = roleMaster.RoleName.Trim();
                     ObjRole.Createdby = roleMaster.Createdby;                    
-                    if(ObjRole.Description!= null || string.IsNullOrEmpty(ObjRole.Description))
+                    if(ObjRole.Description!= null && string.IsNullOrEmpty(ObjRole.Description))
                     ObjRole.Description=roleMaster.Description.Trim();
                     else ObjRole.Description=string.Empty;
 
@@ -108,7 +108,7 @@ namespace net.atos.daf.ct2.roleservicerest.Controllers
                     RoleEntity.RoleMaster ObjRole = new RoleEntity.RoleMaster();                   
                     ObjRole.Name = roleMaster.RoleName.Trim();
                     ObjRole.Id = roleMaster.RoleId;
-                    if(ObjRole.Description!= null || string.IsNullOrEmpty(ObjRole.Description))
+                    if(ObjRole.Description!= null && string.IsNullOrEmpty(ObjRole.Description))
                     ObjRole.Description=roleMaster.Description.Trim();
                     else ObjRole.Description=string.Empty;
 
