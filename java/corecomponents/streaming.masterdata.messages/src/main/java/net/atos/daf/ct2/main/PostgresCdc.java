7c09082b86029b97da7ca4963601faed8942e185
Chennareddy <sunitha.2.chennareddy@atos.net>
2021-02-15 11:46:07 +0530

    Egress layer changes

--- java/corecomponents/streaming.masterdata.messages/src/main/java/net/atos/daf/ct2/main/PostgresCdc.java
+++ java/corecomponents/streaming.masterdata.messages/src/main/java/net/atos/daf/ct2/main/PostgresCdc.java
@@ -1,5 +1,16 @@
 package net.atos.daf.ct2.main;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import io.debezium.config.Configuration;
 import io.debezium.connector.postgresql.PostgresConnectorConfig;
 import io.debezium.embedded.EmbeddedEngine;
@@ -7,147 +18,145 @@ import net.atos.daf.common.AuditETLJobClient;
 import net.atos.daf.common.ct2.utc.TimeFormatter;
 import net.atos.daf.ct2.constant.DAFCT2Constant;
 import net.atos.daf.ct2.exception.DAFCT2Exception;
+import net.atos.daf.ct2.processing.EgressCDCLayer;
 import net.atos.daf.ct2.processing.MessageProcessing;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
 
 public class PostgresCdc {
 
-  private static final Logger log = LogManager.getLogger(PostgresCdc.class);
-  private static AuditETLJobClient auditETLJobClient;
-  private static String FILE_PATH;// = "src/main/resources/configuration.properties";
-  private static Properties properties;
+	  private static final Logger log = LogManager.getLogger(PostgresCdc.class);
+	  private static String FILE_PATH; // = "src/main/resources/configuration.properties";
+	  private static AuditETLJobClient auditETLJobClient;
+	  private static Properties properties;
 
-  public static void main(String[] args) {
+	  public static void main(String[] args) {
 
-    try {
-      FILE_PATH = args[0];
+	    try {
+	       FILE_PATH= args[0];
 
-      PostgresCdc postgresCdc = new PostgresCdc();
-      Properties properties = postgresCdc.configuration(FILE_PATH);
-      // postgresCdc.auditTrail(properties);
+	      PostgresCdc postgresCdc = new PostgresCdc();
+	      Properties properties = postgresCdc.configuration(FILE_PATH);
+	      // postgresCdc.auditTrail(properties);
 
-      Configuration configuration = postgresCdc.connectingPostgreSQL(properties);
+	      Configuration configuration = postgresCdc.connectingPostgreSQL(properties);
 
-      MessageProcessing messageProcessing = new MessageProcessing(configuration, properties);
-      messageProcessing.run();
+	      if (Integer.parseInt(properties.getProperty(DAFCT2Constant.POSTGRE_CDC_VID_VIN)) == 1) {
+	        MessageProcessing messageProcessing = new MessageProcessing(configuration, properties);
+	        messageProcessing.run();
 
-    } catch (DAFCT2Exception e) {
-      log.error("Exception: ", e);
+	      } else {
+	        EgressCDCLayer egressCDCLayer = new EgressCDCLayer(configuration, properties);
+	        egressCDCLayer.run();
+	      }
 
-    } finally {
-      // auditETLJobClient.closeChannel();
-    }
-  }
+	    } catch (DAFCT2Exception e) {
+	      log.error("Exception: ", e);
 
-  public static void auditTrail(Properties properties) {
+	    } finally {
+	      // auditETLJobClient.closeChannel();
+	    }
+	  }
 
-    auditETLJobClient =
-        new AuditETLJobClient(
-            properties.getProperty(DAFCT2Constant.GRPC_SERVER),
-            Integer.valueOf(properties.getProperty(DAFCT2Constant.GRPC_PORT)));
+	  public static void auditTrail(Properties properties) {
 
-    Map<String, String> auditMap = new HashMap<String, String>();
+	    auditETLJobClient =
+	        new AuditETLJobClient(
+	            properties.getProperty(DAFCT2Constant.GRPC_SERVER),
+	            Integer.valueOf(properties.getProperty(DAFCT2Constant.GRPC_PORT)));
 
-    auditMap.put(DAFCT2Constant.JOB_EXEC_TIME, String.valueOf(TimeFormatter.getCurrentUTCTime()));
-    auditMap.put(DAFCT2Constant.AUDIT_PERFORMED_BY, DAFCT2Constant.TRIP_JOB_NAME);
-    auditMap.put(DAFCT2Constant.AUDIT_COMPONENT_NAME, DAFCT2Constant.TRIP_JOB_NAME);
-    auditMap.put(DAFCT2Constant.AUDIT_SERVICE_NAME, DAFCT2Constant.AUDIT_SERVICE);
-    auditMap.put(DAFCT2Constant.AUDIT_EVENT_TYPE, DAFCT2Constant.AUDIT_CREATE_EVENT_TYPE);
-    auditMap.put(
-        DAFCT2Constant.AUDIT_EVENT_TIME, String.valueOf(TimeFormatter.getCurrentUTCTime()));
-    auditMap.put(DAFCT2Constant.AUDIT_EVENT_STATUS, DAFCT2Constant.AUDIT_EVENT_STATUS_START);
-    auditMap.put(DAFCT2Constant.AUDIT_MESSAGE, "Masterdata Message Streaming");
-    auditMap.put(DAFCT2Constant.AUDIT_SOURCE_OBJECT_ID, DAFCT2Constant.DEFAULT_OBJECT_ID);
-    auditMap.put(DAFCT2Constant.AUDIT_TARGET_OBJECT_ID, DAFCT2Constant.DEFAULT_OBJECT_ID);
+	    Map<String, String> auditMap = new HashMap<String, String>();
 
-    auditETLJobClient.auditTrialGrpcCall(auditMap);
+	    auditMap.put(DAFCT2Constant.JOB_EXEC_TIME, String.valueOf(TimeFormatter.getCurrentUTCTime()));
+	    auditMap.put(DAFCT2Constant.AUDIT_PERFORMED_BY, DAFCT2Constant.TRIP_JOB_NAME);
+	    auditMap.put(DAFCT2Constant.AUDIT_COMPONENT_NAME, DAFCT2Constant.TRIP_JOB_NAME);
+	    auditMap.put(DAFCT2Constant.AUDIT_SERVICE_NAME, DAFCT2Constant.AUDIT_SERVICE);
+	    auditMap.put(DAFCT2Constant.AUDIT_EVENT_TYPE, DAFCT2Constant.AUDIT_CREATE_EVENT_TYPE);
+	    auditMap.put(
+	        DAFCT2Constant.AUDIT_EVENT_TIME, String.valueOf(TimeFormatter.getCurrentUTCTime()));
+	    auditMap.put(DAFCT2Constant.AUDIT_EVENT_STATUS, DAFCT2Constant.AUDIT_EVENT_STATUS_START);
+	    auditMap.put(DAFCT2Constant.AUDIT_MESSAGE, "Masterdata Message Streaming");
+	    auditMap.put(DAFCT2Constant.AUDIT_SOURCE_OBJECT_ID, DAFCT2Constant.DEFAULT_OBJECT_ID);
+	    auditMap.put(DAFCT2Constant.AUDIT_TARGET_OBJECT_ID, DAFCT2Constant.DEFAULT_OBJECT_ID);
 
-    log.info("Audit Trial Started");
-  }
+	    auditETLJobClient.auditTrialGrpcCall(auditMap);
 
-  public Properties configuration(String filePath) throws DAFCT2Exception {
+	    log.info("Audit Trial Started");
+	  }
 
-    Properties properties = new Properties();
-    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-    properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+	  public Properties configuration(String filePath) throws DAFCT2Exception {
 
-    try {
-      properties.load(new FileReader(filePath));
-      log.info("Configuration Loaded for Connecting Kafka.");
+	    Properties properties = new Properties();
+	    properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+	    properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
-    } catch (IOException e) {
-      log.error("Unable to Find the File " + filePath, e);
-      throw new DAFCT2Exception("Unable to Find the File " + filePath, e);
-    }
-    return properties;
-  }
+	    try {
+	      properties.load(new FileReader(filePath));
+	      log.info("Configuration Loaded for Connecting Kafka.");
 
-  public Configuration connectingPostgreSQL(Properties properties) throws DAFCT2Exception {
+	    } catch (IOException e) {
+	      log.error("Unable to Find the File " + filePath, e);
+	      throw new DAFCT2Exception("Unable to Find the File " + filePath, e);
+	    }
+	    return properties;
+	  }
 
-    Configuration configuration = null;
+	  public Configuration connectingPostgreSQL(Properties properties) throws DAFCT2Exception {
 
-    try {
-      configuration =
-          Configuration.create()
-              .with(
-                  EmbeddedEngine.CONNECTOR_CLASS,
-                  properties.getProperty(DAFCT2Constant.POSTGRE_CONNECTOR_CLASS))
-              .with(
-                  EmbeddedEngine.OFFSET_STORAGE,
-                  "org.apache.kafka.connect.storage.FileOffsetBackingStore")
-              .with(
-                  EmbeddedEngine.OFFSET_STORAGE_FILE_FILENAME,
-                  properties.getProperty(DAFCT2Constant.POSTGRE_OFFSET_STORAGE_FILE_FILENAME))
-              .with(
-                  EmbeddedEngine.OFFSET_FLUSH_INTERVAL_MS,
-                  properties.getProperty(DAFCT2Constant.POSTGRE_OFFSET_FLUSH_INTERVAL_MS))
-              .with("name", "PostgreCDC_Kafka_Streaming")
-              .with("database.server.id", properties.getProperty(DAFCT2Constant.POSTGRE_SERVER_ID))
-              .with(
-                  PostgresConnectorConfig.SERVER_NAME,
-                  properties.getProperty(DAFCT2Constant.POSTGRE_SERVER_NAME))
-              .with(
-                  PostgresConnectorConfig.HOSTNAME,
-                  properties.getProperty(DAFCT2Constant.POSTGRE_HOSTNAME))
-              .with(
-                  PostgresConnectorConfig.PORT, properties.getProperty(DAFCT2Constant.POSTGRE_PORT))
-              .with(
-                  PostgresConnectorConfig.USER, properties.getProperty(DAFCT2Constant.POSTGRE_USER))
-              .with(
-                  PostgresConnectorConfig.PASSWORD,
-                  properties.getProperty(DAFCT2Constant.POSTGRE_PASSWORD))
-              .with(
-                  PostgresConnectorConfig.SSL_MODE,
-                  properties.getProperty(DAFCT2Constant.POSTGRE_SSL))
-              .with(
-                  PostgresConnectorConfig.DATABASE_NAME,
-                  properties.getProperty(DAFCT2Constant.POSTGRE_DATABASE_NAME))
-              .with(
-                  PostgresConnectorConfig.TABLE_WHITELIST,
-                  properties.getProperty(DAFCT2Constant.POSTGRE_TABLE_WHITELIST))
-              .with(
-                  PostgresConnectorConfig.PLUGIN_NAME,
-                  properties.getProperty(DAFCT2Constant.POSTGRE_PLUGIN_NAME))
-              // .with("database.history", "io.debezium.relational.history.FileDatabaseHistory")
-              // .with("database.history.file.filename", "src/main/resources/dbhistory.dat")
-              .build();
+	    Configuration configuration = null;
 
-      log.info("Configure PostgeSQL Database");
+	    try {
+	      configuration =
+	          Configuration.create()
+	              .with(
+	                  EmbeddedEngine.CONNECTOR_CLASS,
+	                  properties.getProperty(DAFCT2Constant.POSTGRE_CONNECTOR_CLASS))
+	              .with(
+	                  EmbeddedEngine.OFFSET_STORAGE,
+	                  "org.apache.kafka.connect.storage.FileOffsetBackingStore")
+	              .with(
+	                  EmbeddedEngine.OFFSET_STORAGE_FILE_FILENAME,
+	                  properties.getProperty(DAFCT2Constant.POSTGRE_OFFSET_STORAGE_FILE_FILENAME))
+	              .with(
+	                  EmbeddedEngine.OFFSET_FLUSH_INTERVAL_MS,
+	                  properties.getProperty(DAFCT2Constant.POSTGRE_OFFSET_FLUSH_INTERVAL_MS))
+	              .with("name", "PostgreCDC_Kafka_Streaming")
+	              .with("database.server.id", properties.getProperty(DAFCT2Constant.POSTGRE_SERVER_ID))
+	              .with(
+	                  PostgresConnectorConfig.SERVER_NAME,
+	                  properties.getProperty(DAFCT2Constant.POSTGRE_SERVER_NAME))
+	              .with(
+	                  PostgresConnectorConfig.HOSTNAME,
+	                  properties.getProperty(DAFCT2Constant.POSTGRE_HOSTNAME))
+	              .with(
+	                  PostgresConnectorConfig.PORT, properties.getProperty(DAFCT2Constant.POSTGRE_PORT))
+	              .with(
+	                  PostgresConnectorConfig.USER, properties.getProperty(DAFCT2Constant.POSTGRE_USER))
+	              .with(
+	                  PostgresConnectorConfig.PASSWORD,
+	                  properties.getProperty(DAFCT2Constant.POSTGRE_PASSWORD))
+	              .with(
+	                  PostgresConnectorConfig.SSL_MODE,
+	                  properties.getProperty(DAFCT2Constant.POSTGRE_SSL))
+	              .with(
+	                  PostgresConnectorConfig.DATABASE_NAME,
+	                  properties.getProperty(DAFCT2Constant.POSTGRE_DATABASE_NAME))
+	              .with(
+	                  PostgresConnectorConfig.TABLE_WHITELIST,
+	                  properties.getProperty(DAFCT2Constant.POSTGRE_TABLE_WHITELIST))
+	              .with(
+	                  PostgresConnectorConfig.PLUGIN_NAME,
+	                  properties.getProperty(DAFCT2Constant.POSTGRE_PLUGIN_NAME))
+	              // .with("database.history", "io.debezium.relational.history.FileDatabaseHistory")
+	              // .with("database.history.file.filename", "src/main/resources/dbhistory.dat")
+	              .build();
 
-    } catch (Exception e) {
-      log.error("Unable to Configure PostgeSQL Database", e);
-      throw new DAFCT2Exception("Unable to Configure PostgeSQL Database", e);
-    }
+	      log.info("Configure PostgeSQL Database");
 
-    return configuration;
-  }
+	    } catch (Exception e) {
+	      log.error("Unable to Configure PostgeSQL Database", e);
+	      throw new DAFCT2Exception("Unable to Configure PostgeSQL Database", e);
+	    }
+
+	    return configuration;
+	  }
+	
 }
---
+++ java/corecomponents/streaming.masterdata.messages/src/main/java/net/atos/daf/ct2/processing/EgressCDCLayer.java
@@ -0,0 +1,105 @@
+package net.atos.daf.ct2.processing;
+
+import io.debezium.config.Configuration;
+import io.debezium.embedded.EmbeddedEngine;
+import net.atos.daf.ct2.exception.DAFCT2Exception;
+import net.atos.daf.ct2.utils.Operation;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static io.debezium.data.Envelope.FieldName.AFTER;
+import static io.debezium.data.Envelope.FieldName.OPERATION;
+public class EgressCDCLayer implements Runnable {
+
+		  private static final Logger log =
+		      LogManager.getLogger(net.atos.daf.ct2.processing.MessageProcessing.class);
+		  private final Configuration configuration;
+		  private final Producer producer;
+		  private final Properties properties;
+		  private EmbeddedEngine embeddedEngine;
+
+		  public EgressCDCLayer(Configuration configuration, Properties properties) throws DAFCT2Exception {
+
+		    this.configuration = configuration;
+		    this.properties = properties;
+
+		    this.producer = new Producer();
+		    this.producer.createConnection(properties);
+		  }
+
+		  @Override
+		  public void run() {
+
+		    try {
+		      this.embeddedEngine =
+		          EmbeddedEngine.create().using(this.configuration).notifying(this::handleEvent).build();
+
+		      ExecutorService executorService = Executors.newSingleThreadExecutor();
+		      executorService.execute(this.embeddedEngine);
+
+		      Runtime.getRuntime()
+		          .addShutdownHook(
+		              new Thread(
+		                  () -> {
+		                    log.info("Requesting embedded engine to shut down");
+		                    this.embeddedEngine.stop();
+		                    // this.producer.closePublisher();
+		                  }));
+
+		      // the submitted task keeps running, only no more new ones can be added
+		      executorService.shutdown();
+
+		      awaitTermination(executorService);
+
+		      log.info("Engine terminated");
+
+		    } catch (Exception e) {
+		      log.error("Unable to process Postgre CDC ", e.getMessage());
+		    }
+		  }
+
+		  private void awaitTermination(ExecutorService executorService) {
+
+		    try {
+		      while (!executorService.awaitTermination(10, TimeUnit.SECONDS)) {
+		        log.info("Waiting another 10 seconds for the embedded engine to complete");
+		      }
+
+		    } catch (InterruptedException e) {
+		      Thread.currentThread().interrupt();
+		    }
+		  }
+
+		  private void handleEvent(SourceRecord sourceRecord) {
+
+		    Struct sourceRecordValue = (Struct) sourceRecord.value();
+
+		    if (sourceRecordValue != null) {
+		      try {
+		        Operation operation = Operation.forCode((String) sourceRecordValue.get(OPERATION));
+
+		        // Only if this is a transactional operation.
+		        // if (operation != Operation.READ) {
+		        Struct struct = (Struct) sourceRecordValue.get(AFTER);
+
+		        if (struct != null) {
+		          this.producer.publishMessage(
+		              UUID.randomUUID().toString(), struct.toString(), this.properties);
+		          System.out.println("Message:"+struct.toString());
+		        }
+
+		      } catch (Exception e) {
+		        // log.warn(e.getMessage());
+		    	  log.error(e);
+		      }
+		    }
+		  }
+}
