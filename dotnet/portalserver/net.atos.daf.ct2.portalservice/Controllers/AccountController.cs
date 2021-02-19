afbff30496ecae374a7992b4dd910cad4e37dd02
Shailesh Rajput <shailesh-nathusing.rajput@atos.net>
2021-02-19 16:20:39 +0530

    added account Id into the responce of Auth method

--- dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/AccountController.cs
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/AccountController.cs
@@ -41,19 +41,23 @@ namespace net.atos.daf.ct2.portalservice.Controllers
                 }
 
                 AccountBusinessService.AccountData accountResponse = await _accountClient.CreateAsync(request);
-                if (accountResponse.Code==AccountBusinessService.Responcecode.Failed 
+                if (accountResponse!=null && accountResponse.Code==AccountBusinessService.Responcecode.Failed 
                     && accountResponse.Message == "The duplicate account, please provide unique email address.")
                 {
                     return StatusCode(409, "Duplicate Account.");
                 }
-                else if (accountResponse.Code==AccountBusinessService.Responcecode.Failed
+                else if (accountResponse!=null && accountResponse.Code==AccountBusinessService.Responcecode.Failed
                     && accountResponse.Message == "There is an error creating account.")
                 {
                     return StatusCode(500, "There is an error creating account.");
                 }
-                else
+                else if(accountResponse!=null && accountResponse.Code==AccountBusinessService.Responcecode.Success)
                 {
                     return Ok(accountResponse);
+                }
+                else
+                {
+                    return StatusCode(500, "Internal Server Error.");
                 }
             }
             catch (Exception ex)
@@ -86,20 +90,25 @@ namespace net.atos.daf.ct2.portalservice.Controllers
                     return StatusCode(400, "The EmailId address, first name, last name and organization id should be valid.");
                 }
                 AccountBusinessService.AccountData accountResponse = await _accountClient.UpdateAsync(request);
-                if (accountResponse.Code==AccountBusinessService.Responcecode.Failed 
+                if (accountResponse!=null && accountResponse.Code==AccountBusinessService.Responcecode.Failed 
                     && accountResponse.Message == "The duplicate account, please provide unique email address.")
                 {
                     return StatusCode(409, "Duplicate Account.");
                 }
-                else if (accountResponse.Code==AccountBusinessService.Responcecode.Failed
+                else if (accountResponse !=null && accountResponse.Code==AccountBusinessService.Responcecode.Failed
                     && accountResponse.Message == "There is an error creating account.")
                 {
                     return StatusCode(500, "There is an error creating account.");
                 }
-                else
+                else if(accountResponse!=null && accountResponse.Code==AccountBusinessService.Responcecode.Success)
                 {
                     return Ok(accountResponse);
                 }
+                else
+                {
+                    return StatusCode(500, "Internal Server Error.");
+                }
+
             }
             catch (Exception ex)
             {
@@ -128,7 +137,7 @@ namespace net.atos.daf.ct2.portalservice.Controllers
                 accountRequest.EmailId = EmailId;
                 accountRequest.OrganizationId = OrganizationId;
                 AccountBusinessService.AccountResponse response = await _accountClient.DeleteAsync(accountRequest);
-                if (response.Code == AccountBusinessService.Responcecode.Success) 
+                if (response!=null && response.Code == AccountBusinessService.Responcecode.Success) 
                     return Ok(accountRequest);
                 else 
                     return StatusCode(404, "Account not configured.");
@@ -150,7 +159,7 @@ namespace net.atos.daf.ct2.portalservice.Controllers
                     return StatusCode(404, "The Email address and password is required.");
                 }
                 AccountBusinessService.AccountResponse response = await _accountClient.ChangePasswordAsync(request);
-                if (response.Code == AccountBusinessService.Responcecode.Success) 
+                if (response!=null && response.Code == AccountBusinessService.Responcecode.Success) 
                     return Ok("Password has been changed.");
                 else 
                     return StatusCode(404, "Account not configured.");
@@ -174,15 +183,14 @@ namespace net.atos.daf.ct2.portalservice.Controllers
                 {
                     return StatusCode(404, "One of the parameter to filter account is required.");
                 }
-                
                 AccountBusinessService.AccountDataList  response = await _accountClient.GetAsync(accountFilter);
-                if (response!=null || (response!=null && response.Code==AccountBusinessService.Responcecode.Failed))
+                if (response !=null && response.Code==AccountBusinessService.Responcecode.Success)
                 {
-                    return StatusCode(404, "Accounts is not found.");
+                    return Ok(response);
                 }
                 else
                 {
-                    return Ok(response);
+                    return StatusCode(404, "Accounts is not found.");
                 }
             }
             catch (Exception ex)
@@ -190,7 +198,255 @@ namespace net.atos.daf.ct2.portalservice.Controllers
                 _logger.LogError("Error in account service:get accounts with exception - " + ex.Message + ex.StackTrace);
                 return StatusCode(500, "Internal Server Error.");
             }
-        }        
-            
+        }
+        [HttpPost]
+        [Route("getaccountdetail")]
+        public async Task<IActionResult> GetAccountDetail(AccountBusinessService.AccountGroupDetailsRequest request)
+        {
+            try
+            {
+                // Validation                 
+                if (request.OrganizationId <= 0)
+                {
+                    return StatusCode(400, "The organization is required");
+                }
+                AccountBusinessService.AccountDetailsResponse response=await _accountClient.GetAccountDetailAsync(request);
+
+                if (response !=null && response.Code==AccountBusinessService.Responcecode.Success)
+                {
+                    return Ok(response);
+                }
+                else
+                {
+                    return StatusCode(404, "Accounts is not found.");
+                }
             }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in account service:get accounts with exception - " + ex.Message + ex.StackTrace);
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }
+         // Begin Account Preference
+        [HttpPost]
+        [Route("preference/create")]
+        public async Task<IActionResult> CreateAccountPreference(AccountBusinessService.AccountPreference request)
+        {
+            //Task<AccountPreferenceResponse> CreatePreference(AccountPreference request, 
+            try
+            {
+                // Validation                 
+                if ((request.RefId <= 0) || (request.LanguageId <= 0) || (request.TimezoneId <= 0) || (request.CurrencyId <= 0) ||
+                    (request.UnitId <= 0) || (request.VehicleDisplayId <= 0) || (request.DateFormatId <= 0) || (request.TimeFormatId <= 0) ||
+                    (request.LandingPageDisplayId <= 0)
+                    )
+                {
+                    return StatusCode(400, "The Account Id, LanguageId, TimezoneId, CurrencyId, UnitId, VehicleDisplayId,DateFormatId, TimeFormatId, LandingPageDisplayId is required");
+                }
+                
+                AccountBusinessService.AccountPreferenceResponse preference = await _accountClient.CreatePreferenceAsync(request);
+                if (preference !=null && preference.Code==AccountBusinessService.Responcecode.Success)
+                {
+                    return Ok(preference);
+                }
+                else
+                {
+                    return StatusCode(500, "Internal  Server error");
+                }                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in account service:create preference with exception - " + ex.Message + ex.StackTrace);
+                // check for fk violation
+                if (ex.Message.Contains(FK_Constraint))
+                {
+                    return StatusCode(400, "The foreign key violation in one of dependant data.");
+                }                
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }        
+        [HttpPost]
+        [Route("preference/update")]
+        public async Task<IActionResult> UpdateAccountPreference(AccountBusinessService.AccountPreference request)
+        {        
+            try
+            {
+                // Validation                 
+                if ((request.RefId <= 0) || (request.LanguageId <= 0) || (request.TimezoneId <= 0) || (request.CurrencyId <= 0) ||
+                    (request.UnitId <= 0) || (request.VehicleDisplayId <= 0) || (request.DateFormatId <= 0) || (request.TimeFormatId <= 0) ||
+                    (request.LandingPageDisplayId <= 0)
+                    )
+                {
+                    return StatusCode(400, "The Account Id, LanguageId, TimezoneId, CurrencyId, UnitId, VehicleDisplayId,DateFormatId, TimeFormatId, LandingPageDisplayId is required");
+                }
+                AccountBusinessService.AccountPreferenceResponse preference = await _accountClient.UpdatePreferenceAsync(request);
+                if (preference !=null && preference.Code==AccountBusinessService.Responcecode.Success)
+                {
+                    return Ok(preference);
+                }
+                else
+                {
+                    return StatusCode(500, "Internal  Server error");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in account service:create preference with exception - " + ex.Message + ex.StackTrace);
+                // check for fk violation
+                if (ex.Message.Contains(FK_Constraint))
+                {
+                    return StatusCode(400, "The foreign key violation in one of dependant data.");
+                }
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }
+        [HttpDelete]
+        [Route("preference/delete")]
+        public async Task<IActionResult> DeleteAccountPreference(int accountId)
+        { 
+            try
+            {
+                AccountBusinessService.AccountPreferenceFilter request =new AccountBusinessService.AccountPreferenceFilter();
+                // Validation                 
+                if (accountId <= 0)
+                {
+                    return StatusCode(400, "The Account Id is required");
+                }
+                request.RefId=accountId;
+                AccountBusinessService.AccountPreferenceResponse response= await _accountClient.DeletePreferenceAsync(request);
+                if (response !=null && response.Code==AccountBusinessService.Responcecode.Success)
+                {
+                    return Ok(response);
+                }
+                else
+                {
+                    return StatusCode(500, "Internal  Server error");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in account service:create preference with exception - " + ex.Message + ex.StackTrace);
+                // check for fk violation
+                if (ex.Message.Contains(FK_Constraint))
+                {
+                    return StatusCode(400, "The foreign key violation in one of dependant data.");
+                }
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }
+        [HttpGet]
+        [Route("preference/get")]
+        public async Task<IActionResult> GetAccountPreference(int accountId)
+        {
+            try
+            {
+                // Validation                 
+                if ((accountId <= 0))
+                {
+                    return StatusCode(400, "The Account Id is required");
+                }
+                AccountBusinessService.AccountPreferenceFilter request =new AccountBusinessService.AccountPreferenceFilter();
+                request.RefId = accountId;
+                AccountBusinessService.AccountPreferenceDataList response = _accountClient.GetPreference(request);
+                if (response != null && response.Code==AccountBusinessService.Responcecode.Success)
+                {
+                    return Ok(response);
+                }
+                else
+                {
+                    return StatusCode(500, "Internal  Server error");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in account service:get account preference with exception - " + ex.Message + ex.StackTrace);
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }
+        // End - Account Preference
+        // End - Account Preference
+
+        // Begin - AccessRelationship
+        [HttpPost]
+        [Route("accessrelationship/create")]
+        public async Task<IActionResult> CreateAccessRelationship(AccountBusinessService.AccessRelationship request)
+        {            
+            try
+            {
+                // Validation                 
+                if ((request.AccountGroupId <= 0) || (request.VehicleGroupId <= 0) || (string.IsNullOrEmpty(Convert.ToString(request.AccessRelationType))))
+                {
+                    return StatusCode(400, "The AccountGroupId,VehicleGroupId and AccessRelationshipType is required");
+                }
+                else if ((!request.AccessRelationType.Equals("R")) && (!request.AccessRelationType.Equals("W")))
+                {
+                    return StatusCode(400, "The AccessRelationshipType should be ReadOnly and ReadWrite (R/W) only.");
+                }
+                AccountBusinessService.AccessRelationshipResponse accessRelationship = await _accountClient.CreateAccessRelationshipAsync(request);
+                if (accessRelationship != null && accessRelationship.Code==AccountBusinessService.Responcecode.Success)
+                {
+                    return Ok(accessRelationship);
+                }
+                else if (accessRelationship != null && accessRelationship.Code==AccountBusinessService.Responcecode.Failed 
+                    && accessRelationship.Message=="The AccessType should be ReadOnly / ReadWrite.(R/W).")
+                {
+                    return StatusCode(400, "The AccessType should be ReadOnly / ReadWrite.(R/W).");
+                }
+                else
+                {
+                    return StatusCode(500, "Internal  Server error");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in account service:get accounts with exception - " + ex.Message + ex.StackTrace);
+                // check for fk violation
+                if (ex.Message.Contains(FK_Constraint))
+                {
+                    return StatusCode(400, "The foreign key violation in one of dependant data.");
+                }
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }
+        [HttpPost]
+        [Route("accessrelationship/update")]
+        public async Task<IActionResult> UpdateAccessRelationship(AccountBusinessService.AccessRelationship request)
+        {
+            try
+            {
+                // Validation                 
+                if ((request.AccountGroupId <= 0) || (request.VehicleGroupId <= 0) || (string.IsNullOrEmpty(Convert.ToString(request.AccessRelationType))))
+                {
+                    return StatusCode(400, "The AccountGroupId,VehicleGroupId and AccessRelationshipType is required");
+                }
+                else if ((!request.AccessRelationType.Equals("R")) && (!request.AccessRelationType.Equals("W")))
+                {
+                    return StatusCode(400, "The AccessRelationshipType should be ReadOnly and ReadWrite (R/W) only.");
+                }
+                AccountBusinessService.AccessRelationshipResponse accessRelationship = await _accountClient.UpdateAccessRelationshipAsync(request);
+                if (accessRelationship != null && accessRelationship.Code==AccountBusinessService.Responcecode.Success)
+                {
+                    return Ok(accessRelationship);
+                }
+                else if (accessRelationship != null && accessRelationship.Code==AccountBusinessService.Responcecode.Failed 
+                    && accessRelationship.Message=="The AccessType should be ReadOnly / ReadWrite.(R/W).")
+                {
+                    return StatusCode(400, "The AccessType should be ReadOnly / ReadWrite.(R/W).");
+                }
+                else
+                {
+                    return StatusCode(500, "Internal  Server error");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error in account service:get accounts with exception - " + ex.Message + ex.StackTrace);
+                return StatusCode(500, "Internal Server Error.");
+            }
+        }
+
+    
+    }
 }
