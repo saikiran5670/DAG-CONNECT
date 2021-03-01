4508cfa8a11e6f58d7678d1f3169e8d978af62ae
VIRANJAY SINGH <VIRANJAY.SINGH@ATOS.NET>
2021-03-01 15:12:47 +0530

    Application Service Added.

---
+++ dotnet/net.atos.daf.ct2.applicationservice/Controllers/ApplicationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net.atos.daf.ct2.applicationservice.Controllers
+{
+    [ApiController]
+    [Route("application")]
+    public class ApplicationController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get")]
+        public IActionResult Get()
+        {
+            string result = "Get Method Response";
+            return Ok(result);
+        }
+    }
+}
