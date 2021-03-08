f12a8fac5db1bbe5f6bb3ed250b0ec95bc8798e6
Shailesh Vijaivargia <shailesh.vijaivargia@atos.net>
2021-03-08 05:54:51 +0000

    Changed default user from pgrmd_dbcreator_tst2 to pgrmd_dbcreator_acc3

--- dbscripts/postgresql/masterdb/v1.00/_acc3/05-privilege.sql
+++ dbscripts/postgresql/masterdb/v1.00/_acc3/05-privilege.sql
@@ -1,13 +1,13 @@
-GRANT USAGE ON SCHEMA master TO pgrmd_dbcreator_tst2;
-GRANT USAGE ON SCHEMA logs TO pgrmd_dbcreator_tst2;
-GRANT USAGE ON SCHEMA translation TO pgrmd_dbcreator_tst2;
+GRANT USAGE ON SCHEMA master TO pgrmd_dbcreator_acc3;
+GRANT USAGE ON SCHEMA logs TO pgrmd_dbcreator_acc3;
+GRANT USAGE ON SCHEMA translation TO pgrmd_dbcreator_acc3;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA master TO pgrmd_dbcreator_tst2;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA logs TO pgrmd_dbcreator_tst2;
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA translation TO pgrmd_dbcreator_tst2;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA master TO pgrmd_dbcreator_acc3;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA logs TO pgrmd_dbcreator_acc3;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA translation TO pgrmd_dbcreator_acc3;
 
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA master TO pgrmd_dbcreator_tst2;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA logs TO pgrmd_dbcreator_tst2;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA translation TO pgrmd_dbcreator_tst2;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA master TO pgrmd_dbcreator_acc3;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA logs TO pgrmd_dbcreator_acc3;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA translation TO pgrmd_dbcreator_acc3;
 
 
