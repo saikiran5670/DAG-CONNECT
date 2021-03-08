8a1f67236d03dabc40c35596bd5bf4275beebf55
Gaurav Gadewar <gaurav.ratnakargadewar@atos.net>
2021-03-08 21:51:45 +0530

    Updated TCU Vehicle service

--- dotnet/businessservices/TCUProvisioningVehicle/TCUProvisioning/Program.cs
+++ dotnet/businessservices/TCUProvisioningVehicle/TCUProvisioning/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using log4net;
+using log4net.Config;
 
 namespace TCUProvisioning
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         static async System.Threading.Tasks.Task Main(string[] args)
         {
             string brokerList = ConfigurationManager.AppSetting["EH_FQDN"];
@@ -13,7 +19,11 @@ namespace TCUProvisioning
             string consumerGroup = ConfigurationManager.AppSetting["CONSUMER_GROUP"];
             string psqlConnString = ConfigurationManager.AppSetting["psqlconnstring"];
 
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
             Console.WriteLine("Initializing Consumer");
+            //log.Info("Hello logging world!");
             await ProvisionVehicle.readTCUProvisioningData(brokerList, connectionString, consumerGroup, topic, caCertLocation, psqlConnString);
             Console.ReadKey();
 
