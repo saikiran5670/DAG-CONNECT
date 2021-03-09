db6545dfaa6b218e23a1410abc05dbb13fab583d
Gaurav Gadewar <gaurav.ratnakargadewar@atos.net>
2021-03-09 20:36:05 +0530

    Updated TCU Vehicle service

--- dotnet/businessservices/TCUProvisioningVehicle/TCUProvisioning/Program.cs
+++ dotnet/businessservices/TCUProvisioningVehicle/TCUProvisioning/Program.cs
@@ -12,19 +12,12 @@ namespace TCUProvisioning
 
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            string brokerList = ConfigurationManager.AppSetting["EH_FQDN"];
-            string connectionString = ConfigurationManager.AppSetting["EH_CONNECTION_STRING"];
-            string topic = ConfigurationManager.AppSetting["EH_NAME"];
-            string caCertLocation = ConfigurationManager.AppSetting["CA_CERT_LOCATION"];
-            string consumerGroup = ConfigurationManager.AppSetting["CONSUMER_GROUP"];
-            string psqlConnString = ConfigurationManager.AppSetting["psqlconnstring"];
+            
 
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
-
-            Console.WriteLine("Initializing Consumer");
-            //log.Info("Hello logging world!");
-            await ProvisionVehicle.readTCUProvisioningData(brokerList, connectionString, consumerGroup, topic, caCertLocation, psqlConnString);
+            ProvisionVehicle provisionVehicle = new ProvisionVehicle(log);
+            await provisionVehicle.readTCUProvisioningData();
             Console.ReadKey();
 
 
