38647643a95f87acd416aab911827a5a18107260
Kumari, Archana <archana.kumari@atos.net>
2021-03-09 16:28:37 +0530

    Package Validation added

--- dotnet/portalserver/net.atos.daf.ct2.portalservice/Entity/Package/PackagePortalRequest.cs
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Entity/Package/PackagePortalRequest.cs
@@ -8,13 +8,16 @@ namespace net.atos.daf.ct2.portalservice.Entity.Package
     {
         public int Id { get; set; }
         [Required]
+        [StringLength(20, MinimumLength = 7)]
         public string Code { get; set; }
         public int FeatureSetID { get; set; }
         [Required]
         public List<string> Features { get; set; }
         [Required]
+        [StringLength(50, MinimumLength = 5)]
         public string Name { get; set; }
         public string Type { get; set; }
+        [StringLength(100)]
         public string Description { get; set; }        
         public string Status { get; set; }
     }
