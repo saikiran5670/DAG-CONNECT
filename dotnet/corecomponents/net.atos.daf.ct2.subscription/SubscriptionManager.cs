a7d318def43aec9fa968fbb5c5d047e5c7b08377
Manoj Nandamuri <manoj.nandamuri@atos.net>
2021-03-10 12:55:19 +0530

    Subscription CoreComponent and Test project

---
+++ dotnet/corecomponents/net.atos.daf.ct2.subscription/SubscriptionManager.cs
@@ -0,0 +1,28 @@
+﻿using net.atos.daf.ct2.subscription.entity;
+using System;
+using net.atos.daf.ct2.subscription.repository;
+using System.Threading.Tasks;
+
+namespace net.atos.daf.ct2.subscription
+{
+    public class SubscriptionManager : ISubscriptionManager
+    {
+
+        ISubscriptionRepository subscriptionRepository;
+
+        public SubscriptionManager(ISubscriptionRepository _subscriptionRepository)
+        {
+            subscriptionRepository = _subscriptionRepository;
+        }
+
+        public async Task<SubscriptionResponse> Subscribe(Subscription objSubscription)
+        {
+            return await subscriptionRepository.Subscribe(objSubscription);
+        }
+
+        public async Task<SubscriptionResponse> Unsubscribe(UnSubscription objUnSubscription)
+        {
+            return await subscriptionRepository.Unsubscribe(objUnSubscription);
+        }
+    }
+}
---
+++ dotnet/corecomponents/net.atos.daf.ct2.subscription/repository/ISubscriptionRepository.cs
@@ -0,0 +1,11 @@
+﻿using net.atos.daf.ct2.subscription.entity;
+using System.Threading.Tasks;
+
+namespace net.atos.daf.ct2.subscription.repository
+{
+    public interface ISubscriptionRepository
+    {
+         Task<SubscriptionResponse> Subscribe(Subscription objSubscription);
+         Task<SubscriptionResponse> Unsubscribe(UnSubscription objUnSubscription);
+    }
+}
