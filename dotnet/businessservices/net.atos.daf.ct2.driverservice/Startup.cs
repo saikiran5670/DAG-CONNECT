ca6d2e93125611a5dc0f82bfa0edf29becfdc882
Ram Pratap <ram.pratap@atos.net>
2021-03-10 12:26:42 +0530

    Drivermanagement GRPC

---
+++ dotnet/businessservices/net.atos.daf.ct2.driverservice/Startup.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using net.atos.daf.ct2.data;
+using net.atos.daf.ct2.audit;
+using net.atos.daf.ct2.driver;
+using Microsoft.Extensions.Configuration;
+using net.atos.daf.ct2.audit.repository;
+using Microsoft.AspNetCore.Http;
+
+namespace net.atos.daf.ct2.driverservice
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+       
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();   
+            services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+                {
+                builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+                 }));         
+                    
+            string connectionString = Configuration.GetConnectionString("ConnectionString");
+            //connectionString=@"Server=dafct-dev0-dta-cdp-pgsql.postgres.database.azure.com;Database=dafconnectmasterdatabase;Port=5432;User Id=pgadmin@dafct-dev0-dta-cdp-pgsql;Password=W%PQ1AI}Y97;Ssl Mode=Require;";
+
+            IDataAccess dataAccess = new PgSQLDataAccess(connectionString);
+            services.AddSingleton(dataAccess); 
+            services.AddTransient<IAuditTraillib,AuditTraillib>(); 
+            services.AddTransient<IAuditLogRepository, AuditLogRepository>(); 
+            services.AddTransient<IDriverManager,DriverManager>();
+            services.AddTransient<IDriverRepository, DriverRepository>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }           
+            app.UseRouting();
+            app.UseGrpcWeb();
+            app.UseCors();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>().EnableGrpcWeb()
+                                                  .RequireCors("AllowAll");
+                endpoints.MapGrpcService<DriverManagementService>().EnableGrpcWeb()
+                                                  .RequireCors("AllowAll");
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });             
+        }
+    }
+}
--- dotnet/corecomponents/net.atos.daf.ct2.driver/entity/Driver.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.driver/entity/Driver.cs
@@ -13,5 +13,35 @@ namespace net.atos.daf.ct2.driver.entity
         public long DateOfBith { get; set; }
         public string Status { get; set; }
         public Boolean IsActive { get; set; }
+        public string opt_in { get; set; }
+        public long modified_at { get; set; }
+        public int modified_by { get; set; }
+        public long created_at { get; set; }
+         public string Email { get; set; }
+    }
+    public class DriverResponse
+    {
+        public int Id { get; set; }
+        public int Organization_id { get; set; }        
+        public string Driver_id_ext { get; set; }
+        public string Salutation { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBith { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public Boolean IsActive { get; set; }
+        public string opt_in { get; set; }
+        public string modified_at { get; set; }
+        public string modified_by { get; set; }
+        public string created_at { get; set; }
+    }
+    public class DriverImportResponse
+    {
+        public string DriverID { get; set; }       
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ReturnMessage { get; set; }
     }
 }
