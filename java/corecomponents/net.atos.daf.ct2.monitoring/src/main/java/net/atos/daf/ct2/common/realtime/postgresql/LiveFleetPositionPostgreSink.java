fd285c9d35cd30132353225149ee659002a86ef3
Jadhav <kalyan.jadhav@atos.net>
2021-03-10 11:43:23 +0530

    Changes

--- java/corecomponents/net.atos.daf.ct2.monitoring/src/main/java/net/atos/daf/ct2/common/realtime/postgresql/LiveFleetPositionPostgreSink.java
+++ java/corecomponents/net.atos.daf.ct2.monitoring/src/main/java/net/atos/daf/ct2/common/realtime/postgresql/LiveFleetPositionPostgreSink.java
@@ -23,8 +23,13 @@ public class LiveFleetPositionPostgreSink extends RichSinkFunction<KafkaRecord<M
 	private static final long serialVersionUID = 1L;
 	Logger log = LoggerFactory.getLogger(MonitorDataProcess.class);
 
-	//String livefleetposition = "INSERT INTO livefleet.livefleet_position_statistics ( trip_id	, vin	,message_time_stamp	,gps_altitude	,gps_heading	,gps_latitude	,gps_longitude	,co2_emission	,fuel_consumption	, last_odometer_val  ,distance_until_next_service	, created_at_m2m	,created_at_kafka	,created_at_dm	) VALUES (?	,?	,?	,?	,?	,?	,?	,?	,?	,?	,?	,?	,?	,?	)";
-	
+	// String livefleetposition = "INSERT INTO
+	// livefleet.livefleet_position_statistics ( trip_id , vin ,message_time_stamp
+	// ,gps_altitude ,gps_heading ,gps_latitude ,gps_longitude ,co2_emission
+	// ,fuel_consumption , last_odometer_val ,distance_until_next_service ,
+	// created_at_m2m ,created_at_kafka ,created_at_dm ) VALUES (? ,? ,? ,? ,? ,? ,?
+	// ,? ,? ,? ,? ,? ,? ,? )";
+
 	String livefleetposition = null;
 
 	private PreparedStatement statement;
@@ -32,24 +37,47 @@ public class LiveFleetPositionPostgreSink extends RichSinkFunction<KafkaRecord<M
 
 	public void invoke(KafkaRecord<Monitor> row) throws Exception {
 
-		int varVEvtid = row.getValue().getVEvtID();
+		int varVEvtid = 0;
+
+		if (row.getValue().getVEvtID() != null) {
+
+			varVEvtid = row.getValue().getVEvtID();
+		}
 		double varGPSLongi = row.getValue().getGpsLongitude();
+
 		String varTripID = row.getValue().getDocument().getTripID();
 
 		if (varTripID != null) {
 			statement.setString(1, row.getValue().getDocument().getTripID()); // tripID
 		}
 
-		else {
+		else if (row.getValue().getRoName() != null) {
 			statement.setString(1, row.getValue().getRoName());
+		} else {
+			statement.setString(1, "TRIP_ID NOT AVAILABLE");
 		}
 
 		if (row.getValue().getVin() != null)
 			statement.setString(2, (String) row.getValue().getVin()); // vin
-		else
+		else if (row.getValue().getVid() != null) {
 			statement.setString(2, (String) row.getValue().getVid());
+		} else {
+			statement.setString(2, "VIN NOT AVAILABLE");
+		}
 
-		statement.setLong(3, row.getValue().getReceivedTimestamp()); // message_time_stamp
+		if (row.getValue().getReceivedTimestamp() != 0 || row.getValue().getReceivedTimestamp() != null) {
+			statement.setLong(3, row.getValue().getReceivedTimestamp()); // message_time_stamp
+			statement.setLong(12, row.getValue().getReceivedTimestamp()); // created_at_m2m
+			statement.setLong(13, row.getValue().getReceivedTimestamp()); // created_at_kafka
+			statement.setLong(14, row.getValue().getReceivedTimestamp()); // created_at_dm
+		}
+
+		else {
+			statement.setLong(3, 0); // message_time_stamp
+			statement.setLong(12, 0); // created_at_m2m
+			statement.setLong(13, 0); // created_at_kafka
+			statement.setLong(14, 0); // created_at_dm
+		}
 
 		if (varGPSLongi == 255.0) {
 
@@ -59,16 +87,36 @@ public class LiveFleetPositionPostgreSink extends RichSinkFunction<KafkaRecord<M
 			statement.setDouble(7, 255.0);
 
 		} else {
-			statement.setDouble(4, row.getValue().getGpsAltitude()); 
-			statement.setDouble(5, row.getValue().getGpsHeading()); 
-			statement.setDouble(6, row.getValue().getGpsLatitude()); 
-			statement.setDouble(7, row.getValue().getGpsLongitude()); 
+
+			if (row.getValue().getGpsAltitude() != null)
+				statement.setDouble(4, row.getValue().getGpsAltitude());
+			else
+				statement.setDouble(4, 0);
+			if (row.getValue().getGpsHeading() != null)
+				statement.setDouble(5, row.getValue().getGpsHeading());
+			else
+				statement.setDouble(5, 0);
+			if (row.getValue().getGpsLatitude() != null)
+				statement.setDouble(6, row.getValue().getGpsLatitude());
+			else
+				statement.setDouble(6, 0);
+			if (row.getValue().getGpsLongitude() != null)
+				statement.setDouble(7, row.getValue().getGpsLongitude());
+			else
+				statement.setDouble(7, 0);
 
 		}
 
-		statement.setDouble(8, row.getValue().getDocument().getVFuelLevel1()); // CO2 Emission // Have to apply formula
-																				
-		statement.setDouble(9, row.getValue().getDocument().getVFuelLevel1()); // fuel_consumption
+		if (row.getValue().getDocument().getVFuelLevel1() != null)
+			statement.setDouble(8, row.getValue().getDocument().getVFuelLevel1()); // CO2 Emission // Have to apply
+																					// formula
+		else
+			statement.setDouble(8, 0); // CO2 Emission // Have to apply formula
+
+		if (row.getValue().getDocument().getVFuelLevel1() != null)
+			statement.setDouble(9, row.getValue().getDocument().getVFuelLevel1()); // fuel_consumption
+		else
+			statement.setDouble(9, 0); // fuel_consumption
 
 		if (varVEvtid == 26 || varVEvtid == 28 || varVEvtid == 29 || varVEvtid == 32 || varVEvtid == 42
 				|| varVEvtid == 43 || varVEvtid == 44 || varVEvtid == 45 || varVEvtid == 46) {
@@ -84,10 +132,6 @@ public class LiveFleetPositionPostgreSink extends RichSinkFunction<KafkaRecord<M
 			statement.setLong(11, 0);
 		}
 
-		statement.setLong(12, row.getValue().getReceivedTimestamp()); // created_at_m2m
-		statement.setLong(13, row.getValue().getReceivedTimestamp()); // created_at_kafka
-		statement.setLong(14, row.getValue().getReceivedTimestamp()); // created_at_dm
-
 		statement.executeUpdate();
 
 	}
@@ -97,20 +141,21 @@ public class LiveFleetPositionPostgreSink extends RichSinkFunction<KafkaRecord<M
 
 		log.info("########## In LiveFleet Position ##############");
 		ParameterTool envParams = (ParameterTool) getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
-		
+
 		livefleetposition = envParams.get(DafConstants.QUERY_LIVEFLEET_POSITION);
 
 		try {
 			//
-			
-//			Jdbc3PoolingDataSource dataSource = PostgreDataSourceConnection.getDataSource(
-//			envParams.get(DafConstants.DATAMART_POSTGRE_SERVER_NAME),
-//			Integer.parseInt(envParams.get(DafConstants.DATAMART_POSTGRE_PORT)),
-//			envParams.get(DafConstants.DATAMART_POSTGRE_DATABASE_NAME),
-//			envParams.get(DafConstants.DATAMART_POSTGRE_USER),
-//			envParams.get(DafConstants.DATAMART_POSTGRE_PASSWORD));
-//			connection = PostgreDataSourceConnection.getDataSourceConnection(dataSource);
-			
+
+			// Jdbc3PoolingDataSource dataSource =
+			// PostgreDataSourceConnection.getDataSource(
+			// envParams.get(DafConstants.DATAMART_POSTGRE_SERVER_NAME),
+			// Integer.parseInt(envParams.get(DafConstants.DATAMART_POSTGRE_PORT)),
+			// envParams.get(DafConstants.DATAMART_POSTGRE_DATABASE_NAME),
+			// envParams.get(DafConstants.DATAMART_POSTGRE_USER),
+			// envParams.get(DafConstants.DATAMART_POSTGRE_PASSWORD));
+			// connection = PostgreDataSourceConnection.getDataSourceConnection(dataSource);
+
 			Class.forName(envParams.get(DafConstants.POSTGRE_SQL_DRIVER));
 			String dbUrl = createValidUrlToConnectPostgreSql(envParams.get(DafConstants.DATAMART_POSTGRE_SERVER_NAME),
 					Integer.parseInt(envParams.get(DafConstants.DATAMART_POSTGRE_PORT)),
