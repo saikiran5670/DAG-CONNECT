ceac2858df175fd64f11c91a887df4edb6b96088
Kumari, Archana <archana.kumari@atos.net>
2021-03-15 17:26:36 +0530

    moved relationship core component code from organization to relationship

--- dotnet/corecomponents/net.atos.daf.ct2.relationship/IRelationshipManager.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.relationship/IRelationshipManager.cs
@@ -1,6 +1,14 @@
-﻿namespace net.atos.daf.ct2.relationship
+﻿using net.atos.daf.ct2.relationship.entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace net.atos.daf.ct2.relationship
 {
     internal interface IRelationshipManager
     {
+        Task<Relationship> CreateRelationship(Relationship relationship);
+        Task<Relationship> UpdateRelationship(Relationship relationship);
+        Task<bool> DeleteRelationship(int relationshipId);
+        Task<List<Relationship>> GetRelationship(Relationship filter);
     }
 }
