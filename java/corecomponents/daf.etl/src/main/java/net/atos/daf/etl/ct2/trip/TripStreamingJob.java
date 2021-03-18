3207ac4a37b8850250bfb3a2d8b9bb9c627265c7
Jadhav <kalyan.jadhav@atos.net>
2021-03-18 11:37:11 +0530

    daf.etl postgres connection

--- java/corecomponents/daf.etl/src/main/java/net/atos/daf/etl/ct2/trip/TripStreamingJob.java
+++ java/corecomponents/daf.etl/src/main/java/net/atos/daf/etl/ct2/trip/TripStreamingJob.java
@@ -1,5 +1,9 @@
 package net.atos.daf.etl.ct2.trip;
 
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple9;
@@ -7,7 +11,13 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,12 +25,14 @@ import net.atos.daf.common.ct2.audittrail.TripAuditTrail;
 import net.atos.daf.common.ct2.utc.TimeFormatter;
 import net.atos.daf.ct2.pojo.KafkaRecord;
 import net.atos.daf.ct2.pojo.standard.Status;
-import net.atos.daf.etl.ct2.common.bo.Trip;
+import net.atos.daf.ct2.serde.KafkaMessageSerializeSchema;
+//import net.atos.daf.etl.ct2.common.bo.Trip;
 import net.atos.daf.etl.ct2.common.bo.TripStatusData;
 import net.atos.daf.etl.ct2.common.kafka.FlinkKafkaStatusMsgConsumer;
 import net.atos.daf.etl.ct2.common.postgre.TripSink;
 import net.atos.daf.etl.ct2.common.util.ETLConstants;
 import net.atos.daf.etl.ct2.common.util.FlinkUtil;
+import net.atos.daf.postgre.bo.Trip;
 
 public class TripStreamingJob {
 	private static Logger logger = LoggerFactory.getLogger(TripStreamingJob.class);
@@ -37,7 +49,7 @@ public class TripStreamingJob {
 			final StreamExecutionEnvironment env = FlinkUtil.createStreamExecutionEnvironment(envParams);
 			env.getConfig().setGlobalJobParameters(envParams);
 			final StreamTableEnvironment tableEnv = FlinkUtil.createStreamTableEnvironment(env);
-			//TripStreamingJob tripStreamingJob = new TripStreamingJob();
+			TripStreamingJob tripStreamingJob = new TripStreamingJob();
 			TripAggregations tripAggregation = new TripAggregations();
 
 			// Map to status data
@@ -70,7 +82,17 @@ public class TripStreamingJob {
 
 			// TODO read master data
 			finalTripData.addSink(new TripSink());
+			
+			if ("true".equals(envParams.get(ETLConstants.EGRESS_TRIP_AGGR_DATA))){
+				System.out.println("Inside egress trip aggregated data :: ");
+				tripStreamingJob.egressTripData(getSinkProperties(envParams), finalTripData,
+						envParams.get(ETLConstants.EGRESS_TRIP_AGGR_TOPIC_NAME),
+						Long.valueOf(envParams.get(ETLConstants.TRIP_TIME_WINDOW_MILLISEC)));
+			}
+			/*.writeAsText("/home/flink-vm0-user1/winfunTest.txt", FileSystem.WriteMode.OVERWRITE)
+			.name("writeWinfunTestToFile");*/
 
+	
 			env.execute("Trip Streaming Job");
 
 		} catch (Exception e) {
@@ -83,6 +105,55 @@ public class TripStreamingJob {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public void egressTripData(Properties properties, DataStream<Trip> finalTripData, String sinkTopicNm,
+			long timeInMilli) {
+		try {
+			finalTripData.keyBy(rec -> rec.getTripId())
+					.window(TumblingProcessingTimeWindows.of(Time.milliseconds(timeInMilli)))
+					.process(new ProcessWindowFunction<Trip, KafkaRecord<String>, String, TimeWindow>() {
+						private static final long serialVersionUID = 1L;
+
+						@Override
+						public void process(String key, Context ctx, Iterable<Trip> values,
+								Collector<KafkaRecord<String>> out) throws Exception {
+							Map<String, Trip> tripMap = new ConcurrentHashMap<>();
+							for (Trip in : values) {
+								in.setHbaseInsertionTS(TimeFormatter.getInstance().getCurrentUTCTime());
+								tripMap.put(in.getTripId(), in);								
+							}
+
+							for (Map.Entry<String, Trip> entry : tripMap.entrySet()) {
+								System.out.println(" final trip record  for egress :: " + entry.getValue());
+								KafkaRecord<String> kafkaRec = new KafkaRecord<>();
+								kafkaRec.setKey(entry.getValue().getTripId());
+								kafkaRec.setValue(entry.getValue().toString());
+								out.collect(kafkaRec);
+							}
+						}
+					})
+					.addSink(new FlinkKafkaProducer<KafkaRecord<String>>(sinkTopicNm,
+							new KafkaMessageSerializeSchema<String>(sinkTopicNm), properties,
+							FlinkKafkaProducer.Semantic.AT_LEAST_ONCE));
+
+		} catch (Exception e) {
+			logger.error("Issue while egress trip aggregated data :: " + e);
+		}
+	}
+	
+	public static Properties getSinkProperties(ParameterTool envParams){
+		Properties properties = new Properties();
+		//properties.setProperty(ETLConstants.CLIENT_ID, envParams.get(ETLConstants.CLIENT_ID));
+		//properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, envParams.get(ETLConstants.AUTO_OFFSET_RESET_CONFIG));
+		//properties.setProperty(ETLConstants.GROUP_ID, envParams.get(ETLConstants.GROUP_ID));
+		properties.setProperty(ETLConstants.BOOTSTRAP_SERVERS, envParams.get(ETLConstants.EVENT_HUB_BOOTSTRAP));
+		properties.setProperty(ETLConstants.SECURITY_PROTOCOL, envParams.get(ETLConstants.SECURITY_PROTOCOL));
+		properties.setProperty(ETLConstants.SASL_MECHANISM, envParams.get(ETLConstants.SASL_MECHANISM));
+		properties.setProperty(ETLConstants.SASL_JAAS_CONFIG, envParams.get(ETLConstants.SASL_JAAS_CONFIG));
+		//properties.setProperty(ETLConstants.REQUEST_TIMEOUT_MILLISEC, envParams.get(ETLConstants.REQUEST_TIMEOUT_MILLISEC));
+		
+		return properties;
 	}
 	
 	public static TripStatusData fetchStatusData(Status stsMsg)
