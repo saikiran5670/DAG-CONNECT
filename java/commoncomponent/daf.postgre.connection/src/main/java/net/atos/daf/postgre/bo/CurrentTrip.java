ad5539fe1b85d0bcbcc5860e5bc61a749e50e3aa
Jadhav <kalyan.jadhav@atos.net>
2021-03-18 10:58:08 +0530

    Postgres common connection

---
+++ java/commoncomponent/daf.postgre.connection/src/main/java/net/atos/daf/postgre/bo/CurrentTrip.java
@@ -0,0 +1,25 @@
+package net.atos.daf.postgre.bo;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrentTrip implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private long trip_Start_time;
+	private long start_time_stamp ;
+	private long end_time_stamp;
+	private double start_position_lattitude;
+	private double start_position_longitude;
+	
+	
+}
