259cc1a21b51431fd4c2a403ece0ed223f210757
Gaurav Gadewar <gaurav.ratnakargadewar@atos.net>
2021-03-20 12:15:31 +0530

    Added org relationship method in TCU

--- dotnet/businessservices/net.atos.daf.ct2.tcuprovisioningvehicle/TCUProvisioning/Program.cs
+++ dotnet/businessservices/net.atos.daf.ct2.tcuprovisioningvehicle/TCUProvisioning/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Reflection;
 using log4net;
 using log4net.Config;
+using Microsoft.Extensions.Configuration;
 
 namespace TCUProvisioning
 {
@@ -18,7 +19,9 @@ namespace TCUProvisioning
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
 
-            ProvisionVehicle provisionVehicle = new ProvisionVehicle(log);
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+
+            ProvisionVehicle provisionVehicle = new ProvisionVehicle(log,config);
             await provisionVehicle.readTCUProvisioningData();
 
         }
