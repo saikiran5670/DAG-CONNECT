38850934aed5305ac8a67a65c0e4796ae730c568
Manoj Nandamuri <manoj.nandamuri@atos.net>
2021-03-22 15:31:08 +0530

    Modified the Properties in Organizationservice and component to fetch  data from DB

--- dotnet/corecomponents/net.atos.daf.ct2.organization/entity/OrganizationNameandType.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.organization/entity/OrganizationNameandType.cs
@@ -3,7 +3,7 @@ namespace net.atos.daf.ct2.organization.entity
 {
    public class OrganizationNameandID
     {
-        public int organizationId { get; set; }
-        public string OrganizationName { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
     }
 }
