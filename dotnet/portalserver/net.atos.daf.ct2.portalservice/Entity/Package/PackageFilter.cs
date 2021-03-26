e47af9b924494ad536ce1b7d0ff83102652556a9
Kumari, Archana <archana.kumari@atos.net>
2021-03-26 18:23:11 +0530

    Package Bug- 3377 Removed name filter as it was not required

--- dotnet/portalserver/net.atos.daf.ct2.portalservice/Entity/Package/PackageFilter.cs
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Entity/Package/PackageFilter.cs
@@ -4,8 +4,7 @@ namespace net.atos.daf.ct2.portalservice.Entity.Package
     public class PackageFilter
     {
         public int Id { get; set; }
-        public string Code { get; set; }
-        public string Name { get; set; }
+        public string Code { get; set; }      
         public string Type { get; set; }
         public int FeatureSetId { get; set; }      
         public string Status { get; set; }
