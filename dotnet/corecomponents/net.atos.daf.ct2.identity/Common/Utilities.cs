853b0771a50457172cfe6a11c70eb0bbb377d3f5
harneet rekhi <harneet.rekhi@atos.net>
2021-03-27 16:52:11 +0530

    Change password validation in identity component Model Validation or 400 and 422 in the startup

---
+++ dotnet/corecomponents/net.atos.daf.ct2.identity/Common/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace net.atos.daf.ct2.identity.Common
+{
+    public static class IdentityUtilities
+    {
+        public static bool ValidationByRegex(Regex regEx, string valueToTest)
+        {
+            return regEx.IsMatch(valueToTest);
+        }
+    }
+}
