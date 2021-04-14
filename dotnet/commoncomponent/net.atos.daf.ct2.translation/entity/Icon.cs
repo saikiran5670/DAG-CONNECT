77d482cbd03a546cacc6944bd339c0a5a4c88b11
snehal bante <snehal.bante@atos.net>
2021-04-14 13:19:25 +0530

    DTC warning

---
+++ dotnet/commoncomponent/net.atos.daf.ct2.translation/entity/Icon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.atos.daf.ct2.translation.entity
+{
+    public class Icon
+    {
+        public int id { get; set; }
+        public byte icon { get; set; }
+        public char type { get; set; }
+        public int warning_class { get; set; }
+        public int number { get; set; }
+        public string name { get; set; }
+        public char color_name { get; set; }
+        public char state { get; set; }
+        public long created_at { get; set; }
+        public int created_by { get; set; }
+        public long modify_at { get; set; }
+        public int modify_by { get; set; }
+        
+    }
+}
