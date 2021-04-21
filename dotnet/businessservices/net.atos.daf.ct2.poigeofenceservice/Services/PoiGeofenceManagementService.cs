642e2f900275bb32a7047b0a003a3cc1c5a52308
Kumari, Archana <archana.kumari@atos.net>
2021-04-21 16:47:58 +0530

    Project create for poigeofence business service ,corecomponent and test

---
+++ dotnet/businessservices/net.atos.daf.ct2.poigeofenceservice/Services/PoiGeofenceManagementService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using log4net;
+using net.atos.daf.ct2.poigeofence;
+using net.atos.daf.ct2.poigeofenceservice;
+
+namespace net.atos.daf.ct2.poigeofenceservice
+{
+    public class PoiGeofenceManagementService:PoiGeofenceService.PoiGeofenceServiceBase
+    {
+        private ILog _logger;
+        private readonly IPoiManager _poiManager ;
+        public PoiGeofenceManagementService(IPoiManager poiManager)
+        {
+            _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            _poiManager = poiManager;
+        }
+    }
+}
---
+++ dotnet/corecomponents/test/net.atos.daf.ct2.poigeofence.test/PoiManager.Test.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace net.atos.daf.ct2.poigeofence.test
+{
+    [TestClass]
+    public class PoiManagerTest
+    {
+        [TestMethod]
+        public void CreatePoi()
+        {
+        }
+    }
+}
