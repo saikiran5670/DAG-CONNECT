cf93752b32da509b1fedeb108c039065b3510574
Manoj Nandamuri <manoj.nandamuri@atos.net>
2021-04-22 17:39:14 +0530

    4302 : List of DAF/Global POIs

--- dotnet/corecomponents/net.atos.daf.ct2.poigeofence/repository/IPoiRepository.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.poigeofence/repository/IPoiRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace net.atos.daf.ct2.poigeofence.repository
+﻿using net.atos.daf.ct2.poigeofence.entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace net.atos.daf.ct2.poigeofence.repository
 {
     public interface IPoiRepository
     {
+        Task<List<POIEntityResponce>> GetAllPOI(POIEntityRequest objPOIEntityRequest);
     }
 }
