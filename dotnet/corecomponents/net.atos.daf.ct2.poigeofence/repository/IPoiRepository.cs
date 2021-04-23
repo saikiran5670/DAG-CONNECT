8dc5c196921fee9394e172107b8645ab188c29f5
Saurabh.Komawar@sthlvblvd.onmicrosoft.com <saurabh.komawar@atos.net>
2021-04-23 12:30:41 +0530

    POI and Geofence separate

--- dotnet/corecomponents/net.atos.daf.ct2.poigeofence/repository/IPoiRepository.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.poigeofence/repository/IPoiRepository.cs
@@ -7,9 +7,6 @@ namespace net.atos.daf.ct2.poigeofence.repository
     public interface IPoiRepository
     {
         Task<List<POIEntityResponce>> GetAllPOI(POIEntityRequest objPOIEntityRequest);
-        Task<bool> DeleteGeofence(List<int> geofenceIds, int organizationID);
-        Task<Geofence> CreateGeofence(Geofence geofence);
-
         Task<List<POI>> GetAllPOI(POI poi);
         Task<POI> CreatePOI(POI poi);
         Task<bool> UpdatePOI(POI poi);
--- dotnet/corecomponents/net.atos.daf.ct2.poigeofence/repository/PoiRepository.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.poigeofence/repository/PoiRepository.cs
@@ -63,36 +63,6 @@ namespace net.atos.daf.ct2.poigeofence.repository
             return null;
         }
 
-        public async Task<bool> DeleteGeofence(List<int> geofenceIds, int organizationID)
-        {
-            log.Info("Delete geofenceIds method called in repository");
-            try
-            {
-                if (geofenceIds.Count > 0)
-                {
-                    foreach (var item in geofenceIds)
-                    {
-                        var parameter = new DynamicParameters();
-                        parameter.Add("@organization_id", organizationID);
-                        parameter.Add("@id", item);
-                        var queryLandmark = @"update master.landmark set state='D' where id=@id and organization_id=@organization_id";
-                        await _dataAccess.ExecuteScalarAsync<int>(queryLandmark, parameter);
-
-                        var queryNodes = @"update master.nodes set state='D' where landmark_id=@id";
-                        await _dataAccess.ExecuteScalarAsync<int>(queryNodes, parameter);
-                    }   
-                }
-                return true;
-            }
-            catch (System.Exception ex)
-            {
-                log.Info("Delete geofenceIds method in repository failed :");
-                log.Error(ex.ToString());
-                throw ex;
-            }           
-        }
-
-
         public async Task<POI> CreatePOI(POI poi)
         {
             try
@@ -128,76 +98,6 @@ namespace net.atos.daf.ct2.poigeofence.repository
             return poi;
         }
 
-        #region Geofence
-
-        public async Task<Geofence> CreateGeofence(Geofence geofence)
-        {
-            try
-            {
-                var parameter = new DynamicParameters();
-                parameter.Add("@organization_id", geofence.Id);
-                parameter.Add("@category_id", geofence.CategoryId);
-                parameter.Add("@sub_category_id", geofence.SubCategoryId);
-                parameter.Add("@name", geofence.Name);
-                parameter.Add("@address", geofence.Address);
-                parameter.Add("@city", geofence.City);
-                parameter.Add("@country", geofence.Country);
-                parameter.Add("@zipcode", geofence.Zipcode);
-                parameter.Add("@type", geofence.Type);
-                parameter.Add("@latitude", geofence.Latitude);
-                parameter.Add("@longitude", geofence.Longitude);
-                parameter.Add("@distance", geofence.Distance);
-                parameter.Add("@trip_id", geofence.TripId);
-                parameter.Add("@state", 'A');
-                parameter.Add("@created_at", UTCHandling.GetUTCFromDateTime(DateTime.Now.ToString()));
-                parameter.Add("@created_by", geofence.CreatedBy);
-
-                string query = @"Update master.landmark
-                                SET 	organization_id=@organization_id,
-		                                category_id=@category_id,
-		                                sub_category_id=@sub_category_id, 
-		                                name=@name, 
-		                                address=@address,
-		                                city=@city, 
-		                                country=@country, 
-		                                zipcode=@zipcode, 
-		                                [type]=@type, 
-		                                latitude=@latitude, 
-		                                longitude=@longitude, 
-		                                distance@distance, 
-		                                trip_id=@trip_id, 
-		                                state=@state
-		                                ) RETURNING id;";
-
-                var id = await dataAccess.ExecuteScalarAsync<int>(query, parameter);
-                geofence.Id = id;
-                if (geofence.Type.ToString() == LandmarkType.PolygonGeofence.ToString())
-                {
-                    if (geofence.Id > 0)
-                    {
-                        foreach (var item in geofence.Nodes)
-                        {
-                            var nodeparameter = new DynamicParameters();
-                            nodeparameter.Add("@landmark_id", item.LandmarkId);
-                            nodeparameter.Add("@seq_no", item.SeqNo);
-                            nodeparameter.Add("@latitude", item.Latitude);
-                            nodeparameter.Add("@longitude", item.Longitude);
-                            nodeparameter.Add("@state", "A");
-                            nodeparameter.Add("@created_at", UTCHandling.GetUTCFromDateTime(DateTime.Now.ToString()));
-                            nodeparameter.Add("@created_by", item.CreatedBy);
-                            string nodeQuery = @"INSERT INTO master.nodes(landmark_id, seq_no, latitude, longitude, state, created_at, created_by)
-	                                VALUES (@landmark_id, @seq_no, @latitude, @longitude, @state, @created_at, @created_by) RETURNING id";
-                            var nodeId = await dataAccess.ExecuteScalarAsync<int>(nodeQuery, nodeparameter);
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-            return geofence;
-        }
 
         public Task<List<POI>> GetAllPOI(POI poi)
         {
@@ -260,6 +160,6 @@ namespace net.atos.daf.ct2.poigeofence.repository
             throw new NotImplementedException();
         }
 
-        #endregion
+     
     }
 }
