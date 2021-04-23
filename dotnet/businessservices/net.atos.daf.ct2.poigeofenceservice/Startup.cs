13c0c8322c155a39db5022362be577d8117e3085
Shailesh Rajput <shailesh-nathusing.rajput@atos.net>
2021-04-23 20:44:25 +0530

    configure business service enpoints for poi

--- dotnet/businessservices/net.atos.daf.ct2.poigeofenceservice/Startup.cs
+++ dotnet/businessservices/net.atos.daf.ct2.poigeofenceservice/Startup.cs
@@ -55,10 +55,11 @@ namespace net.atos.daf.ct2.poigeofenceservice
 
             app.UseEndpoints(endpoints =>
             {
-                
+
                 //endpoints.MapGrpcService<PoiGeofenceManagementService>().EnableGrpcWeb()
                 //                                  .RequireCors("AllowAll");
-
+                endpoints.MapGrpcService<POIManagementService>().EnableGrpcWeb()
+                                                  .RequireCors("AllowAll");
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
