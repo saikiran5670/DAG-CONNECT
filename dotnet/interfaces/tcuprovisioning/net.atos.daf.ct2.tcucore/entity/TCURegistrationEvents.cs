299c7796ee565ea388202166f7150d1f38fd8841
Smita Bhowad <smita.bhowad@atos.net>
2021-04-24 13:26:55 +0530

    tcu code added

---
+++ dotnet/interfaces/tcuprovisioning/net.atos.daf.ct2.tcucore/entity/TCURegistrationEvents.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace net.atos.daf.ct2.tcucore
+{
+    public class TCURegistrationEvents
+    {
+        private List<TCURegistrationEvent> tCURegistrationEvent;
+
+        public TCURegistrationEvents(List<TCURegistrationEvent> _tcuRegistrationEvent)
+        {
+            this.tCURegistrationEvent = _tcuRegistrationEvent;
+        }
+
+        public List<TCURegistrationEvent> TCURegistrationEvent 
+        { 
+            get => tCURegistrationEvent; 
+            set => tCURegistrationEvent = value; 
+        }
+    }
+}
