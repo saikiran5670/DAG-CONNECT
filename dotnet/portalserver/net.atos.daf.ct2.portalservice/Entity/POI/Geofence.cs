d88c21a0c94849a16d9985c7faff863ed79334fd
Ram Pratap <ram.pratap@atos.net>
2021-04-26 14:09:21 +0530

    Add new method for get all geofence in controller

---
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Entity/POI/Geofence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.atos.daf.ct2.poigeofence.entity
+{
+    public class Geofence
+    {
+        public int Id { get; set; }
+        public int OrganizationId { get; set; }
+        public int CategoryId { get; set; }
+        public int SubCategoryId { get; set; }
+        public string Name { get; set; }
+        public string CategoryName { get; set; }
+        public string SubCategoryName { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Zipcode { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Distance { get; set; }
+        public string State { get; set; }
+        public int TripId { get; set; }
+        public long CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public long ModifiedAt { get; set; }
+        public int ModifiedBy { get; set; }
+       
+    }
+}
