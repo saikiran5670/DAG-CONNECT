fb90c2a78f2da6ef1ad7f1b628b51b761880aa1d
namitapatil0502 <namita.patil@atos.net>
2021-04-30 12:01:47 +0530

    Category filter changes in landmark screens

--- ui/src/app/configuration/landmarks/manage-category/create-edit-view-category/create-edit-view-category.component.ts
+++ ui/src/app/configuration/landmarks/manage-category/create-edit-view-category/create-edit-view-category.component.ts
@@ -86,7 +86,7 @@ export class CreateEditViewCategoryComponent implements OnInit {
     this.imageMaxMsg = false;
     this.imageEmptyMsg = false;
     this.categoryForm.get('categoryName').setValue(this.selectedRowData.parentCategoryName);
-    this.categoryForm.get('type').setValue(this.selectedRowData.Organization_Id ? (this.selectedRowData.Organization_Id  > 0 ? 'Regular': 'Global' ) : 'Global');
+    this.categoryForm.get('type').setValue(this.selectedRowData.organizationId ? (this.selectedRowData.organizationId  > 0 ? 'Regular': 'Global' ) : 'Global');
     this.categoryForm.get('categoryDescription').setValue(this.selectedRowData.description);
     this.categoryForm.get('categoryType').setValue(this.selectedCategoryType);
     this.categoryForm.get('parentCategory').setValue(this.selectedRowData.parentCategoryId);
