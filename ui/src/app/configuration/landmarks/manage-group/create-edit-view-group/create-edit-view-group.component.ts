8e9f53f067bfb540c203b55c6db185b934576860
namitapatil0502 <namita.patil@atos.net>
2021-05-03 18:29:27 +0530

    Manage group : Bug#5567 resolved

--- ui/src/app/configuration/landmarks/manage-group/create-edit-view-group/create-edit-view-group.component.ts
+++ ui/src/app/configuration/landmarks/manage-group/create-edit-view-group/create-edit-view-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -28,10 +28,8 @@ export class CreateEditViewGroupComponent implements OnInit {
   selectedGeofence = new SelectionModel(true, []);
   poiDataSource: any = new MatTableDataSource([]);
   geofenceDataSource: any = new MatTableDataSource([]);
-  @ViewChild(MatPaginator) poiPaginator: MatPaginator;
-  @ViewChild(MatPaginator) geofencePaginator: MatPaginator;
-  @ViewChild(MatSort) poiSort: MatSort;
-  @ViewChild(MatSort) geofenceSort: MatSort;
+  @ViewChildren(MatPaginator) paginator = new QueryList<MatPaginator>();
+  @ViewChildren(MatSort) sort = new QueryList<MatSort>();
   @Input() translationData: any;
   @Input() selectedRowData: any;
   @Input() actionType: any;
@@ -204,16 +202,16 @@ export class CreateEditViewGroupComponent implements OnInit {
   updatePOIDataSource(tableData: any){
     this.poiDataSource= new MatTableDataSource(tableData);
     setTimeout(()=>{
-      this.poiDataSource.paginator = this.poiPaginator;
-      this.poiDataSource.sort = this.poiSort;
+      this.poiDataSource.paginator = this.paginator.toArray()[0];
+      this.poiDataSource.sort = this.sort.toArray()[0];
     });
   }
 
   updateGeofenceDataSource(tableData: any){
     this.geofenceDataSource = new MatTableDataSource(tableData);
     setTimeout(()=>{
-      this.geofenceDataSource.paginator = this.geofencePaginator;
-      this.geofenceDataSource.sort = this.geofenceSort;
+      this.geofenceDataSource.paginator = this.paginator.toArray()[1];
+      this.geofenceDataSource.sort = this.sort.toArray()[1];
     });
   }
 
