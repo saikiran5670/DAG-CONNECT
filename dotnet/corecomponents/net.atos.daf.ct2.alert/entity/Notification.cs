49618c1b0a5042b01507310dc70536ccf62c64d7
Shailesh Rajput <shailesh-nathusing.rajput@atos.net>
2021-05-04 22:28:36 +0530

    Added Notifcation related classes in core entity and alert.proto

--- dotnet/corecomponents/net.atos.daf.ct2.alert/entity/Notification.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.alert/entity/Notification.cs
@@ -17,5 +17,8 @@ namespace net.atos.daf.ct2.alert.entity
         public int CreatedBy { get; set; }
         public DateTime ModifiedAt { get; set; }
         public int ModifiedBy { get; set; }
+        public List<NotificationRecipient> NotificationRecipients { get; set; }
+        public List<NotificationLimit> NotificationLimits { get; set; }
+        public List<NotificationAvailabilityPeriod> NotificationAvailabilityPeriods { get; set; }
     }
 }
