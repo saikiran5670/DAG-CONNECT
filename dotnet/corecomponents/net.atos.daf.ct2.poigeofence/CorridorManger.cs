8321a85d6cebc0b4e83ac174704194b8f416748b
snehal bante <snehal.bante@atos.net>
2021-05-04 13:22:30 +0530

    Manage Corridor

---
+++ dotnet/corecomponents/net.atos.daf.ct2.poigeofence/CorridorManger.cs
@@ -0,0 +1,17 @@
+﻿using net.atos.daf.ct2.poigeofence.repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.atos.daf.ct2.poigeofence
+{
+    public class CorridorManger : ICorridorManger
+    {
+        private readonly ICorridorRepository _corridorRepository;
+        public CorridorManger(ICorridorRepository corridorRepository)
+        {
+
+            _corridorRepository = corridorRepository;
+        }
+    }
+}
---
+++ dotnet/corecomponents/net.atos.daf.ct2.poigeofence/repository/ICorridorRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.atos.daf.ct2.poigeofence.repository
+{
+    public interface ICorridorRepository
+    {
+
+    }
+}
