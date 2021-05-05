942d234a84cabd09ec62072d47e083c4ea1bb782
Manoj Nandamuri <manoj.nandamuri@atos.net>
2021-05-05 12:41:20 +0530

    commiting GetCorridorList  DB Query, Model and dapper logic for repository

--- dotnet/corecomponents/net.atos.daf.ct2.poigeofence/CorridorManger.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.poigeofence/CorridorManger.cs
@@ -1,7 +1,9 @@
-﻿using net.atos.daf.ct2.poigeofence.repository;
-using System;
+﻿using net.atos.daf.ct2.poigeofence.entity;
+using net.atos.daf.ct2.poigeofence.repository;
+//using net.atos.daf.ct2.poigeofence.entity;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace net.atos.daf.ct2.poigeofence
 {
@@ -10,8 +12,11 @@ namespace net.atos.daf.ct2.poigeofence
         private readonly ICorridorRepository _corridorRepository;
         public CorridorManger(ICorridorRepository corridorRepository)
         {
-
             _corridorRepository = corridorRepository;
+        }
+        public async Task<List<CorridorResponse>> GetCorridorList(CorridorRequest objCorridorRequest)
+        {
+            return await _corridorRepository.GetCorridorList(objCorridorRequest);
         }
     }
 }
--- dotnet/corecomponents/net.atos.daf.ct2.poigeofence/entity/CorridorRequestResponse.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.poigeofence/entity/CorridorRequestResponse.cs
@@ -11,21 +11,15 @@ namespace net.atos.daf.ct2.poigeofence.entity
         public double StartLat { get; set; }
         public double StartLong { get; set; }
         public string EndPoint { get; set; }
-        public int Parent_Id { get; set; }
         public double EndLat { get; set; }
         public double EndLong { get; set; }
         public double Distance { get; set; }
         public double Width { get; set; }
-        public long Created_At { get; set; }
-        public int Created_By { get; set; }
-        public long Modified_At { get; set; }
-        public int Modified_By { get; set; }
+        public long CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public long ModifiedAt { get; set; }
+        public int ModifiedBy { get; set; }
         public string Description { get; set; }
-    }
-
-    public class CorridorListResponse
-    {
-        public List<CorridorResponse> CorridorList { get; set; }
     }
 
    public  class CorridorRequest
