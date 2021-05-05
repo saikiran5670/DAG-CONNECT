e9ef16e1555288711d05f7f47930464709c6d1b5
Manoj Nandamuri <manoj.nandamuri@atos.net>
2021-05-06 05:28:36 +0530

    Modified Payload for GetCorridor details for GridView and EditView

--- dotnet/corecomponents/net.atos.daf.ct2.poigeofence/repository/CorridorRepository.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.poigeofence/repository/CorridorRepository.cs
@@ -25,12 +25,12 @@ namespace net.atos.daf.ct2.poigeofence.repository
 
         }
 
-        public async Task<List<CorridorResponse>> GetCorridorList(CorridorRequest objCorridorRequest)
+        public async Task<List<CorridorResponse>> GetCorridorListByOrganization(CorridorRequest objCorridorRequest)
         {
             List<CorridorResponse> objCorridorResponseList = new List<CorridorResponse>();
             try
             {
-                string query = string.Empty;
+                string query = string.Empty; var parameter = new DynamicParameters();
                 query = @"select l.id 
                                 ,l.organization_id as OrganizationId
 	                            ,l.name as CorridoreName
@@ -42,26 +42,111 @@ namespace net.atos.daf.ct2.poigeofence.repository
 	                            ,n.longitude as EndLong
 	                            ,l.distance as Distance
 	                            ,l.distance as Width
+                                ,l.state as State
+                                ,l.type as CorridorType
 	                            ,l.created_at as Created_At
 	                            ,l.created_by as CreatedBy
 	                            ,l.modified_at as ModifiedAt
 	                            ,l.modified_by as ModifiedBy
                         FROM       master.landmark l
-                        INNER JOIN master.nodes n on l.id= n.landmark_id
+                        LEFT JOIN master.nodes n on l.id = n.landmark_id
                         WHERE      l.type IN ('E','R')
                         AND        l.organization_id = @organization_id";
 
-                var parameter = new DynamicParameters();
                 parameter.Add("@organization_id", objCorridorRequest.OrganizationId);
-               
-                if (objCorridorRequest.CorridorId > 0)
-                {
-                    parameter.Add("@id", objCorridorRequest.CorridorId);
-                    query = $"{query} AND l.id = @id";
-                }
-
                 var data = await _dataAccess.QueryAsync<CorridorResponse>(query, parameter);
                 return objCorridorResponseList = data.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<List<CorridorEditViewResponse>> GetCorridorListByOrgIdAndCorriId(CorridorRequest objCorridorRequest)
+        {
+            List<CorridorEditViewResponse> objCorridorEditViewResponse1 = new List<CorridorEditViewResponse>();
+            try
+            {
+                string query = string.Empty; var parameter = new DynamicParameters();
+                query = @"select l.id 
+                                ,l.organization_id as OrganizationId
+	                            ,l.name as CorridoreName
+	                            ,l.address as StartPoint
+	                            ,l.latitude as StartLat
+	                            ,l.longitude as StartLong
+	                            ,n.address as EndPoint
+	                            ,n.latitude as EndLat
+	                            ,n.longitude as EndLong
+	                            ,l.distance as Distance
+	                            ,l.distance as Width
+	                            ,l.created_at as CreatedAt
+	                            ,l.created_by as CreatedBy
+	                            ,l.modified_at as ModifiedAt
+	                            ,l.modified_by as ModifiedBy
+								,cp.id as CorridorPropertiesId
+								, cp.landmark_id
+								, cp.is_transport_data as IsTransportData
+								, cp.is_traffic_flow as IsTrafficFlow
+								, cp.no_of_trailers as NoOfTrailers
+								, cp.is_explosive as IsExplosive
+								, cp.is_gas as IsGas
+								, cp.is_flammable as IsFlammable
+								, cp.is_combustible as IsCombustible
+								, cp.is_organic as IsOrganic
+								, cp.is_poison as IsPoision
+								, cp.is_radio_active as IsRadioActive
+								, cp.is_corrosive as IsCorrosive
+								, cp.is_poisonous_inhalation as IsPoisonousInhalation
+								, cp.is_warm_harm as IsWaterHarm
+								, cp.is_other as IsOther
+								, cp.toll_road_type as TollRoadType
+								, cp.motorway_type as Mortorway
+								, cp.boat_ferries_type as BoatFerriesType
+								, cp.rail_ferries_type as RailFerriesType
+								, cp.tunnels_type as TunnelsType
+								, cp.dirt_road_type as DirtRoadType
+								, cp.vehicle_height as VehicleHeight
+								, cp.vehicle_width as vehicleWidth
+								, cp.vehicle_length as vehicleLength
+								, cp.vehicle_limited_weight as vehicleLimitedWeight
+								, cp.vehicle_weight_per_axle as vehicleWeightPerAxle
+								, cp.created_at as CreatedAtForCP
+								, cp.modified_at as ModifiedAtForCP
+                        FROM       master.landmark l
+                        LEFT JOIN master.nodes n on l.id = n.landmark_id
+						LEFT JOIN master.corridorproperties cp on l.id = cp.landmark_id
+                        WHERE      l.type IN ('E','R')
+                        AND        l.organization_id = @organization_id
+                        AND        l.id = @id";
+
+                parameter.Add("@organization_id", objCorridorRequest.OrganizationId);
+                parameter.Add("@id", objCorridorRequest.CorridorId);
+                var data = await _dataAccess.QueryAsync<CorridorEditViewResponse>(query, parameter);
+                return objCorridorEditViewResponse1 = data.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<List<ViaAddressDetail>> GetCorridorViaStopById(int Id)
+        {
+            List<ViaAddressDetail> objViaAddressDetailList = new List<ViaAddressDetail>();
+            try
+            {
+                string query = string.Empty; var parameter = new DynamicParameters();
+                query = @"select id as CorridorViaStopId
+                                 ,name as CorridorViaStopName
+                                 ,latitude
+                                 ,longitude
+                          FROM MASTER.CORRIDORVIASTOP WHERE
+                          landmark_id=@landmark_id";
+
+                parameter.Add("@landmark_id", Id);
+                var data = await _dataAccess.QueryAsync<ViaAddressDetail>(query, parameter);
+                return objViaAddressDetailList = data.ToList();
             }
             catch (Exception ex)
             {
