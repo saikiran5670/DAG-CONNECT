99f70282805ed755d9c6f4d79ea78dfdfeaff9de
Chaitali <chaitali.chamale@atos.net>
2021-05-10 01:42:03 -0700

    display poi on map changes added in alert

--- ui/src/app/configuration/alerts/create-edit-view-alerts/create-edit-view-alerts.component.ts
+++ ui/src/app/configuration/alerts/create-edit-view-alerts/create-edit-view-alerts.component.ts
@@ -59,6 +59,7 @@ export class CreateEditViewAlertsComponent implements OnInit {
   isDuplicateAlert: boolean= false;
   private platform: any;
   map: any;
+  markerArray: any = [];
   alertTypeByCategoryList: any= [];
   vehicleByVehGroupList: any= [];
   alert_category_selected: string= '';
@@ -261,15 +262,43 @@ export class CreateEditViewAlertsComponent implements OnInit {
     var ui = H.ui.UI.createDefault(this.map, defaultLayers);
 }
 
-  checkboxClicked(row) {
-    // console.log(this.selectedGroup.isSelected);
+checkboxClicked(event: any, row: any) {
+  if(event.checked){ //-- add new marker
+    this.markerArray.push(row);
+  }else{ //-- remove existing marker
+    let arr = this.markerArray.filter(item => item.id != row.id);
+    this.markerArray = arr;
+  }
+  this.addMarkerOnMap();
     
-    console.log("checkbox is clicked");
-    console.log(row.latitude);
-    console.log(row.longitude);
-    let marker = new H.map.Marker({lat:row.latitude, lng:row.longitude});
-    this.map.addObject(marker);
+  }
+  
+  addMarkerOnMap(){
+    this.map.removeObjects(this.map.getObjects());
+    this.markerArray.forEach(element => {
+      let marker = new H.map.Marker({ lat: element.latitude, lng: element.longitude }, { icon: this.getSVGIcon() });
+      this.map.addObject(marker);
+      // this.createResizableCircle(this.circularGeofenceFormGroup.controls.radius.value ? parseInt(this.circularGeofenceFormGroup.controls.radius.value) : 0, element);
+    });
+  }
+  
+  getSVGIcon(){
+    let markup = '<svg xmlns="http://www.w3.org/2000/svg" width="28px" height="36px" >' +
+    '<path d="M 19 31 C 19 32.7 16.3 34 13 34 C 9.7 34 7 32.7 7 31 C 7 29.3 9.7 ' +
+    '28 13 28 C 16.3 28 19 29.3 19 31 Z" fill="#000" fill-opacity=".2"></path>' +
+    '<path d="M 13 0 C 9.5 0 6.3 1.3 3.8 3.8 C 1.4 7.8 0 9.4 0 12.8 C 0 16.3 1.4 ' +
+    '19.5 3.8 21.9 L 13 31 L 22.2 21.9 C 24.6 19.5 25.9 16.3 25.9 12.8 C 25.9 9.4 24.6 ' +
+    '6.1 22.1 3.8 C 19.7 1.3 16.5 0 13 0 Z" fill="#fff"></path>' +
+    '<path d="M 13 2.2 C 6 2.2 2.3 7.2 2.1 12.8 C 2.1 16.1 3.1 18.4 5.2 20.5 L ' +
+    '13 28.2 L 20.8 20.5 C 22.9 18.4 23.8 16.2 23.8 12.8 C 23.6 7.07 20 2.2 ' +
+    '13 2.2 Z" fill="${COLOR}"></path><text transform="matrix( 1 0 0 1 13 18 )" x="0" y="0" fill-opacity="1" ' +
+    'fill="#fff" text-anchor="middle" font-weight="bold" font-size="13px" font-family="arial" style="fill:black"></text></svg>';
     
+    let locMarkup = '<svg height="24" version="1.1" width="24" xmlns="http://www.w3.org/2000/svg" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><g transform="translate(0 -1028.4)"><path d="m12 0c-4.4183 2.3685e-15 -8 3.5817-8 8 0 1.421 0.3816 2.75 1.0312 3.906 0.1079 0.192 0.221 0.381 0.3438 0.563l6.625 11.531 6.625-11.531c0.102-0.151 0.19-0.311 0.281-0.469l0.063-0.094c0.649-1.156 1.031-2.485 1.031-3.906 0-4.4183-3.582-8-8-8zm0 4c2.209 0 4 1.7909 4 4 0 2.209-1.791 4-4 4-2.2091 0-4-1.791-4-4 0-2.2091 1.7909-4 4-4z" fill="#55b242" transform="translate(0 1028.4)"/><path d="m12 3c-2.7614 0-5 2.2386-5 5 0 2.761 2.2386 5 5 5 2.761 0 5-2.239 5-5 0-2.7614-2.239-5-5-5zm0 2c1.657 0 3 1.3431 3 3s-1.343 3-3 3-3-1.3431-3-3 1.343-3 3-3z" fill="#ffffff" transform="translate(0 1028.4)"/></g></svg>';
+    
+    //let icon = new H.map.Icon(markup.replace('${COLOR}', '#55b242'));
+    let icon = new H.map.Icon(locMarkup);
+    return icon;
   }
   
   setDefaultValue(){
