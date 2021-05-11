8930d99bc525106ad334e399b56d689e24fe56ff
Hemanshu Waghulde <hemanshu.waghulde@atos.net>
2021-05-11 16:31:05 +0530

    Customer data interface bug fixes

--- dotnet/corecomponents/net.atos.daf.ct2.organization/entity/EndCustomer.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.organization/entity/EndCustomer.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace net.atos.daf.ct2.organization.entity
 {
     public class EndCustomer
     {
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
         public string ID { get; set; } 
         public string Name { get; set; } 
         public Address Address { get; set; } 
--- dotnet/corecomponents/net.atos.daf.ct2.organization/entity/KeyHandOver.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.organization/entity/KeyHandOver.cs
@@ -1,15 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace net.atos.daf.ct2.organization.entity
 { 
-    public class KeyHandOver  {
+    public class KeyHandOver  
+    {
+        [Required]
         public KeyHandOverEvent KeyHandOverEvent { get; set; } 
     }
-    public class KeyHandOverEvent   {
-        public string VIN { get; set; } 
+    public class KeyHandOverEvent   
+    {
+        [Required]
+        [StringLength(17, MinimumLength = 17)]
+        public string VIN { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
         public string TCUID { get; set; } 
-        public EndCustomer EndCustomer { get; set; } 
-        public string TCUActivation { get; set; } 
+        [Required]
+        public EndCustomer EndCustomer { get; set; }
+        [Required]
+        [StringLength(3, MinimumLength = 2)]
+        public string TCUActivation { get; set; }
+        [Required]
         public string ReferenceDateTime { get; set; } 
     }
 }
