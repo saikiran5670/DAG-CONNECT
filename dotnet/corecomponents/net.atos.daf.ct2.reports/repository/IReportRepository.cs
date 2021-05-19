3774c632b4464d5101696a0a025a12cf41313266
harneet rekhi <harneet.rekhi@atos.net>
2021-05-19 14:06:20 +0530

    report chnages for user prefernces

--- dotnet/corecomponents/net.atos.daf.ct2.reports/repository/IReportRepository.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.reports/repository/IReportRepository.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using net.atos.daf.ct2.reports.entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace net.atos.daf.ct2.reports.repository
 {
     public interface IReportRepository
     {
+        //Task<IEnumerable<UserPrefernceReportDataColumn>> GetUserPreferenceReportDataColumn(int reportId, int accountId);
     }
 }
