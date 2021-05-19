d645cdd920af9d68b868ae233b4f8253f816d72d
harneet rekhi <harneet.rekhi@atos.net>
2021-05-19 16:37:58 +0530

    Task 7421: Select user preference for data column per report -Business service

--- dotnet/businessservices/net.atos.daf.ct2.reportservice/entity/Mapper.cs
+++ dotnet/businessservices/net.atos.daf.ct2.reportservice/entity/Mapper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Google.Protobuf.Collections;
+using net.atos.daf.ct2.reports.entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,22 @@ namespace net.atos.daf.ct2.reportservice.entity
 {
     public class Mapper
     {
+        internal RepeatedField<UserPreferenceDataColumn> GetUserPrefences(IEnumerable<UserPrefernceReportDataColumn> userPrefernces)
+        {
+            var userPreferenceResult = new RepeatedField<UserPreferenceDataColumn>();
+            foreach (var userpreference in userPrefernces)
+            {
+                userPreferenceResult.Add(new UserPreferenceDataColumn
+                {
+                    DataAtrributeId = userpreference.DataAtrributeId,
+                    Name = userpreference.Name,
+                    Description = userpreference.Description,
+                    Type = userpreference.Type,
+                    Key = userpreference.Key,
+                    IsExclusive = userpreference.IsExclusive,
+                });
+            }
+            return userPreferenceResult;
+        }
     }
 }
