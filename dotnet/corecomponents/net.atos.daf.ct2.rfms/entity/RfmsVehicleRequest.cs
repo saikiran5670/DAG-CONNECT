96eef1cf5c7ee968f5b277a4dc09c996f7cf42ae
Prajwal Patil <prajwal.patil@atos.net>
2021-05-20 19:06:54 +0530

    Merge branch 'sprint4' of https://dev.azure.com/dafct-dta/daf-ct-dta/_git/atos.daf.ct2.0 into sprint4

---
+++ dotnet/corecomponents/net.atos.daf.ct2.rfms/entity/RfmsVehicleRequest.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace net.atos.daf.ct2.rfms.entity
+{
+    public class RfmsVehicleRequest
+    {
+
+        public string Id { get; set; }
+        public string LastVin { get; set; }
+
+    }
+}
---
+++ dotnet/corecomponents/net.atos.daf.ct2.rfms/repository/RfmsRepository.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using net.atos.daf.ct2.data;
+using Dapper;
+using net.atos.daf.ct2.rfms.responce;
+using net.atos.daf.ct2.rfms.entity;
+using System;
+
+namespace net.atos.daf.ct2.rfms.repository
+{
+    public class RfmsRepository : IRfmsRepository
+    {
+        private readonly IDataAccess  _dataAccess;
+        private readonly IDataMartDataAccess _dataMartDataAccess;
+         private static readonly log4net.ILog _log =
+        log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        public RfmsRepository(IDataAccess dataAccess,IDataMartDataAccess dataMartAccess )
+        {
+            _dataMartDataAccess = dataMartAccess;
+            _dataAccess = dataAccess;
+        }
+
+        
+        public async Task<RfmsVehicles> Get(RfmsVehicleRequest rfmsVehicleRequest)
+        {
+         try
+            {   
+
+             var queryStatement = @"vin
+                                   ,customer_vehicle_name
+                                   ,brand 
+                                   ,type 
+                                   ,model 
+                                   ,production_date 
+                                   ,possible_fuel_type 
+                                   ,emission_level
+                                   ,tell_tale_code 
+                                   ,authorized_paths
+                                   from master.vehicle 
+                                   where 1=1";
+             var parameter = new DynamicParameters();
+
+             //ID filter
+
+             if (rfmsVehicleRequest.Id != null)
+                 {
+                   parameter.Add("@id", "%" + rfmsVehicleRequest.Id + "%");
+                   queryStatement = queryStatement + " and id LIKE @id";
+
+                 }
+            
+                var rfmsVehicles = new RfmsVehicles();               
+                dynamic result = await _dataAccess.QueryAsync<dynamic>(queryStatement, parameter);
+            
+            return rfmsVehicles;
+            
+            }    
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+
+        }
+
+    }
+}
