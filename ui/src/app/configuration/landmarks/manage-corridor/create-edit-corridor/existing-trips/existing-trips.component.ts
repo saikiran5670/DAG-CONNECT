5516bd708dbf2dd5a0fc9779eabae20b930ae2ea
Shubham Gami <shubham.gami@atos.net>
2021-05-21 15:25:29 +0530

    table layot integraded for existing Trips and css changes done for create corridor dropdown

--- ui/src/app/configuration/landmarks/manage-corridor/create-edit-corridor/existing-trips/existing-trips.component.ts
+++ ui/src/app/configuration/landmarks/manage-corridor/create-edit-corridor/existing-trips/existing-trips.component.ts
@@ -6,7 +6,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { SelectionModel } from '@angular/cdk/collections';
 // import { AccountService } from '../../../services/account.service';
 import { CustomValidators } from '../../../../../shared/custom.validators';
-import {NgxMaterialTimepickerComponent, NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import { NgxMaterialTimepickerComponent, NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 
 @Component({
   selector: 'app-existing-trips',
@@ -18,8 +18,8 @@ export class ExistingTripsComponent implements OnInit {
   endDate = new FormControl();
   @Input() ngxTimepicker: NgxMaterialTimepickerComponent;
   @Input() disabled: boolean;
-  @Input()	value: string = '11:00 PM';
-  @Input()	format: number = 12;
+  @Input() value: string = '11:00 PM';
+  @Input() format: number = 12;
 
   // range = new FormGroup({
   //   start: new FormControl(),
@@ -33,6 +33,21 @@ export class ExistingTripsComponent implements OnInit {
   dataSource: any = new MatTableDataSource([]);
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+  displayedColumns = ['All', 'DriverName', 'distance', 'date', 'startPoint', 'endPoint'];
+  createEditStatus = false;
+  accountOrganizationId: any = 0;
+  corridorCreatedMsg: any = '';
+  actionType: string;
+  titleVisible: boolean = false;
+  titleFailVisible: boolean = false;
+  showMap: boolean = false;
+  map: any;
+  initData: any = [];
+  // dataSource: any;
+  markerArray: any = [];
+  showLoadingIndicator: boolean;
+  selectedCorridors = new SelectionModel(true, []);
+
   // @Input() translationData: any;
   // @Input() selectedRowData: any;
   // @Input() actionType: any;
@@ -46,30 +61,19 @@ export class ExistingTripsComponent implements OnInit {
   @Input() vehicleGroupList: any;
   vinList: any = [];
   vinListSelectedValue: any = [];
-  vehicleGroupIdsSet:any = [];
+  vehicleGroupIdsSet: any = [];
+  localStLanguage: any;
 
-  
   constructor(private _formBuilder: FormBuilder) { }
-  
+
   ngOnInit(): void {
     this.vehicleGroupList.forEach(item => {
       this.vehicleGroupIdsSet.push(item.vehicleGroupId);
       this.vehicleGroupIdsSet = [...new Set(this.vehicleGroupIdsSet)];
     });
-    // console.log("----vinList on Selection---",this.vinList)
-    
-    this.translationData = {
-      lblVehicleGroup : '',
-      lblToday : '',
-      lblYesterday : '',
-      lblLastWeek : '',
-      lblLastMonth : '',
-      lblLast3Months : '',
-      lblGroupType: '',
-      lblTimeRanges: '',
-      lblPleasechoosevehicleGroup: ''
-      
-    }
+    this.showLoadingIndicator = true;
+    this.localStLanguage = JSON.parse(localStorage.getItem("language"));
+    this.accountOrganizationId = localStorage.getItem('accountOrganizationId') ? parseInt(localStorage.getItem('accountOrganizationId')) : 0;
     this.OrgId = localStorage.getItem('accountOrganizationId') ? parseInt(localStorage.getItem('accountOrganizationId')) : 0;
     this.existingTripForm = this._formBuilder.group({
       // userGroupName: ['', [Validators.required, CustomValidators.noWhitespaceValidator]],
@@ -77,31 +81,130 @@ export class ExistingTripsComponent implements OnInit {
       vehicle: ['', [Validators.required]],
       // userGroupDescription: ['', [CustomValidators.noWhitespaceValidatorforDesc]]
     },
-    {
-      validator: [
-        // CustomValidators.specialCharValidationForName('userGroupName'),
-        // CustomValidators.specialCharValidationForNameWithoutRequired('userGroupDescription')
-      ]
-    });
+      {
+        validator: [
+          // CustomValidators.specialCharValidationForName('userGroupName'),
+          // CustomValidators.specialCharValidationForNameWithoutRequired('userGroupDescription')
+        ]
+      });
+    this.loadExistingTripData();
   }
-  
+  loadExistingTripData() {
+    this.showLoadingIndicator = true;
+    // this.corridorService.getCorridorList(this.accountOrganizationId).subscribe((data : any) => {
+    //   this.initData = data;
+    //   this.hideloader();
+    //   this.updatedTableData(this.initData);
+    // }, (error) => {
+    //   this.initData = [];
+    //   this.hideloader();
+    //   this.updatedTableData(this.initData);
+    // });
+  }
+  hideloader() {
+    // Setting display of spinner
+    this.showLoadingIndicator = false;
+  }
   vehicleGroupSelection(vehicleGroupValue: any) {
     this.vinList = [];
     // console.log("----vehicleGroupList---",this.vehicleGroupList)
     this.vehicleGroupList.forEach(item => {
       // this.vehicleGroupIdsSet.push(item.vehicleGroupId)
-      if(item.vehicleGroupId == vehicleGroupValue.value){
+      if (item.vehicleGroupId == vehicleGroupValue.value) {
         this.vinList.push(item.vin)
       }
-      // this.vehicleGroupIdsSet = [...new Set(this.vehicleGroupIdsSet)]
-      // console.log("---unique this.vehicleGroupIdsSet---",this.vehicleGroupIdsSet)
     });
-    // console.log("----vinList--",this.vinList)
   }
+
+  masterToggleForCorridor() {
+    this.markerArray = [];
+    if (this.isAllSelectedForCorridor()) {
+      this.selectedCorridors.clear();
+      this.showMap = false;
+    }
+    else {
+      this.dataSource.data.forEach((row) => {
+        this.selectedCorridors.select(row);
+        this.markerArray.push(row);
+      });
+      this.showMap = true;
+    }
+    // this.addPolylineToMap();
+  }
+
+  isAllSelectedForCorridor() {
+    const numSelected = this.selectedCorridors.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  checkboxLabelForCorridor(row?: any): string {
+    if (row)
+      return `${this.isAllSelectedForCorridor() ? 'select' : 'deselect'} all`;
+    else
+      return `${this.selectedCorridors.isSelected(row) ? 'deselect' : 'select'
+        } row`;
+  }
+
+  checkboxClicked(event: any, row: any) {
+    this.showMap = this.selectedCorridors.selected.length > 0 ? true : false;
+    //console.log(this.selectedpois.selected.length)
+    //console.log(row);
+    if (event.checked) { //-- add new marker
+      this.markerArray.push(row);
+    } else { //-- remove existing marker
+      //It will filter out checked points only
+      let arr = this.markerArray.filter(item => item.id != row.id);
+      this.markerArray = arr;
+    }
+    // this.addPolylineToMap();
+  }
+
+  updatedTableData(tableData: any) {
+    tableData = this.getNewTagData(tableData);
+    this.dataSource = new MatTableDataSource(tableData);
+    setTimeout(() => {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+
+  getNewTagData(data: any) {
+    let currentDate = new Date().getTime();
+    if (data.length > 0) {
+      data.forEach(row => {
+        let createdDate = parseInt(row.createdAt);
+        let nextDate = createdDate + 86400000;
+        if (currentDate > createdDate && currentDate < nextDate) {
+          row.newTag = true;
+        }
+        else {
+          row.newTag = false;
+        }
+      });
+      let newTrueData = data.filter(item => item.newTag == true);
+      newTrueData.sort((userobj1, userobj2) => parseInt(userobj2.createdAt) - parseInt(userobj1.createdAt));
+      let newFalseData = data.filter(item => item.newTag == false);
+      Array.prototype.push.apply(newTrueData, newFalseData);
+      return newTrueData;
+    }
+    else {
+      return data;
+    }
+  }
+
   vinSelection(vinSelectedValue: any) {
     this.vinListSelectedValue = vinSelectedValue;
-    // console.log("----selectionValue from Second dropdown--",vinSelectedValue)
-
   }
 
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+  pageSizeUpdated(_event) {
+    setTimeout(() => {
+      document.getElementsByTagName('mat-sidenav-content')[0].scrollTo(0, 0)
+    }, 100);
+  }
 }
