12e575115a8265c3c0540cef0ace39079d574582
Manoj Nandamuri <manoj.nandamuri@atos.net>
2021-05-21 12:36:18 +0530

    Adding Create preferences in Portal

--- dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/ReportController.cs
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/ReportController.cs
@@ -11,6 +11,7 @@ using static net.atos.daf.ct2.reportservice.ReportService;
 using Report = net.atos.daf.ct2.portalservice.Entity.Report;
 using net.atos.daf.ct2.portalservice.Entity.Report;
 using System;
+using Newtonsoft.Json;
 
 namespace net.atos.daf.ct2.portalservice.Controllers
 {
@@ -75,5 +76,39 @@ namespace net.atos.daf.ct2.portalservice.Controllers
             }
         }
         #endregion
+
+        [HttpPost]
+        [Route("createuserpreference")]
+        public async Task<IActionResult> CreateUserPreference(net.atos.daf.ct2.portalservice.Entity.Report.UserPreferenceCreateRequest objUserPreferenceCreateRequest)
+        {
+            try
+            {
+                var request = _mapper.MapCreateUserPrefences(objUserPreferenceCreateRequest);
+                var response = await _reportServiceClient.CreateUserPreferenceAsync(request);
+                if (response == null)
+                    return StatusCode(500, "Internal Server Error.");
+
+                switch (response.Code)
+                {
+                    case Responsecode.Success:
+                        return Ok(response);
+                    case Responsecode.Failed:
+                        return StatusCode((int)response.Code, response.Message);
+                    case Responsecode.InternalServerError:
+                        return StatusCode((int)response.Code, response.Message);
+                    default:
+                        return StatusCode((int)response.Code, response.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                await _auditHelper.AddLogs(DateTime.Now, DateTime.Now, "Report Controller",
+                                 "Report service", Entity.Audit.AuditTrailEnum.Event_type.CREATE, Entity.Audit.AuditTrailEnum.Event_status.FAILED,
+                                 $"createuserpreference method Failed. Error:{ex.Message}", 0, 0, JsonConvert.SerializeObject(objUserPreferenceCreateRequest),
+                                  Request);
+                _logger.Error(null, ex);
+                return StatusCode(500, $"{ex.Message} {ex.StackTrace}");
+            }
+        }
     }
 }
