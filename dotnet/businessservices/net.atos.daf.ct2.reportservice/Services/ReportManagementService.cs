7298f3191d64392231455f2bf32640daaaaf4d59
Shailesh Sangekar <shailesh.sangekar@atos.net>
2021-05-24 09:30:14 +0530

    Trip Details Report Table implementation

--- dotnet/businessservices/net.atos.daf.ct2.reportservice/Services/ReportManagementService.cs
+++ dotnet/businessservices/net.atos.daf.ct2.reportservice/Services/ReportManagementService.cs
@@ -2,12 +2,14 @@
 using log4net;
 using net.atos.daf.ct2.reports.ENUM;
 using net.atos.daf.ct2.reports;
+using ReportComponent = net.atos.daf.ct2.reports;
 using net.atos.daf.ct2.reportservice.entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace net.atos.daf.ct2.reportservice.Services
 {
@@ -28,25 +30,26 @@ namespace net.atos.daf.ct2.reportservice.Services
         {
             try
             {
-                var userPrefernces = await _reportManager.GetUserPreferenceReportDataColumn(request.ReportId, request.AccountId, request.OrganizationId);
-                if(userPrefernces.Count() == 0)
+                var userPrefernces = await _reportManager.GetUserPreferenceReportDataColumn(request.ReportId, request.AccountId);
+                if (userPrefernces.Count() == 0)
                 {
-                    return  await Task.FromResult(new UserPreferenceDataColumnResponse
+                    return await Task.FromResult(new UserPreferenceDataColumnResponse
                     {
                         Message = String.Format(ReportConstants.USER_PREFERENCE_FAILURE_MSG, request.AccountId, request.ReportId, ReportConstants.USER_PREFERENCE_FAILURE_MSG2),
                         Code = Responsecode.Failed
-                    }); 
+                    });
                 }
-                if (!userPrefernces.Any(a => !string.IsNullOrEmpty(a.State))) {
-
-                    foreach (var userpreferece in userPrefernces) {
-                        userpreferece.State = ((char)ReportPreferenceState.Active).ToString();
+                if (!userPrefernces.Any(a => !string.IsNullOrEmpty(a.IsExclusive)))
+                {
+                    foreach (var userpreferece in userPrefernces)
+                    {
+                        userpreferece.IsExclusive = ((char)IsExclusive.No).ToString();
                     }
                 }
 
                 var response = new UserPreferenceDataColumnResponse
-                {                    
-                    Message = String.Format(ReportConstants.USER_PREFERENCE_SUCCESS_MSG,request.AccountId, request.ReportId),
+                {
+                    Message = String.Format(ReportConstants.USER_PREFERENCE_SUCCESS_MSG, request.AccountId, request.ReportId),
                     Code = Responsecode.Success
                 };
                 response.UserPreferences.AddRange(_mapper.MapUserPrefences(userPrefernces));
@@ -114,7 +117,7 @@ namespace net.atos.daf.ct2.reportservice.Services
                 {
                     Message = ReportConstants.GET_VIN_SUCCESS_MSG,
                     Code = Responsecode.Success
-                };                
+                };
                 response.VinList.AddRange(_mapper.MapVinList(vinList));
                 return await Task.FromResult(response);
             }
@@ -128,6 +131,46 @@ namespace net.atos.daf.ct2.reportservice.Services
                 };
                 errorResponse.VinList.Add(new List<string>());
                 return await Task.FromResult(errorResponse);
+            }
+        }
+        #endregion
+
+
+        #region Trip Report Table Details
+        public override async Task<TripResponce> GetFilteredTripDetails(TripFilterRequest request, ServerCallContext context)
+        {
+            try
+            {
+                _logger.Info("Get GetAllTripDetails.");
+                ReportComponent.entity.TripFilterRequest objTripFilter = new ReportComponent.entity.TripFilterRequest();
+                objTripFilter.VIN = request.VIN;
+                objTripFilter.StartDateTime = request.StartDateTime;
+                objTripFilter.EndDateTime = request.EndDateTime;
+
+                var result = await _reportManager.GetFilteredTripDetails(objTripFilter);
+                TripResponce response = new TripResponce();
+                if (result.Count > 0)
+                {
+                    var res = JsonConvert.SerializeObject(result);
+                    response.TripData.AddRange(
+                        JsonConvert.DeserializeObject<Google.Protobuf.Collections.RepeatedField<TripDetils>>(res)
+                        );
+
+                    //foreach (ReportComponent.entity.TripFilterRequest entity in result)
+                    //{
+                    //    response.TripData.Add(_mapper.ToTripResponce(entity));
+                    //}
+                }
+                return await Task.FromResult(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(null, ex);
+                return await Task.FromResult(new TripResponce
+                {
+                    Code = Responsecode.Failed,
+                    Message = "GetFilteredTripDetails get faile due to - " + ex.Message
+                });
             }
         }
         #endregion
