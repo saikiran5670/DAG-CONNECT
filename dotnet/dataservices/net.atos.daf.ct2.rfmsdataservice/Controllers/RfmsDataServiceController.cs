94767f39eddba1ed540199620f5620622602e6b4
Kumari, Archana <archana.kumari@atos.net>
2021-05-25 18:54:39 +0530

    fixed error

--- dotnet/dataservices/net.atos.daf.ct2.rfmsdataservice/Controllers/RfmsDataServiceController.cs
+++ dotnet/dataservices/net.atos.daf.ct2.rfmsdataservice/Controllers/RfmsDataServiceController.cs
@@ -13,50 +13,51 @@ using net.atos.daf.ct2.rfms.responce;
 using net.atos.daf.ct2.rfmsdataservice.Entity;
 using net.atos.daf.ct2.utilities;
 using net.atos.daf.ct2.vehicle;
+using Newtonsoft.Json;
 
 namespace net.atos.daf.ct2.rfmsdataservice.Controllers
 {
 
     [ApiController]
-    [Route("rfms-vehicles")]
-    [Authorize(Policy = AccessPolicies.MainAccessPolicy)]
+    [Route("rfms-data")]
+    [Authorize]
     public class RfmsDataServiceController : ControllerBase
     {
-              private readonly ILogger<RfmsDataServiceController> _logger;
-              private readonly IRfmsManager _rfmsManager;
-              private readonly IAuditTraillib _auditTrail;
-               
-
-              public RfmsDataServiceController(IRfmsManager rfmsManager,ILogger<RfmsDataServiceController> logger,IAuditTraillib auditTrail)
-              {
-                _rfmsManager = rfmsManager;
-                _logger = logger;
-                _auditTrail = auditTrail;
-                      
-              }
+        private readonly ILogger<RfmsDataServiceController> _logger;
+        private readonly IRfmsManager _rfmsManager;
+        private readonly IAuditTraillib _auditTrail;
 
 
-               [HttpGet]
-               [Route("rfms/vehicles")]
-               public async Task<IActionResult> GetVehicles(RfmsVehicleRequest rfmsVehicleRequest)
-              {
-                try
-                  {
-                     long currentdatetime = UTCHandling.GetUTCFromDateTime(DateTime.Now);
-                     await AuditTrail.AddLogs(DateTime.Now, DateTime.Now, 0, "Rfms Vehicle Service", "Rfms Vehicle Service", AuditTrailEnum.Event_type.GET, AuditTrailEnum.Event_status.PARTIAL, "Get namelist method vehicle namelist service", 1, 2, since, 0, 0);
-                     var responce = new RfmsVehicles();                  
-                     responce = await rfmsManager.Get(rfmsVehicleRequest);
-                     return Ok(responce);
+        public RfmsDataServiceController(IRfmsManager rfmsManager, ILogger<RfmsDataServiceController> logger, IAuditTraillib auditTrail)
+        {
+            _rfmsManager = rfmsManager;
+            _logger = logger;
+            _auditTrail = auditTrail;
 
-                  }
+        }
 
-                     
-                catch (Exception ex)
-                {
-                     Logger.LogError(ex, "Error occurred while processing Rfms Vehicle data.");
-                     return StatusCode(500, string.Empty);
-                }
-              }     
+
+        [HttpGet]
+        [Route("rfms/vehicles")]
+        public async Task<IActionResult> GetVehicles(RfmsVehicleRequest rfmsVehicleRequest)
+        {
+            try
+            {
+                long currentdatetime = UTCHandling.GetUTCFromDateTime(DateTime.Now);
+                await _auditTrail.AddLogs(DateTime.Now, DateTime.Now, 0, "Rfms Vehicle Service", "Rfms Vehicle Service", AuditTrailEnum.Event_type.GET, AuditTrailEnum.Event_status.PARTIAL, "Get namelist method vehicle namelist service", 1, 2, JsonConvert.SerializeObject(rfmsVehicleRequest), 0, 0);
+                var responce = new RfmsVehicles();
+                responce = await _rfmsManager.GetVehicles(rfmsVehicleRequest);
+                return Ok(responce);
+
+            }
+
+
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while processing Rfms Vehicle data.");
+                return StatusCode(500, string.Empty);
+            }
+        }
 
     }
 }
