41e7988f9fc02483af7c9a26fe98843f6cd84e1d
Kumari, Archana <archana.kumari@atos.net>
2021-05-25 22:25:32 +0530

    added repository for  here map

---
+++ dotnet/commoncomponent/net.atos.daf.ct2.utilities/repository/MapGeocodeRepository.cs
@@ -0,0 +1,20 @@
+﻿
+using net.atos.daf.ct2.data;
+
+namespace net.atos.daf.ct2.utilities.repository
+{
+    public class MapGeocodeRepository : IMapGeocodeRepository
+    {
+        private readonly IDataAccess _dataAccess;
+        private readonly IMapGeocodeManager _mapGeocodeManager;
+        private static readonly log4net.ILog log =
+       log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public MapGeocodeRepository()
+        {
+
+        }
+
+    }
+
+  
+}
