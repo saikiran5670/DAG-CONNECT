8af8134d94661b1e1892d5ac353d12541743e647
Shailesh Sangekar <shailesh.sangekar@atos.net>
2021-06-02 15:07:34 +0530

    Report Driver time management's details result implementation

--- dotnet/businessservices/net.atos.daf.ct2.reportservice/Services/ReportManagementService.cs
+++ dotnet/businessservices/net.atos.daf.ct2.reportservice/Services/ReportManagementService.cs
@@ -211,5 +211,94 @@ namespace net.atos.daf.ct2.reportservice.Services
             }
         }
         #endregion
+
+        #region Driver Time management Report
+        /// <summary>
+        /// Fetch Multiple Drivers activity data
+        /// </summary>
+        /// <param name="request"> Filters for driver activity with VIN and Driver ID </param>
+        /// <param name="context">GRPC Context</param>
+        /// <returns>Driver activity by type column</returns>
+        public override async Task<DriverActivityResponce> GetDriversActivity(ActivityFilterRequest request, ServerCallContext context)
+        {
+            try
+            {
+                _logger.Info("Get GetDriversActivity for multiple drivers.");
+                ReportComponent.entity.DriverActivityFilter objActivityFilter = new ReportComponent.entity.DriverActivityFilter();
+                objActivityFilter.VIN.Append(request.VINs);
+                objActivityFilter.StartDateTime = request.StartDateTime;
+                objActivityFilter.EndDateTime = request.EndDateTime;
+
+                var result = await _reportManager.GetDriversActivity(objActivityFilter);
+                DriverActivityResponce response = new DriverActivityResponce();
+                if (result?.Count > 0)
+                {
+                    string res = JsonConvert.SerializeObject(result);
+                    response.DriverActivities.AddRange(JsonConvert.DeserializeObject<Google.Protobuf.Collections.RepeatedField<DriverActivity>>(res));
+                    response.Code = Responsecode.Success;
+                    response.Message = Responsecode.Success.ToString();
+                }
+                else
+                {
+                    response.Code = Responsecode.NotFound;
+                    response.Message = "No Result Found";
+                }
+                return await Task.FromResult(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(null, ex);
+                return await Task.FromResult(new DriverActivityResponce
+                {
+                    Code = Responsecode.Failed,
+                    Message = "GetDriversActivity get failed due to - " + ex.Message
+                });
+            }
+        }
+
+        /// <summary>
+        /// Fetch Single driver activity data
+        /// </summary>
+        /// <param name="request"> Filters for driver activity with VIN and Driver ID </param>
+        /// <param name="context">GRPC Context</param>
+        /// <returns>Driver activity by type column</returns>
+        public override async Task<DriverActivityResponce> GetDriverActivity(ActivityFilterRequest request, ServerCallContext context)
+        {
+            try
+            {
+                _logger.Info("Get GetDriverActivity for single driver.");
+                ReportComponent.entity.DriverActivityFilter objActivityFilter = new ReportComponent.entity.DriverActivityFilter();
+                objActivityFilter.VIN.Append(request.VINs);
+                objActivityFilter.StartDateTime = request.StartDateTime;
+                objActivityFilter.EndDateTime = request.EndDateTime;
+
+                var result = await _reportManager.GetDriverActivity(objActivityFilter);
+                DriverActivityResponce response = new DriverActivityResponce();
+                if (result?.Count > 0)
+                {
+                    string res = JsonConvert.SerializeObject(result);
+                    response.DriverActivities.AddRange(JsonConvert.DeserializeObject<Google.Protobuf.Collections.RepeatedField<DriverActivity>>(res));
+                    response.Code = Responsecode.Success;
+                    response.Message = Responsecode.Success.ToString();
+                }
+                else
+                {
+                    response.Code = Responsecode.NotFound;
+                    response.Message = "No Result Found";
+                }
+                return await Task.FromResult(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(null, ex);
+                return await Task.FromResult(new DriverActivityResponce
+                {
+                    Code = Responsecode.Failed,
+                    Message = "GetDriverActivity get failed due to - " + ex.Message
+                });
+            }
+        }
+
+        #endregion
     }
 }
