cfad857c72ad57257c346316ce845a50e28b7d42
JA <jayuahire@gmail.com>
2021-06-02 21:04:03 +0530

    Added class and interface in report schedular

---
+++ dotnet/corecomponents/net.atos.daf.ct2.reportschedular/repository/IReportSchedularRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace net.atos.daf.ct2.reportschedular.repository
+{
+    public interface IReportSchedularRepository
+    {
+    }
+}
---
+++ dotnet/corecomponents/net.atos.daf.ct2.reportschedular/repository/ReportSchedularRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.atos.daf.ct2.reportschedular.repository
+{
+    public class ReportSchedularRepository
+    {
+    }
+}
