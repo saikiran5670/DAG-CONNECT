c0ec16a57daee82e14c464a2d96b92435c8bae21
Chaitali <chaitali.chamale@atos.net>
2021-06-02 00:45:41 -0700

    adv filter changes added

--- ui/src/app/configuration/alerts/create-edit-view-alerts/alert-advanced-filter/alert-advanced-filter.component.ts
+++ ui/src/app/configuration/alerts/create-edit-view-alerts/alert-advanced-filter/alert-advanced-filter.component.ts
@@ -1,4 +1,8 @@
+import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-alert-advanced-filter',
@@ -6,10 +10,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert-advanced-filter.component.css']
 })
 export class AlertAdvancedFilterComponent implements OnInit {
-
-  constructor() { }
+  @Input() translationData: any = [];
+  alertAdvancedFilterForm: FormGroup;
+  localStLanguage: any;
+  organizationId: number;
+  accountId: number;
+  isDistanceSelected: boolean= false;
+  isOccurenceSelected: boolean= false;
+  isDurationSelected: boolean= false;
+  selectedPoiSite: any;
+  
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.localStLanguage = JSON.parse(localStorage.getItem("language"));
+    this.organizationId = parseInt(localStorage.getItem("accountOrganizationId"));
+    this.accountId= parseInt(localStorage.getItem("accountId"));
+    this.alertAdvancedFilterForm = this._formBuilder.group({
+      poiSite: [''],
+      distance: [''],
+      occurences: [''],
+      duration: ['']
+    })
   }
 
+  onChangeDistance(event: any){
+    if(event.checked){
+      this.isDistanceSelected= true;
+    }
+    else{
+      this.isDistanceSelected= false;
+    }
+  }
+
+  onChangeOccurence(event: any)
+  {
+    if(event.checked){
+      this.isOccurenceSelected= true;
+    }
+    else{
+      this.isOccurenceSelected= false;
+    }
+  }
+
+  onChangeDuration(event: any)
+  {
+    if(event.checked){
+      this.isDurationSelected= true;
+    }
+    else{
+      this.isDurationSelected= false;
+    }
+  }
+
+  onRadioButtonChange(event: any){
+    this.selectedPoiSite = event.value;
+  }
 }
