47f0e59ff3405e8ae809aede78ff636a2082da29
Chaitali <chaitali.chamale@atos.net>
2021-06-01 22:31:17 -0700

    alert-adv-filter component is added

---
+++ ui/src/app/configuration/alerts/create-edit-view-alerts/alert-advanced-filter/alert-advanced-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-advanced-filter',
+  templateUrl: './alert-advanced-filter.component.html',
+  styleUrls: ['./alert-advanced-filter.component.css']
+})
+export class AlertAdvancedFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
