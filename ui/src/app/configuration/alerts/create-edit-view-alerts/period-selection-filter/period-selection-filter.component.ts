d37fe6aaaef5c08afde3d62a7d685b40082f57cc
namitapatil0502 <namita.patil@atos.net>
2021-06-07 16:10:49 +0530

    Component added for Hours of Service alert type

--- ui/src/app/configuration/alerts/create-edit-view-alerts/period-selection-filter/period-selection-filter.component.ts
+++ ui/src/app/configuration/alerts/create-edit-view-alerts/period-selection-filter/period-selection-filter.component.ts
@@ -1,6 +1,6 @@
 import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder, FormArray } from '@angular/forms';
+import { FormGroup,FormBuilder, FormArray, FormControl } from '@angular/forms';
 import { CustomValidators } from 'src/app/shared/custom.validators';
 import { Validators } from '@angular/forms';
 
@@ -17,6 +17,7 @@ localStLanguage: any;
 organizationId: number;
 accountId: number;
 FormArrayItems:  FormArray;
+days: any= [];
 
   constructor(private _formBuilder: FormBuilder) { }
 
@@ -24,14 +25,55 @@ FormArrayItems:  FormArray;
     this.localStLanguage = JSON.parse(localStorage.getItem("language"));
     this.organizationId = parseInt(localStorage.getItem("accountOrganizationId"));
     this.accountId= parseInt(localStorage.getItem("accountId"));
+    this.days= ['Sunday', 'Monday', 'tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    
     this.periodSelectionForm = this._formBuilder.group({
       // recipientLabel: ['', [ Validators.required ]],
       FormArrayItems : this._formBuilder.array([this.initPeriodItems()]),
-    })
+    });
+
+    for(let i = 1; i < 7; i++ )
+      this.weekDays().push(this.initPeriodItems());
+    }
+
+  initPeriodItems(): FormGroup{
+    return this._formBuilder.group({
+      daySelection: [''],
+      fulldayCustom: [''],
+      FormArrayCustomItems : this._formBuilder.array([this.initCustomPeriodItems()])
+    });
   }
 
-  initPeriodItems(){
-
+  initCustomPeriodItems(): FormGroup{
+    return this._formBuilder.group({
+      fromTime : new FormControl({value: '00:00'}),
+      toTime:  new FormControl({value: '00:00'})
+    });
   }
+
+  onChangeDaySelection(event){
+    if(event.checked){
+      this.periodSelectionForm.get('fulldayCustom').setValue('A');
+    }
+  }
+  
+  onDeleteCustomPeriod(periodIndex, customIndex){
+     this.customPeriods(periodIndex).removeAt(customIndex);
+  }
+
+
+
+  addCustomPeriod(periodIndex){
+    if(this.customPeriods(periodIndex).length < 4)
+      this.customPeriods(periodIndex).push(this.initCustomPeriodItems());
+  }
+
+  weekDays(): FormArray {
+    return this.periodSelectionForm.get("FormArrayItems") as FormArray;
+  }
+
+  customPeriods(periodIndex: number) : FormArray {
+    return this.weekDays().at(periodIndex).get("FormArrayCustomItems") as FormArray
+  }
 
 }
