9c4e762243db767ddc5803b866d2aa36548b500b
Hemanshu Waghulde <hemanshu.waghulde@atos.net>
2021-06-07 15:46:11 +0530

    eco score model modification

--- dotnet/portalserver/net.atos.daf.ct2.portalservice/Entity/Report/EcoScoreProfileModel.cs
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Entity/Report/EcoScoreProfileModel.cs
@@ -1,7 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace net.atos.daf.ct2.portalservice.Entity.Report
 {
+    public class EcoScoreProfileModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public List<EcoScoreProfileSection> ProfileSections { get; set; }
+    }
+
+    public class EcoScoreProfileSection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<EcoScoreKPI> KPIs { get; set; }
+    }
+
+    public class EcoScoreKPI
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public char LimitType { get; set; }
+        public char RangeValueType { get; set; }
+        public int LimitValue { get; set; }
+        public int TargetValue { get; set; }
+        public int LowerValue { get; set; }
+        public int UpperValue { get; set; }
+        public int MaxUpperValue { get; set; }
+        public int SequenceNo { get; set; }
+    }
+
     public class EcoScoreProfileCreateRequest
     {
         public string Name { get; set; }
@@ -12,7 +44,7 @@ namespace net.atos.daf.ct2.portalservice.Entity.Report
 
     public class EcoScoreProfileKPI
     {
-        public int KPI_Id { get; set; }
+        public int KPIId { get; set; }
         public int LimitValue { get; set; }
         public int TargetValue { get; set; }
         public int LowerValue { get; set; }
