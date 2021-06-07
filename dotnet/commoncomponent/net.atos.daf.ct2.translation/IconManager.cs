754db5b8c182a524c6cd39d4ce050df2a96f4843
snehal bante <snehal.bante@atos.net>
2021-06-07 18:55:06 +0530

    DTC translation

--- dotnet/commoncomponent/net.atos.daf.ct2.translation/IconManager.cs
+++ dotnet/commoncomponent/net.atos.daf.ct2.translation/IconManager.cs
@@ -14,12 +14,39 @@ namespace net.atos.daf.ct2.translation
         {
             _iconRepository = _repository;
         }
-        public async Task<bool> UpdateIcons(List<Icon> iconlist)
+        public async Task<string> UpdateIcons(List<Icon> iconlist)
         {
             try
             {
-                bool result = await _iconRepository.UpdateIcons(iconlist);
-                return result;
+                string result = "";
+                string is_Result = "";
+                int count = 0;
+                int count1 = 0;
+                foreach (var icon in iconlist)
+                {
+                    result = await _iconRepository.UpdateIcons(icon);
+
+                    if (result != "" )
+                    {
+                        if (count > 0 || count1 > 0)
+                        {
+                            is_Result += " , ";
+                        }
+                        is_Result += result;
+                        count++;
+                    }
+                    else
+                    {
+                        if (count1 > 0 || count > 0)
+                        {
+                            is_Result += " , ";
+                        }
+                        is_Result += " File Name not exist : " + icon.Name;
+                        count1++;
+                    }
+                
+                }
+                return is_Result;
             }
             catch (Exception)
             {
