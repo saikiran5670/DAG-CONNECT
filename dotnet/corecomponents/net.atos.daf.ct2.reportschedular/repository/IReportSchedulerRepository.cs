34b57c84d98cafc9ae9910201edbe365b62e16dc
JA <jayuahire@gmail.com>
2021-06-09 20:37:45 +0530

    Report Type and Receipt email method added in report schedular repository

--- dotnet/corecomponents/net.atos.daf.ct2.reportschedular/repository/IReportSchedulerRepository.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.reportschedular/repository/IReportSchedulerRepository.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using net.atos.daf.ct2.reportscheduler.entity;
 
 namespace net.atos.daf.ct2.reportscheduler.repository
 {
     public interface IReportSchedulerRepository
     {
+        #region Parameter Report Schedular
+        Task<IEnumerable<ReportType>> GetReportType(int accountid, int organizationid);
+        Task<IEnumerable<string>> GetRecipientsEmail();
+        #endregion
     }
 }
