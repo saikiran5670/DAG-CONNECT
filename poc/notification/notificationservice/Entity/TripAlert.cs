237f9963393f5e45f4786577b791bccb531d5a0f
Shailesh Rajput <shailesh-nathusing.rajput@atos.net>
2021-06-15 19:20:40 +0530

    Alert processing POC -User Story 9924

---
+++ poc/notification/notificationservice/Entity/TripAlert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace notificationservice.Entity
+{
+    public class TripAlert
+    {
+        public int id { get; set; }
+        public string Tripid { get; set; }
+        public string Vin { get; set; }
+        public string CategoryType { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public int Alertid { get; set; }
+        public double ThresholdValue { get; set; }
+        public string ThresholdValueUnitType { get; set; }
+        public double ValueAtAlertTime { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public long AlertGeneratedTime { get; set; }
+        public long MessageTimestamp { get; set; }
+        public long CreatedAt { get; set; }
+        public long ModifiedAt { get; set; }
+    }
+}
