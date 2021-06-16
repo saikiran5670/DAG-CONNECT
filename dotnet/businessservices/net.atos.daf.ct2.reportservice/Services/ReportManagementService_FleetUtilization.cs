d405b57c2fd5d4b8bf751ae87562a6fd2cbb356e
Shailesh Sangekar <shailesh.sangekar@atos.net>
2021-06-16 10:39:02 +0530

    Created partial class for Fleet utilization report

---
+++ dotnet/businessservices/net.atos.daf.ct2.reportservice/Services/ReportManagementService_FleetUtilization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Newtonsoft.Json;
+using VisibleEntity = net.atos.daf.ct2.visibility.entity;
+using ReportComponent = net.atos.daf.ct2.reports;
+using ProtobufCollection = Google.Protobuf.Collections;
+
+namespace net.atos.daf.ct2.reportservice.Services
+{
+    public partial class ReportManagementService : ReportService.ReportServiceBase
+    {
+
+
+    }
+}
