dcd64f8983075f56eaba3e4e560f091d3fa3fbb0
JA <jayuahire@gmail.com>
2021-06-17 19:24:49 +0530

    Get method for report scheduler added in portal service

--- dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/ReportSchedulerController.cs
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/ReportSchedulerController.cs
@@ -33,7 +33,7 @@ namespace net.atos.daf.ct2.portalservice.Controllers
         #region Get Report Scheduler Paramenter
         [HttpGet]
         [Route("getreportschedulerparameter")]
-        public async Task<IActionResult> GetReportScheduleraParameter(int accountId, int orgnizationid, int roleid)
+        public async Task<IActionResult> GetReportSchedulerParameter(int accountId, int orgnizationid, int roleid)
         {
             try
             {
@@ -100,6 +100,36 @@ namespace net.atos.daf.ct2.portalservice.Controllers
             }
         }
 
+        #endregion
+
+        #region Get Report Scheduler
+        [HttpGet]
+        [Route("get")]
+        public async Task<IActionResult> GetReportScheduler(int accountId, int orgnizationid)
+        {
+            try
+            {
+                if (orgnizationid == 0) return BadRequest(ReportSchedulerConstants.REPORTSCHEDULER_ORG_ID_NOT_NULL_MSG);
+                ReportSchedulerListResponse response = await _reportschedulerClient.GetReportSchedulerListAsync(new ReportParameterRequest { AccountId = accountId, OrganizationId = orgnizationid });
+
+                if (response == null)
+                    return StatusCode(500, ReportSchedulerConstants.REPORTSCHEDULER_INTERNEL_SERVER_ISSUE);
+                if (response.Code == ResponseCode.Success)
+                    return Ok(response);
+                if (response.Code == ResponseCode.InternalServerError)
+                    return StatusCode((int)response.Code, String.Format(ReportSchedulerConstants.REPORTSCHEDULER_DATA_NOT_FOUND_MSG, response.Message));
+                return StatusCode((int)response.Code, response.Message);
+            }
+            catch (Exception ex)
+            {
+                await _auditHelper.AddLogs(DateTime.Now, ReportSchedulerConstants.REPORTSCHEDULER_CONTROLLER_NAME,
+                 ReportSchedulerConstants.REPORTSCHEDULER_SERVICE_NAME, Entity.Audit.AuditTrailEnum.Event_type.GET, Entity.Audit.AuditTrailEnum.Event_status.FAILED,
+                string.Format(ReportSchedulerConstants.REPORTSCHEDULER_EXCEPTION_LOG_MSG, "GetReportScheduler", ex.Message), 1, 2, Convert.ToString(accountId),
+                  _userDetails);
+                _logger.Error(null, ex);
+                return StatusCode(500, ex.Message + " " + ex.StackTrace);
+            }
+        }
         #endregion
     }
 }
