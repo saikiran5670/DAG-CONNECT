b55a314b19cb1bd789c1aa913a41b7fa4735c630
JA <jayuahire@gmail.com>
2021-06-18 10:04:39 +0530

    script aligment in report scheduler

--- dotnet/businessservices/net.atos.daf.ct2.reportschedularservice/Entity/ReportSchedulerConstant.cs
+++ dotnet/businessservices/net.atos.daf.ct2.reportschedularservice/Entity/ReportSchedulerConstant.cs
@@ -7,7 +7,7 @@ namespace net.atos.daf.ct2.reportschedulerservice.Entity
 {
     public static class ReportSchedulerConstant
     {
-        public const string REPORT_SCHEDULER_FEATURE_NAME = "ReportScheduler";
+        public const string REPORT_SCHEDULER_FEATURE_NAME = "Configuration.ReportScheduler";
         public const string REPORT_SCHEDULER_GET_SUCCESS_MSG = "Report Scheduler data retrieved";
         public const string REPORT_SCHEDULER_GET_CALLED_MSG = "Get method in report scheduler called.";
         public const string REPORT_SCHEDULER_GET_FAIL_MSG = "Get report scheduler list fail : ";
