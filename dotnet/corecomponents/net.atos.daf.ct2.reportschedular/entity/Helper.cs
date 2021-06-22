a5ff6051ec76c3ba935b61c5bd3567ed43faea93
Kumari, Archana <archana.kumari@atos.net>
2021-06-22 15:52:08 +0530

    added method to calculate timerange as per frequency

---
+++ dotnet/corecomponents/net.atos.daf.ct2.reportschedular/entity/Helper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using net.atos.daf.ct2.reportscheduler.ENUM;
+
+namespace net.atos.daf.ct2.reportscheduler.entity
+{
+    public class Helper
+    {
+        public DateTime GetNextFrequencyTime(long currentdate, TimeFrequenyType timeFrequenyType)
+        {
+            DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            DateTime date = start.AddMilliseconds(currentdate).ToLocalTime();
+            var nextDate = date.AddDays(1);
+            switch (timeFrequenyType)
+            {
+
+                case TimeFrequenyType.Daily:
+                    nextDate = date.AddDays(1);
+                    break;
+                case TimeFrequenyType.Weekly:
+                    nextDate = date.AddDays(7);
+                    break;
+                case TimeFrequenyType.BiWeekly:
+                    nextDate = date.AddDays(14);
+                    break;
+            }
+            return nextDate;
+        }
+
+        public Tuple<DateTime, DateTime> GetNextQuarterTime(long currentdate)
+        {
+            DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            DateTime date = start.AddMilliseconds(currentdate).ToLocalTime();
+            var quarterNumber = ((date.Month - 1) / 3) + 1;
+            var firstDayOfQuarter = new DateTime(date.Year, ((quarterNumber - 1) * 3) + 1, 1);
+            var lastDayOfQuarter = firstDayOfQuarter.AddMonths(3).AddDays(-1);
+            var nextQuarter = quarterNumber + 1;
+            var firstDayOfnextQuarter = new DateTime(date.Year, ((nextQuarter - 1) * 3) + 1, 1);
+            var lastDayOfnextQuarter = firstDayOfnextQuarter.AddMonths(3).AddDays(-1);
+            return Tuple.Create(firstDayOfnextQuarter, lastDayOfnextQuarter);
+        }
+
+
+        public Tuple<DateTime, DateTime> GetNextMonthlyTime(long currentdate)
+        {
+            DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            DateTime date = start.AddMilliseconds(currentdate).ToLocalTime();
+            var nextMonth = date.AddMonths(1);
+            var startDate = new DateTime(nextMonth.Year, nextMonth.Month, 1);
+            var endDate = startDate.AddMonths(1).AddDays(-1);
+            return Tuple.Create(startDate, endDate);
+        }
+
+
+    }
+}
