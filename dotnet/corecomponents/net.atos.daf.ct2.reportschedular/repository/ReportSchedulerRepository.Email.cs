78d35911c7460e157543eb1233a05ffbc882b643
Kumari, Archana <archana.kumari@atos.net>
2021-06-23 20:10:27 +0530

    added GetReportEmailDetails method to report Email repositiory

---
+++ dotnet/corecomponents/net.atos.daf.ct2.reportschedular/repository/ReportSchedulerRepository.Email.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Dapper;
+using net.atos.daf.ct2.reportscheduler.entity;
+
+namespace net.atos.daf.ct2.reportscheduler.repository
+{
+    public partial class ReportSchedulerRepository : IReportSchedulerRepository
+    {
+        private async void GetReportEmailDetails()
+        {
+
+
+            var parameterAlert = new DynamicParameters();
+
+            string queryAlert = @"SELECT repsch.id as repsch_id, 
+                                            repsch.organization_id as repsch_organization_id, 
+                                            repsch.report_id as repsch_report_id, 
+                                            repsch.frequency_type as repsch_frequency_type,                                           
+                                            repsch.last_schedule_run_date as repsch_last_schedule_run_date, 
+                                            repsch.next_schedule_run_date as repsch_next_schedule_run_date,                                            
+                                            repsch.mail_subject as repsch_mail_subject, 
+                                            repsch.mail_description as repsch_mail_description,                                         
+                                            receipt.id as receipt_id, 
+                                            repsch.created_by as repsch_created_by,                                           
+                                            repsch.modified_by as repsch_modified_by, 
+                                            receipt.schedule_report_id as receipt_schedule_report_id, 
+                                            receipt.email as receipt_email,  
+                                            schrep.id as schrep_id, 
+                                            schrep.token as schrep_token,
+                                            schrep.schedule_report_id as schrep_schedule_report_id, 
+                                            schrep.report as schrep_report,
+                                            schrep.downloaded_at as schrep_downloaded_at, 
+                                            schrep.valid_till as schrep_valid_till, 
+                                            schrep.created_at as schrep_created_at, 
+                                            schrep.start_date as schrep_start_date, 
+                                            schrep.end_date as schrep_end_date
+	                                        FROM master.reportscheduler as repsch	                                 
+	                                       LEFT JOIN master.scheduledreportrecipient as receipt
+	                                       ON repsch.id=receipt.schedule_report_id AND repsch.status='A' AND receipt.state='A'	                                  
+	                                       LEFT JOIN master.scheduledreport as schrep
+	                                       ON repsch.id=schrep.schedule_report_id AND repsch.status='A' ";
+            queryAlert += " where date_trunc('hour', (to_timestamp(repsch.next_schedule_run_date) AT TIME ZONE 'UTC')) = date_trunc('hour', NOW() AT TIME ZONE 'UTC')";
+
+            IEnumerable<ReportSchedulerResult> reportSchedulerResult = await _dataAccess.QueryAsync<ReportSchedulerResult>(queryAlert, parameterAlert);
+
+        }
+    }
+}
