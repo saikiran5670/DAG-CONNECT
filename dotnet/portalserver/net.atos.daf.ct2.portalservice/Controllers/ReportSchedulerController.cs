fd674caea459a97443b571c13c514754203a0ea0
JA <jayuahire@gmail.com>
2021-06-24 21:02:04 +0530

    Changes in report scheduler controller

--- dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/ReportSchedulerController.cs
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/ReportSchedulerController.cs
@@ -251,7 +251,7 @@ namespace net.atos.daf.ct2.portalservice.Controllers
 
                 if (reportSchedulerResponse != null && reportSchedulerResponse.Code == ResponseCode.Failed)
                 {
-                    return StatusCode(500, ReportSchedulerConstants.REPORTSCHEDULER_CREATE_FAILED_MSG);
+                    return StatusCode(500, ReportSchedulerConstants.REPORTSCHEDULER_UPDATE_FAILED_MSG);
                 }
                 else if (reportSchedulerResponse != null && reportSchedulerResponse.Code == ResponseCode.Conflict)
                 {
@@ -263,7 +263,7 @@ namespace net.atos.daf.ct2.portalservice.Controllers
                 }
                 else
                 {
-                    return StatusCode(500, ReportSchedulerConstants.REPORTSCHEDULER_CREATE_FAILED_MSG);
+                    return StatusCode(500, ReportSchedulerConstants.REPORTSCHEDULER_UPDATE_FAILED_MSG);
                 }
             }
             catch (Exception ex)
@@ -405,7 +405,7 @@ namespace net.atos.daf.ct2.portalservice.Controllers
 
         #region GetPDFBinaryFormatByToken
         [HttpGet]
-        [Route("pdfreportbytoken")]
+        [Route("download")]
         public async Task<IActionResult> GetPDFBinaryFormatByToken(ReportPDFByTokenRequest request)
         {
             try
@@ -436,7 +436,7 @@ namespace net.atos.daf.ct2.portalservice.Controllers
         #endregion
         #region GetPDFBinaryFormatById
         [HttpGet]
-        [Route("pdfreportbyid")]
+        [Route("getpdf")]
         public async Task<IActionResult> GetPDFBinaryFormatById(ReportPDFByTokenRequest request)
         {
             try
