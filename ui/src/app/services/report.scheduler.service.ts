32e94d91463ee64aaa51352678d15fbfc0214bfb
namitapatil0502 <namita.patil@atos.net>
2021-06-29 10:47:21 +0530

    Report scheduler service added

--- ui/src/app/services/report.scheduler.service.ts
+++ ui/src/app/services/report.scheduler.service.ts
@@ -12,10 +12,10 @@ import { ConfigService } from '@ngx-config/core';
 
 @Injectable()
 export class ReportSchedulerService {
-  reportServiceUrl: string = '';
+  reportSchedulerServiceURL: string = '';
   
   constructor(private httpClient: HttpClient, private config: ConfigService) {
-    this.reportServiceUrl = config.getSettings("foundationServices").reportRESTServiceURL;
+    this.reportSchedulerServiceURL = config.getSettings("foundationServices").reportSchedulerRESTServiceURL;
   }
 
   generateHeader(){
@@ -29,51 +29,27 @@ export class ReportSchedulerService {
     return getHeaderObj;
   }
 
-  getReportSchedulerData(){
-      
-  }
-
-  getVINFromTrip(accountId: any, orgId: any){
+  getReportSchedulerData(accountId: any, orgId: any){
     let headerObj = this.generateHeader();
     const headers = {
       headers: new HttpHeaders({ headerObj }),
     };
     return this.httpClient
-      .get<any[]>(`${this.reportServiceUrl}/getvinsfromtripstatisticsandvehicledetails?accountId=${accountId}&organizationId=${orgId}`, headers)
+      .get<any[]>(`${this.reportSchedulerServiceURL}/get?accountId=${accountId}&organizationid=${orgId}`, headers)
       .pipe(catchError(this.handleError));
   }
 
-  getUserPreferenceReport(reportId: any, accountId: any, orgId: any){
-    let headerObj = this.generateHeader();
-    const headers = {
-      headers: new HttpHeaders({ headerObj }),
-    };
-    return this.httpClient
-      .get<any[]>(`${this.reportServiceUrl}/getuserpreferencereportdatacolumn?reportId=${reportId}&accountId=${accountId}&organizationId=${orgId}`, headers)
-      .pipe(catchError(this.handleError));
-  }
-
-  getTripDetails(startTime: any, endTime: any, vin: any){
-    let headerObj = this.generateHeader();
-    const headers = {
-      headers: new HttpHeaders({ headerObj }),
-    };
-    return this.httpClient
-      .get<any[]>(`${this.reportServiceUrl}/gettripdetails?StartDateTime=${startTime}&EndDateTime=${endTime}&VIN=${vin}`, headers)
-      .pipe(catchError(this.handleError));
-  }
-
-  createTripReportPreference(data: any): Observable<any[]> {
-    let headerObj = this.generateHeader();
-    const headers = {
-      headers: new HttpHeaders({ headerObj }),
-    };
-    return this.httpClient
-      .post<any[]>(
-        `${this.reportServiceUrl}/createuserpreference`, data, headers
-      )
-      .pipe(catchError(this.handleError));
-  }
+  // createTripReportPreference(data: any): Observable<any[]> {
+  //   let headerObj = this.generateHeader();
+  //   const headers = {
+  //     headers: new HttpHeaders({ headerObj }),
+  //   };
+  //   return this.httpClient
+  //     .post<any[]>(
+  //       `${this.reportServiceUrl}/createuserpreference`, data, headers
+  //     )
+  //     .pipe(catchError(this.handleError));
+  // }
   
   private handleError(errResponse: HttpErrorResponse) {
       console.error('Error : ', errResponse.error);
