ac21c30b151901123c01a58af4f45ec93bd14be8
Vishal Bangar <vishal.bangar@atos.net>
2021-06-29 12:11:08 +0530

    Trip tracing - component removed

--- ui/src/app/report/report-routing.module.ts
+++ ui/src/app/report/report-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { ReportComponent } from './report.component';
 import { TripReportComponent } from './trip-report/trip-report.component';
-import { TripTracingComponent } from './trip-tracing/trip-tracing.component';
 import { DriverTimeManagementComponent } from './driver-time-management/driver-time-management.component'
 import { FleetUtilisationComponent } from './fleet-utilisation/fleet-utilisation.component';
 
@@ -11,7 +10,6 @@ const routes: Routes = [
   {
     path: "", component: ReportComponent, children:[
       { path: "tripreport", component: TripReportComponent },
-      { path: "triptracing", component: TripTracingComponent },
       { path: "advancedfleetfuelreport", component: DashboardComponent },
       { path: "fleetfuelreport", component: DashboardComponent },
       { path: "fleetutilisation", component: FleetUtilisationComponent },
