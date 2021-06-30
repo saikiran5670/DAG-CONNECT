d2c98da6ad2cf30ae123dead5a0c03de57feed9b
harneet rekhi <harneet.rekhi@atos.net>
2021-06-30 18:55:15 +0530

    Task 11449: Report Creation Comman code

---
+++ dotnet/commoncomponent/net.atos.daf.ct2.template/entity/ReportTemplate.cs
@@ -0,0 +1,24 @@
+﻿//TO DO: ROmve this, after testing
+
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using net.atos.daf.ct2.template.ENUM;
+
+//namespace net.atos.daf.ct2.template.entity
+//{
+//    public class ReportTemplate
+//    {
+//        public int TemplateId { get; set; }
+//        public char ContentType { get; set; }
+//        public ReportType ReportType { get; set; }
+//        public string Description { get; set; }
+//        public IEnumerable<ReportTemplateTranslationLabel> TemplateLabels { get; set; } = new List<ReportTemplateTranslationLabel>();
+//    }
+
+//    public class ReportTemplateTranslationLabel
+//    {
+//        public string LabelKey { get; set; }
+//        public string TranslatedValue { get; set; }
+//    }
+//}
---
+++ dotnet/commoncomponent/net.atos.daf.ct2.unitconversion/entity/UnitConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.atos.daf.ct2.unitconversion.entity
+{
+    public static class ConverterConstants
+    {
+        public const string IMPERIAL = "Imperial";
+        public const string METRIC = "Metric";
+    }
+
+    public static class DistanceConstants
+    {
+        public const string METER = "m";
+    }
+}
