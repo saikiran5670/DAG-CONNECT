6014c8d9a9f7936c3884e977d69de83d2800782e
Chaitali <chaitali.chamale@atos.net>
2021-06-30 03:05:23 -0700

    fleet-fuel-report changes added.

--- ui/src/app/report/fleet-fuel-report/fleet-fuel-report-vehicle/fleet-fuel-report-vehicle.component.ts
+++ ui/src/app/report/fleet-fuel-report/fleet-fuel-report-vehicle/fleet-fuel-report-vehicle.component.ts
@@ -1,17 +1,418 @@
+import { Inject } from '@angular/core';
 import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { OrganizationService } from 'src/app/services/organization.service';
+import { TranslationService } from 'src/app/services/translation.service';
+import { Util } from 'src/app/shared/util';
 
 @Component({
   selector: 'app-fleet-fuel-report-vehicle',
   templateUrl: './fleet-fuel-report-vehicle.component.html',
-  styleUrls: ['./fleet-fuel-report-vehicle.component.css']
+  styleUrls: ['./fleet-fuel-report-vehicle.component.less']
 })
 export class FleetFuelReportVehicleComponent implements OnInit {
   @Input() translationData: any;
+  tripForm: FormGroup;
+  selectedStartTime: any = '00:00';
+  selectedEndTime: any = '23:59'; 
+  startTimeDisplay: any = '00:00:00';
+  endTimeDisplay: any = '23:59:59';
+  fleetFuelSearchData: any = {};
+  localStLanguage: any;
+  accountOrganizationId: any;
+  accountId: any;
+  internalSelection: boolean = false;
+  accountPrefObj: any;
+  prefTimeFormat: any; //-- coming from pref setting
+  prefTimeZone: any; //-- coming from pref setting
+  prefDateFormat: any = 'ddateformat_mm/dd/yyyy'; //-- coming from pref setting
+  prefUnitFormat: any = 'dunit_Metric'; //-- coming from pref setting
+  vehicleGrpDD: any = [];
+  selectionTab: any;
+  startDateValue: any = 0;
+  endDateValue: any = 0;
+  last3MonthDate: any;
+  todayDate: any;
+  vehicleDD: any = [];
+  searchExpandPanel: boolean = true;
   
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder, 
+              private translationService: TranslationService,
+              private organizationService: OrganizationService,
+              @Inject(MAT_DATE_FORMATS) private dateFormats) { }
 
   ngOnInit(): void {
+    this.fleetFuelSearchData = JSON.parse(localStorage.getItem("globalSearchFilterData"));
+    // console.log("----globalSearchFilterData---",this.fleetUtilizationSearchData)
+    this.localStLanguage = JSON.parse(localStorage.getItem("language"));
+    this.accountOrganizationId = localStorage.getItem('accountOrganizationId') ? parseInt(localStorage.getItem('accountOrganizationId')) : 0;
+    this.accountId = localStorage.getItem('accountId') ? parseInt(localStorage.getItem('accountId')) : 0;
+    this.accountPrefObj = JSON.parse(localStorage.getItem('accountInfo'));
+    this.tripForm = this._formBuilder.group({
+      vehicleGroup: ['', [Validators.required]],
+      vehicle: ['', [Validators.required]],
+      startDate: ['', []],
+      endDate: ['', []],
+      startTime: ['', []],
+      endTime: ['', []]
+    });
+    let translationObj = {
+      id: 0,
+      code: this.localStLanguage ? this.localStLanguage.code : "EN-GB",
+      type: "Menu",
+      name: "",
+      value: "",
+      filter: "",
+      menuId: 10 //-- for fleet utilisation
+    }
+    this.translationService.getMenuTranslations(translationObj).subscribe((data: any) => {
+      this.processTranslation(data);
+      this.translationService.getPreferences(this.localStLanguage.code).subscribe((prefData: any) => {
+        if(this.accountPrefObj.accountPreference && this.accountPrefObj.accountPreference != ''){ // account pref
+          this.proceedStep(prefData, this.accountPrefObj.accountPreference);
+        }else{ // org pref
+          this.organizationService.getOrganizationPreference(this.accountOrganizationId).subscribe((orgPref: any)=>{
+            this.proceedStep(prefData, orgPref);
+          }, (error) => { // failed org API
+            let pref: any = {};
+            this.proceedStep(prefData, pref);
+          });
+        }
+      });
+    });
+  }
+
+  onSearch(){
+  }
+
+  processTranslation(transData: any) {
+    this.translationData = transData.reduce((acc, cur) => ({ ...acc, [cur.name]: cur.value }), {});
+    setTimeout(() =>{
+      // this.setPDFTranslations();
+    }, 0);
+  }
+
+  proceedStep(prefData: any, preference: any){
+    let _search = prefData.timeformat.filter(i => i.id == preference.timeFormatId);
+    if(_search.length > 0){
+      this.prefTimeFormat = parseInt(_search[0].value.split(" ")[0]);
+      this.prefTimeZone = prefData.timezone.filter(i => i.id == preference.timezoneId)[0].value;
+      this.prefDateFormat = prefData.dateformat.filter(i => i.id == preference.dateFormatTypeId)[0].name;
+      this.prefUnitFormat = prefData.unit.filter(i => i.id == preference.unitId)[0].name;  
+    }else{
+      this.prefTimeFormat = parseInt(prefData.timeformat[0].value.split(" ")[0]);
+      this.prefTimeZone = prefData.timezone[0].value;
+      this.prefDateFormat = prefData.dateformat[0].name;
+      this.prefUnitFormat = prefData.unit[0].name;
+    }
+    this.setDefaultStartEndTime();
+    this.setPrefFormatDate();
+    this.setDefaultTodayDate();
+    // this.getFleetPreferences();
+  }
+
+  setDefaultStartEndTime()
+  {
+  if(!this.internalSelection && this.fleetFuelSearchData.modifiedFrom !== "" &&  ((this.fleetFuelSearchData.startTimeStamp || this.fleetFuelSearchData.endTimeStamp) !== "") ) {
+    if(this.prefTimeFormat == this.fleetFuelSearchData.filterPrefTimeFormat){ // same format
+      this.selectedStartTime = this.fleetFuelSearchData.startTimeStamp;
+      this.selectedEndTime = this.fleetFuelSearchData.endTimeStamp;
+      this.startTimeDisplay = (this.prefTimeFormat == 24) ? `${this.fleetFuelSearchData.startTimeStamp}:00` : this.fleetFuelSearchData.startTimeStamp;
+      this.endTimeDisplay = (this.prefTimeFormat == 24) ? `${this.fleetFuelSearchData.endTimeStamp}:59` : this.fleetFuelSearchData.endTimeStamp;  
+    }else{ // different format
+      if(this.prefTimeFormat == 12){ // 12
+        this.selectedStartTime = this._get12Time(this.fleetFuelSearchData.startTimeStamp);
+        this.selectedEndTime = this._get12Time(this.fleetFuelSearchData.endTimeStamp);
+        this.startTimeDisplay = this.selectedStartTime; 
+        this.endTimeDisplay = this.selectedEndTime;
+      }else{ // 24
+        this.selectedStartTime = this.get24Time(this.fleetFuelSearchData.startTimeStamp);
+        this.selectedEndTime = this.get24Time(this.fleetFuelSearchData.endTimeStamp);
+        this.startTimeDisplay = `${this.selectedStartTime}:00`; 
+        this.endTimeDisplay = `${this.selectedEndTime}:59`;
+      }
+    }
+  }else {
+    if(this.prefTimeFormat == 24){
+      this.startTimeDisplay = '00:00:00';
+      this.endTimeDisplay = '23:59:59';
+      this.selectedStartTime = "00:00";
+      this.selectedEndTime = "23:59";
+    } else{
+      this.startTimeDisplay = '12:00 AM';
+      this.endTimeDisplay = '11:59 PM';
+      this.selectedStartTime = "00:00";
+      this.selectedEndTime = "23:59";
+    }
+  }
+}
+
+_get12Time(_sTime: any){
+  let _x = _sTime.split(':');
+  let _yy: any = '';
+  if(_x[0] >= 12){ // 12 or > 12
+    if(_x[0] == 12){ // exact 12
+      _yy = `${_x[0]}:${_x[1]} PM`;
+    }else{ // > 12
+      let _xx = (_x[0] - 12);
+      _yy = `${_xx}:${_x[1]} PM`;
+    }
+  }else{ // < 12
+    _yy = `${_x[0]}:${_x[1]} AM`;
+  }
+  return _yy;
+}
+
+get24Time(_time: any){
+  let _x = _time.split(':');
+  let _y = _x[1].split(' ');
+  let res: any = '';
+  if(_y[1] == 'PM'){ // PM
+    let _z: any = parseInt(_x[0]) + 12;
+    res = `${(_x[0] == 12) ? _x[0] : _z}:${_y[0]}`;
+  }else{ // AM
+    res = `${_x[0]}:${_y[0]}`;
+  }
+  return res;
+}
+
+setPrefFormatDate(){
+  switch(this.prefDateFormat){
+    case 'ddateformat_dd/mm/yyyy': {
+      this.dateFormats.display.dateInput = "DD/MM/YYYY";
+      break;
+    }
+    case 'ddateformat_mm/dd/yyyy': {
+      this.dateFormats.display.dateInput = "MM/DD/YYYY";
+      break;
+    }
+    case 'ddateformat_dd-mm-yyyy': {
+      this.dateFormats.display.dateInput = "DD-MM-YYYY";
+      break;
+    }
+    case 'ddateformat_mm-dd-yyyy': {
+      this.dateFormats.display.dateInput = "MM-DD-YYYY";
+      break;
+    }
+    default:{
+      this.dateFormats.display.dateInput = "MM/DD/YYYY";
+    }
+  }
+}
+
+setDefaultTodayDate(){
+  if(!this.internalSelection && this.fleetFuelSearchData.modifiedFrom !== "") {
+    //console.log("---if fleetUtilizationSearchData startDateStamp exist")
+    if(this.fleetFuelSearchData.timeRangeSelection !== ""){
+      this.selectionTab = this.fleetFuelSearchData.timeRangeSelection;
+    }else{
+      this.selectionTab = 'today';
+    }
+    let startDateFromSearch = new Date(this.fleetFuelSearchData.startDateStamp);
+    let endDateFromSearch = new Date(this.fleetFuelSearchData.endDateStamp);
+    this.startDateValue = this.setStartEndDateTime(startDateFromSearch, this.selectedStartTime, 'start');
+    this.endDateValue = this.setStartEndDateTime(endDateFromSearch, this.selectedEndTime, 'end');
+  }else{
+  this.selectionTab = 'today';
+  this.startDateValue = this.setStartEndDateTime(this.getTodayDate(), this.selectedStartTime, 'start');
+  this.endDateValue = this.setStartEndDateTime(this.getTodayDate(), this.selectedEndTime, 'end');
+  this.last3MonthDate = this.getLast3MonthDate();
+  this.todayDate = this.getTodayDate();
+  }
+}
+
+setStartEndDateTime(date: any, timeObj: any, type: any){
+
+    if(type == "start"){
+      console.log("--date type--",date)
+      console.log("--date type--",timeObj)
+      // this.fleetUtilizationSearchData["startDateStamp"] = date;
+      // this.fleetUtilizationSearchData.testDate = date;
+      // this.fleetUtilizationSearchData["startTimeStamp"] = timeObj;
+      // this.setGlobalSearchData(this.fleetUtilizationSearchData)
+      // localStorage.setItem("globalSearchFilterData", JSON.stringify(this.globalSearchFilterData));
+      // console.log("---time after function called--",timeObj)
+    }else if(type == "end") {
+      // this.fleetUtilizationSearchData["endDateStamp"] = date;
+      // this.fleetUtilizationSearchData["endTimeStamp"] = timeObj;
+      // this.setGlobalSearchData(this.fleetUtilizationSearchData)
+      // localStorage.setItem("globalSearchFilterData", JSON.stringify(this.globalSearchFilterData));
+    }
+
+    let _x = timeObj.split(":")[0];
+    let _y = timeObj.split(":")[1];
+    if(this.prefTimeFormat == 12){
+      if(_y.split(' ')[1] == 'AM' && _x == 12) {
+        date.setHours(0);
+      }else{
+        date.setHours(_x);
+      }
+      date.setMinutes(_y.split(' ')[0]);
+    }else{
+      date.setHours(_x);
+      date.setMinutes(_y);
+    }
+    date.setSeconds(type == 'start' ? '00' : '59');
+    return date;
+  }
+
+  getTodayDate(){
+    let _todayDate: any = Util.getUTCDate(this.prefTimeZone);
+    return _todayDate;
+    //let todayDate = new Date();
+    // let _date = moment.utc(todayDate.getTime());
+    // let _tz = moment.utc().tz('Europe/London');
+    // let __tz = moment.utc(todayDate.getTime()).tz('Europe/London').isDST();
+    // var timedifference = new Date().getTimezoneOffset(); //-- difference from the clients timezone from UTC time.
+    // let _tzOffset = this.getUtcOffset(todayDate);
+    // let dt = moment(todayDate).toDate();
+  }
+
+getLast3MonthDate(){
+    // let date = new Date();
+    var date = Util.getUTCDate(this.prefTimeZone);
+    date.setMonth(date.getMonth()-3);
+    return date;
+  }
+
+  onReset(){
+    
+  }
+  
+  endTimeChanged(selectedTime: any) {
+    this.internalSelection = true;
+    this.selectedEndTime = selectedTime;
+    if(this.prefTimeFormat == 24){
+      this.endTimeDisplay = selectedTime + ':59';
+    }
+    else{
+      this.endTimeDisplay = selectedTime;
+    }
+    this.endDateValue = this.setStartEndDateTime(this.endDateValue, this.selectedEndTime, 'end');
+    this.resetTripFormControlValue(); // extra addded as per discuss with Atul
+    this.filterDateData();
+  }
+
+  resetTripFormControlValue(){
+    if(!this.internalSelection && this.fleetFuelSearchData.modifiedFrom !== ""){
+      this.tripForm.get('vehicle').setValue(this.fleetFuelSearchData.vehicleDropDownValue);
+      this.tripForm.get('vehicleGroup').setValue(this.fleetFuelSearchData.vehicleGroupDropDownValue);
+    }else{
+      this.tripForm.get('vehicle').setValue(0);
+      this.tripForm.get('vehicleGroup').setValue(0);
+      // this.fleetUtilizationSearchData["vehicleGroupDropDownValue"] = 0;
+      // this.fleetUtilizationSearchData["vehicleDropDownValue"] = '';
+      // this.setGlobalSearchData(this.fleetUtilizationSearchData);
+    }
+  }
+
+  filterDateData(){
+  }
+
+  onVehicleGroupChange(event: any){
+  }
+
+  onVehicleChange(event: any){
+    // this.internalSelection = true; 
+  }
+
+  changeEndDateEvent(event: MatDatepickerInputEvent<any>){
+    //this.endDateValue = event.value._d;
+    this.internalSelection = true;
+    this.endDateValue = this.setStartEndDateTime(event.value._d, this.selectedEndTime, 'end');
+    this.resetTripFormControlValue(); // extra addded as per discuss with Atul
+    this.filterDateData(); // extra addded as per discuss with Atul
+  }
+  
+  startTimeChanged(selectedTime: any) {
+    this.internalSelection = true;
+    this.selectedStartTime = selectedTime;
+    if(this.prefTimeFormat == 24){
+      this.startTimeDisplay = selectedTime + ':00';
+    }
+    else{
+      this.startTimeDisplay = selectedTime;
+    }
+    this.startDateValue = this.setStartEndDateTime(this.startDateValue, this.selectedStartTime, 'start');
+    this.resetTripFormControlValue(); // extra addded as per discuss with Atul
+    this.filterDateData();// extra addded as per discuss with Atul
+  }
+
+  changeStartDateEvent(event: MatDatepickerInputEvent<any>){
+    this.internalSelection = true;
+    //this.startDateValue = event.value._d;
+    this.startDateValue = this.setStartEndDateTime(event.value._d, this.selectedStartTime, 'start');
+    this.resetTripFormControlValue(); // extra addded as per discuss with Atul
+    this.filterDateData(); // extra addded as per discuss with Atul
+  }
+
+  selectionTimeRange(selection: any){
+    this.internalSelection = true;
+    switch(selection){
+      case 'today': {
+        this.selectionTab = 'today';
+        this.setDefaultStartEndTime();
+        this.startDateValue = this.setStartEndDateTime(this.getTodayDate(), this.selectedStartTime, 'start');
+        this.endDateValue = this.setStartEndDateTime(this.getTodayDate(), this.selectedEndTime, 'end');
+        break;
+      }
+      case 'yesterday': {
+        this.selectionTab = 'yesterday';
+        this.setDefaultStartEndTime();
+        this.startDateValue = this.setStartEndDateTime(this.getYesterdaysDate(), this.selectedStartTime, 'start');
+        this.endDateValue = this.setStartEndDateTime(this.getYesterdaysDate(), this.selectedEndTime, 'end');
+        break;
+      }
+      case 'lastweek': {
+        this.selectionTab = 'lastweek';
+        this.setDefaultStartEndTime();
+        this.startDateValue = this.setStartEndDateTime(this.getLastWeekDate(), this.selectedStartTime, 'start');
+        this.endDateValue = this.setStartEndDateTime(this.getYesterdaysDate(), this.selectedEndTime, 'end');
+        break;
+      }
+      case 'lastmonth': {
+        this.selectionTab = 'lastmonth';
+        this.setDefaultStartEndTime();
+        this.startDateValue = this.setStartEndDateTime(this.getLastMonthDate(), this.selectedStartTime, 'start');
+        this.endDateValue = this.setStartEndDateTime(this.getYesterdaysDate(), this.selectedEndTime, 'end');
+        break;
+      }
+      case 'last3month': {
+        this.selectionTab = 'last3month';
+        this.setDefaultStartEndTime();
+        this.startDateValue = this.setStartEndDateTime(this.getLast3MonthDate(), this.selectedStartTime, 'start');
+        this.endDateValue = this.setStartEndDateTime(this.getYesterdaysDate(), this.selectedEndTime, 'end');
+        break;
+      }
+    }
+    // this.fleetUtilizationSearchData["timeRangeSelection"] = this.selectionTab;
+    // this.setGlobalSearchData(this.fleetUtilizationSearchData);
+    this.resetTripFormControlValue(); // extra addded as per discuss with Atul
+    this.filterDateData(); // extra addded as per discuss with Atul
+  }
+
+  getYesterdaysDate() {
+    //var date = new Date();
+    var date = Util.getUTCDate(this.prefTimeZone);
+    date.setDate(date.getDate()-1);
+    return date;
+  }
+
+  getLastWeekDate() {
+    // var date = new Date();
+    var date = Util.getUTCDate(this.prefTimeZone);
+    date.setDate(date.getDate()-7);
+    return date;
+  }
+
+  getLastMonthDate(){
+    // let date = new Date();
+    var date = Util.getUTCDate(this.prefTimeZone);
+    date.setMonth(date.getMonth()-1);
+    return date;
   }
 
 }
