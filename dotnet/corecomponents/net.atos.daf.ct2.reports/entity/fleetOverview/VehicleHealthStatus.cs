bcf331f616affef59788eaa56f54427705ee1c0c
Kumari, Archana <archana.kumari@atos.net>
2021-07-01 11:12:07 +0530

    VehicleHealthStatus response entity

--- dotnet/corecomponents/net.atos.daf.ct2.reports/entity/fleetOverview/VehicleHealthStatus.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.reports/entity/fleetOverview/VehicleHealthStatus.cs
@@ -1,10 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace net.atos.daf.ct2.reports.entity.fleetOverview
 {
+    public class Summary
+    {
+        public string VIN { get; set; }
+        public string VehicleName { get; set; }
+        public string VehicleRegNo { get; set; }
+        public string DrivingStatus { get; set; }
+        public int Alert { get; set; }
+        public string Address { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string WarningType { get; set; }
+    }
+
+    public class CurrentWarning
+    {
+        public string Name { get; set; }
+        public string ActivatedTime { get; set; }
+        public string DriverName { get; set; }
+        public string Advice { get; set; }
+        public string DeactivatedTime { get; set; }
+    }
+
+    public class Current
+    {
+        public Summary Summary { get; set; }
+        public List<CurrentWarning> CurrentWarning { get; set; }
+    }
+
+    public class WarningHistory
+    {
+        public string Name { get; set; }
+        public string ActivatedTime { get; set; }
+        public string DeactivatedTime { get; set; }
+        public string DriverName { get; set; }
+        public string Advice { get; set; }
+    }
+
+    public class Hitory
+    {
+        public Summary Summary { get; set; }
+        public List<WarningHistory> WarningHistory { get; set; }
+    }
+
     public class VehicleHealthStatus
     {
+        public Current Current { get; set; }
+        public Hitory Hitory { get; set; }
     }
+
+
 }
