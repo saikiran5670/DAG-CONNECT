689bd6ad984d5a858c467610c48a5d980ff74073
sharayumadane29 <sharayu.madane@atos.net>
2021-07-02 14:44:34 +0530

    EcoScore profile management component added

---
+++ ui/src/app/configuration/eco-score-profile-management/eco-score-profile-management.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eco-score-profile-management',
+  templateUrl: './eco-score-profile-management.component.html',
+  styleUrls: ['./eco-score-profile-management.component.less']
+})
+export class EcoScoreProfileManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
