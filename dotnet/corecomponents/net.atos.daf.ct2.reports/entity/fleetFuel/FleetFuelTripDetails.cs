1435612a808339a37f1960277b447df6abb94fa6
Shailesh Sangekar <shailesh.sangekar@atos.net>
2021-07-05 12:22:35 +0000

    Fleet Fueld report Updated Model

--- dotnet/corecomponents/net.atos.daf.ct2.reports/entity/fleetFuel/FleetFuelTripDetails.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.reports/entity/fleetFuel/FleetFuelTripDetails.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace net.atos.daf.ct2.reports.entity.fleetFuel
 {
-   public class FleetFuelTripDetails : FleetFuelDetails
-    {    
+    public class FleetFuelTripDetails : FleetFuelDetails
+    {
         public double StartPositionLattitude { get; set; }
         public double StartPositionLongitude { get; set; }
         public double EndPositionLattitude { get; set; }
-        public double EndPositionLongitude { get; set; }     
+        public double EndPositionLongitude { get; set; }
         public string StartPosition { get; set; }
         public string EndPosition { get; set; }
     }
