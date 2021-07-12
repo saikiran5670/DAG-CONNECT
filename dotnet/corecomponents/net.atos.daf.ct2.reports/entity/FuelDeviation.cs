af9d2ab55251dd3f245eb4d4a90a85a47a3a214f
Harneet Rekhi <harneet.rekhi@atos.net>
2021-07-12 06:53:08 +0000

    Fuel Deviation common

---
+++ dotnet/corecomponents/net.atos.daf.ct2.reports/entity/FuelDeviation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.atos.daf.ct2.reports.entity
+{
+    public class FuelDeviation
+    {
+        #region trip statistic and Vehicle fields  
+        public int Id { get; set; }
+        public string TripId { get; set; }
+        public string VIN { get; set; }
+
+        public long StartTimeStamp { get; set; }
+        public long EndTimeStamp { get; set; }
+
+        public int Distance { get; set; }
+        public int IdleDuration { get; set; }
+
+        public int AverageSpeed { get; set; }
+        public int AverageWeight { get; set; }
+
+        public string StartPosition { get; set; }
+        public string EndPosition { get; set; }
+
+        public double FuelConsumed { get; set; }
+        public int DrivingTime { get; set; }
+
+        public int Alert { get; set; }
+        public int Events { get; set; }
+        public double FuelConsumed100km { get; set; }
+
+        public double StartPositionLattitude { get; set; }
+        public double StartPositionLongitude { get; set; }
+        public double EndPositionLattitude { get; set; }
+        public double EndPositionLongitude { get; set; }
+        public string RegistrationNo { get; set; }
+        public string VehicleName { get; set; }
+        #endregion
+
+        #region livefleet_trip_fuel_deviation    
+        public char FuelEventType { get; set; }
+        public char VehicleActivityType { get; set; }
+        public long EventTime { get; set; }
+        public double FuelDiffernce { get; set; }
+        public long Odometer { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Heading { get; set; }
+        #endregion
+    }
+}
