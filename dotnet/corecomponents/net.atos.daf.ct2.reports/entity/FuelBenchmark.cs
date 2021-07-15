dce58237fb380b181f8333ecfe3d8ef6e25fe17b
Pratap, Ram <ram.pratap@atos.net>
2021-07-15 04:43:05 +0000

    Modify the benchmark model class

--- dotnet/corecomponents/net.atos.daf.ct2.reports/entity/FuelBenchmark.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.reports/entity/FuelBenchmark.cs
@@ -7,17 +7,14 @@ namespace net.atos.daf.ct2.reports.entity
     public class FuelBenchmark
     {
         public string VIN { get; set; }
-        public string Vehicle_Group_Name { get; set; }
-        public int Vehicle_group_id { get; set; }
-        public int Number_of_Active_Vehicles { get; set; }
-        public int Number_of_Total_Vehicles { get; set; }
-        public string Total_fuel_consumed { get; set; }
-        public string Total_Mileage { get; set; }
-        public string Average_fuel_consumption { get; set; }
+        public string VehicleGroupName { get; set; }
+        public int VehicleGroupId { get; set; }
+        public int NumberOfActiveVehicles { get; set; }
+        public int NumberOfTotalVehicles { get; set; }
+        public string TotalFuelConsumed { get; set; }
+        public string TotalMileage { get; set; }
+        public string AverageFuelConsumption { get; set; }
         public List<Ranking> VehicleRanking { get; set; }
-        public string Fuel_consumption_High { get; set; }
-        public string Fuel_consumption_Medium { get; set; }
-        public string Fuel_consumption_Low { get; set; }
         public long StartDate { get; set; }
         public long EndDate { get; set; }
 
@@ -34,9 +31,9 @@ namespace net.atos.daf.ct2.reports.entity
 
     public class FuelBenchmarkConsumptionParameter
     {
-        public long FromDate { get; set; }
-        public long ToDate { get; set; }
-        public string Vin { get; set; }
+        public long StartDateTime { get; set; }
+        public long EndDateTime { get; set; }
+        public List<string> VINs { get; set; }
     }
 
 }
