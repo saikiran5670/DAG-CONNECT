e4670dbb54abaf46a2be45fc48135a1f35bc06aa
Amar Nath Roy <amar-nath.roy@atos.net>
2021-07-16 10:09:22 +0000

    Change for Fuelbenchmark time filter

--- dotnet/portalserver/net.atos.daf.ct2.portalservice/Entity/Report/ReportFuelBenchmarkFilter.cs
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Entity/Report/ReportFuelBenchmarkFilter.cs
@@ -17,5 +17,17 @@ namespace net.atos.daf.ct2.portalservice.Entity.Report
 
         public int VehicleGroupId { get; set; }
 
+        public class ReportFuelBenchmarkTimePeriodFilter
+        {
+            [Required]
+            public long StartDateTime { get; set; }
+            [Required]
+            public long EndDateTime { get; set; }
+            public List<string> VINs { get; set; }
+
+            public int VehicleGroupId { get; set; }
+
+        }
+
     }
 }
