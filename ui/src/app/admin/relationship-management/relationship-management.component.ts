5fc7f9a899f75f8249cc14408cd72d66507e8527
narisettytarakram <narisetty.tarakram@atos.net>
2021-07-19 09:29:19 +0530

    11369-sorting applied for Relationship Management

--- ui/src/app/admin/relationship-management/relationship-management.component.ts
+++ ui/src/app/admin/relationship-management/relationship-management.component.ts
@@ -131,9 +131,23 @@ export class RelationshipManagementComponent implements OnInit {
     setTimeout(()=>{
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
+      this.dataSource.sortData = (data : String[], sort: MatSort) => {
+        const isAsc = sort.direction === 'asc';
+        let columnName = this.sort.active;
+        return data.sort((a: any, b: any) => {
+          return this.compare(a[sort.active], b[sort.active], isAsc, columnName);
+        });
+      }
     });
   }
 
+  compare(a: Number | String, b: Number | String, isAsc: boolean, columnName: any) {
+    if("name"){
+      if(!(a instanceof Number)) a = a.toString().toUpperCase();
+      if(!(b instanceof Number)) b = b.toString().toUpperCase();
+    }
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1); 
+  }
   getNewTagData(data: any){
     let currentDate = new Date().getTime();
     if(data.length > 0){
