9ff0fe16578346a4a00eec418c4e55715c29a7fb
Ravi Ellapalayam Murugasamy <ravi.ellapalayammurugasamy@atos.net>
2021-07-22 16:53:49 +0530

    code changes for #13789

--- ui/src/app/live-fleet/current-fleet/fleet-overview-summary/fleet-overview-summary.component.ts
+++ ui/src/app/live-fleet/current-fleet/fleet-overview-summary/fleet-overview-summary.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
-import { Color, Label, MultiDataSet, SingleDataSet, SingleOrMultiDataSet } from 'ng2-charts'
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 import { Subscription } from 'rxjs';
 import { MessageService } from 'src/app/services/message.service';
+import { ReportService } from 'src/app/services/report.service';
 import { Util } from 'src/app/shared/util';
 
 @Component({
@@ -13,13 +14,14 @@ import { Util } from 'src/app/shared/util';
 export class FleetOverviewSummaryComponent implements OnInit {
   @Input() translationData: any=[];
   criticalAlert: number = 0;
-  mileageDone: number = 0;
+  mileageDone: string;
   drivers: number = 0;
   driveTime: any;
   noAction: number = 0;
   serviceNow: number = 0;
   stopNow: number = 0;
-  barChartLabels: Label[] = ['Moved Vehicle', 'Total Vehicle'];
+  vehicleGroup: string;
+  barChartLabels: Label[] = [(this.translationData.lblMovedVehicle || 'Moved Vehicle'), (this.translationData.lblTotalVehicle || 'Total Vehicle')];
   barChartType: ChartType = 'bar';
   barChartLegend = true;
   barChartPlugins = [];
@@ -27,26 +29,57 @@ export class FleetOverviewSummaryComponent implements OnInit {
   doughnutChartType: ChartType = 'doughnut';
   messages: any[] = [];
   subscription: Subscription;
-  isToday: boolean = false;
   summaryData: any=[];
   movedVehicle: number = 0;
   totalVehicle: number = 0;
-  // @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+  mileageRate: number = 0;
+  utilizationRate: number = 0;
+  //preferemces value
+  prefUnitFormat: any = 'dunit_Metric';
+  unitValkm: string;
+  filterInvoked: boolean = false;
 
-  constructor(private messageService: MessageService) {
+  constructor(private messageService: MessageService, private reportService: ReportService) {
+    this.unitValkm = (this.prefUnitFormat == 'dunit_Metric') ? (this.translationData.lblkm || 'km') : (this.prefUnitFormat == 'dunit_Imperial') ? (this.translationData.lblmile || 'mile') : (this.translationData.lblmile || 'mile');
+    this.loadData();
     this.subscription = this.messageService.getMessage().subscribe(message => {
       if (message.key.indexOf("refreshData") < 0 && message.key.indexOf("refreshTimer") < 0) {
-        this.summaryData = message.key;
-        this.refreshData();
-        if(message.key.indexOf("true") !== -1)
-          this.isToday = true;
-        else
-          this.isToday = false;
+        this.filterInvoked = true;
+        this.vehicleGroup = message.key[0].vehicleGroup;
+        if(message.key[0].vehicleGroup && message.key[0].vehicleGroup === 'all')
+          this.vehicleGroup +=' Groups';
+        if(JSON.stringify(message.key[0].data).indexOf("No Result Found") !== -1){
+          this.resetSummary();
+        } else {
+          this.summaryData = message.key[0].data;
+          this.refreshData();
+        }
+      } else if (!this.filterInvoked && message.key.indexOf("refreshData") !== -1){
+        this.loadData();
       }
     });
   }
 
-  ngOnInit(): void { 
+  ngOnInit(): void { }
+
+  loadData(){
+    let localStLanguage = JSON.parse(localStorage.getItem("language"));
+    let objData = {
+      "groupId": ['all'],
+      "alertLevel": ['all'],
+      "alertCategory": ['all'],
+      "healthStatus": ['all'],
+      "otherFilter": ['all'],
+      "driverId": ['all'],
+      "days": 90,
+      "languagecode": localStLanguage.code
+    }
+    this.reportService.getFleetOverviewDetails(objData).subscribe((data:any) => {
+      this.summaryData = data;
+      this.refreshData();
+    }, (error) => {
+      this.resetSummary();
+    });
   }
 
   barChartOptions: ChartOptions = {
@@ -83,14 +116,14 @@ export class FleetOverviewSummaryComponent implements OnInit {
     { data: [this.movedVehicle, this.totalVehicle], label: '', barThickness: 16, barPercentage: 0.5 }
   ];
 
-  // events
-  chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
+  // // events
+  // chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+  //   console.log(event, active);
+  // }
 
-  chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
-    console.log(event, active);
-  }
+  // chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+  //   console.log(event, active);
+  // }
   
   barChartColors: Color[] = [
     {
@@ -115,8 +148,8 @@ export class FleetOverviewSummaryComponent implements OnInit {
     }
    ];
     // Doughnut - Fleet Mileage Rate
-   doughnutChartLabelsMileage: Label[] = ['Fleet Mileage Rate', ''];
-   doughnutChartDataMileage: MultiDataSet = [ [65, 35] ];
+   doughnutChartLabelsMileage: Label[] = [(this.translationData.lblFleetMileageRate || 'Fleet Mileage Rate'), ''];
+   doughnutChartDataMileage: MultiDataSet = [ [0, 0] ];
    doughnutChartOptionsMileage: ChartOptions = {
     responsive: true,
     legend: {
@@ -133,8 +166,8 @@ export class FleetOverviewSummaryComponent implements OnInit {
   };
 
   // Doughnut - Fleet Utilization Rate
-  doughnutChartLabelsUtil: Label[] = ['Fleet Utilization Rate', '', ''];
-  doughnutChartDataUtil: MultiDataSet = [ [75, 25] ];
+  doughnutChartLabelsUtil: Label[] = [(this.translationData.lblFleetUtilizationRate || 'Fleet Utilization Rate'), '', ''];
+  doughnutChartDataUtil: MultiDataSet = [ [0, 0] ];
 
   doughnutChartOptionsUtil: ChartOptions = {
    responsive: true,
@@ -153,29 +186,33 @@ export class FleetOverviewSummaryComponent implements OnInit {
 
  refreshData(){
   let totalDriveTime=0;
+  let tripDistance=0;
   this.movedVehicle=0;
   this.noAction=0;
   this.serviceNow=0;
   this.stopNow=0;
   this.totalVehicle=0;
   this.criticalAlert=0;
-  this.mileageDone=0;
   //this.mileageDone = this.summaryData.reduce((val, elem) => val + elem.tripDistance, 0);
   if(this.summaryData){
     let drivers = this.summaryData.filter((elem) => elem.driver1Id);
     let uniqueDrivers = [...new Set(drivers)];
     this.drivers = uniqueDrivers.length;
+
     let vins = this.summaryData.filter((elem) => elem.vin);
     let uniqueVin = [...new Set(vins)];
     this.totalVehicle = uniqueVin.length;
+
     this.summaryData.forEach(element => {
       totalDriveTime += element.drivingTime;
       if(element.tripDistance){
-        this.mileageDone += element.tripDistance;
+        tripDistance += element.tripDistance;
       }
+
       if(element.vehicleDrivingStatusType && element.vehicleDrivingStatusType === 'D'){
         this.movedVehicle += 1;
       }
+
       if(element.vehicleHealthStatusType){
         if(element.vehicleHealthStatusType === 'N'){
           this.noAction += 1;
@@ -187,13 +224,39 @@ export class FleetOverviewSummaryComponent implements OnInit {
         if(element.latestWarningType && element.latestWarningType === 'C')
           this.criticalAlert += 1;
       }
+
+      if(element.tripDistance){
+        tripDistance += element.tripDistance;
+      }
     });
   }
-  this.driveTime = Util.getHhMmTime(totalDriveTime);
+  this.mileageDone = (this.prefUnitFormat == 'dunit_Metric' ? tripDistance : (tripDistance * 0.621371)) + ' ' + this.unitValkm;
+  let totDriveTime = (Util.getHhMmTime(totalDriveTime)).split(':');
+  this.driveTime = totDriveTime[0] + (this.translationData.lblhh || ' hh ') + totDriveTime[1] + (this.translationData.lblmm || ' mm');
   this.barChartData = [
     { data: [this.movedVehicle, this.totalVehicle], label: '', barThickness: 16, barPercentage: 0.5 }
   ];
-  // this.chart.chart.update();
+  //Fleet Mileage rate
+  this.mileageRate = (tripDistance/Number.parseInt(localStorage.getItem('liveFleetMileageThreshold'))) * 100;
+  this.doughnutChartDataMileage = [ [this.mileageRate, 100-this.mileageRate] ];
+  //Fleet Utilization rate
+  this.utilizationRate = (this.movedVehicle/Number.parseInt(localStorage.getItem('liveFleetUtilizationThreshold'))) * 100;
+  this.doughnutChartDataUtil = [ [this.utilizationRate, 100-this.utilizationRate] ];
+ }
+
+ resetSummary(){
+  this.movedVehicle=0;
+  this.noAction=0;
+  this.serviceNow=0;
+  this.stopNow=0;
+  this.totalVehicle=0;
+  this.criticalAlert=0;
+  this.barChartData = [ { data : [0, 0] } ];
+  this.doughnutChartDataMileage = [ [0 , 0] ];
+  this.doughnutChartDataUtil = [ [0, 0] ];
+  this.mileageDone = '00' + this.unitValkm;
+  this.driveTime = '00' + (this.translationData.lblhh || ' hh ') + '00' + (this.translationData.lblmm || ' mm');
+  this.drivers=0;
  }
 
 }
