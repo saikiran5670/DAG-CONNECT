612e878513db5dbff41afedb85c10807c654a595
Shailesh Rajput <shailesh-nathusing.rajput@atos.net>
2021-07-22 10:47:06 +0000

    consume confluent kafka component into kafka cdc

--- dotnet/commoncomponent/net.atos.daf.ct2.kafkacdc/VehicleAlertRefIntegrator.cs
+++ dotnet/commoncomponent/net.atos.daf.ct2.kafkacdc/VehicleAlertRefIntegrator.cs
@@ -4,16 +4,27 @@ using System.Text;
 using System.Threading.Tasks;
 using net.atos.daf.ct2.kafkacdc.entity;
 using net.atos.daf.ct2.kafkacdc.repository;
+using net.atos.daf.ct2.confluentkafka;
+using net.atos.daf.ct2.confluentkafka.entity;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
 
 namespace net.atos.daf.ct2.kafkacdc
 {
     public class VehicleAlertRefIntegrator : IVehicleAlertRefIntegrator
     {
-        private readonly IVehicleAlertRepository _vehicleAlertRepository;
+        private readonly IConfiguration _configuration;
 
-        public VehicleAlertRefIntegrator(IVehicleAlertRepository vehicleAlertRepository)
+        private readonly IVehicleAlertRepository _vehicleAlertRepository;
+        private readonly KafkaConfiguration _kafkaConfig;
+
+        public VehicleAlertRefIntegrator(IVehicleAlertRepository vehicleAlertRepository, IConfiguration configuration)
         {
             _vehicleAlertRepository = vehicleAlertRepository;
+            this._configuration = configuration;
+            _kafkaConfig = new KafkaConfiguration();
+            configuration.GetSection("KafkaConfiguration").Bind(_kafkaConfig);
         }
 
         public Task GetVehicleAlertRefFromAlertConfiguration(List<int> alertIds) => ExtractAndSyncVehicleAlertRefByAlertIds(alertIds);
@@ -26,7 +37,37 @@ namespace net.atos.daf.ct2.kafkacdc
             List<VehicleAlertRef> masterVehicleAlerts = await _vehicleAlertRepository.GetVehiclesFromAlertConfiguration(alertIds);
             await _vehicleAlertRepository.DeleteVehicleAlertRef(alertIds);
             await _vehicleAlertRepository.InsertVehicleAlertRef(masterVehicleAlerts);
+            foreach (VehicleAlertRef vlr in masterVehicleAlerts)
+            {
+
+                KafkaEntity kafkaEntity = new KafkaEntity()
+                {
+                    BrokerList = _kafkaConfig.EH_FQDN,
+                    ConnString = _kafkaConfig.EH_CONNECTION_STRING,
+                    Topic = _kafkaConfig.EH_NAME,
+                    Cacertlocation = _kafkaConfig.CA_CERT_LOCATION,
+                    ProducerMessage = PrepareKafkaJSON(vlr).Result
+                };
+                await KafkaConfluentWorker.Producer(kafkaEntity);
+            }
             return masterVehicleAlerts;
+        }
+        internal Task<string> PrepareKafkaJSON(VehicleAlertRef vehicleAlertRef)
+        {
+            vehicleAlertRef.State = "A";
+            Payload payload = new Payload()
+            {
+                Data = JsonConvert.SerializeObject(vehicleAlertRef),
+                Op = "I",
+                Namespace = "master.vehiclealertref",
+                Ts_ms = 0
+            };
+            VehicleAlertRefKafkaMessage vehicleAlertRefKafkaMessage = new VehicleAlertRefKafkaMessage()
+            {
+                Payload = payload,
+                Schema = new List<object>()
+            };
+            return Task.FromResult(JsonConvert.SerializeObject(vehicleAlertRefKafkaMessage));
         }
     }
 }
