8a4389b311b10fb070de5e464b8e79861b2a4945
Teli <krishna-ashok.teli.external@atos.net>
2021-07-22 11:37:14 +0530

    refresh issue fixed

--- ui/src/app/preferences/reports-preferences/reports-preferences.component.ts
+++ ui/src/app/preferences/reports-preferences/reports-preferences.component.ts
@@ -130,10 +130,11 @@ export class ReportsPreferencesComponent implements OnInit {
       reportId: this.reportListData.filter(i => i.name == 'Fleet Fuel Report')[0].id,
       attributes: [...vehicleObj, ...driverObj]
     };
-    console.log("full object", objData)
     this.reportService.updateReportUserPreference(objData).subscribe((res: any) => {
-      console.log("save res", res)
       this.updateFleetFuelPerferencesFlag({ flag: false, msg: this.getSuccessMsg() });
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
     });
   }
 
