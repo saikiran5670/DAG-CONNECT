0ea6ad03ac40c62092c95c3c72e7846bd4acabed
Prajwal Patil <prajwal.patil@atos.net>
2021-07-27 16:04:37 +0530

    Consumer method added in cdc vehicle

--- dotnet/commoncomponent/net.atos.daf.ct2.kafkacdc/IVehicleCdcManager.cs
+++ dotnet/commoncomponent/net.atos.daf.ct2.kafkacdc/IVehicleCdcManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using net.atos.daf.ct2.confluentkafka.entity;
 using net.atos.daf.ct2.kafkacdc.entity;
 
 namespace net.atos.daf.ct2.kafkacdc
@@ -7,5 +8,7 @@ namespace net.atos.daf.ct2.kafkacdc
     public interface IVehicleCdcManager
     {
         Task VehicleCdcProducer(List<VehicleCdc> vehicleCdcList, KafkaConfiguration kafkaConfiguration);
+
+        //Task VehicleCdcConsumer(KafkaEntity kafkaEntity);
     }
 }
