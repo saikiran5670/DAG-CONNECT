be44fd42ef577ea73f75a4d2f0de07c097860d0d
Shailesh Sangekar <shailesh.sangekar@atos.net>
2021-07-28 13:06:33 +0000

    Dshboard Fleet KPI initial commit

---
+++ dotnet/corecomponents/net.atos.daf.ct2.dashboard/net.atos.daf.ct2.dashboard/entity/FleetKpiFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.atos.daf.ct2.dashboard.entity
+{
+    public class FleetKpiFilter
+    {
+        public long StartDateTime { get; set; }
+        public long EndDateTime { get; set; }
+        public List<string> VINs { get; set; }
+    }
+}
--- dotnet/corecomponents/net.atos.daf.ct2.dashboard/net.atos.daf.ct2.dashboard/repository/IDashBoardRepository.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.dashboard/net.atos.daf.ct2.dashboard/repository/IDashBoardRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace net.atos.daf.ct2.dashboard.repository
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using net.atos.daf.ct2.dashboard.entity;
+
+namespace net.atos.daf.ct2.dashboard.repository
 {
     public interface IDashBoardRepository
     {
+        Task<List<FleetKpi>> GetFleetKPIDetails(FleetKpiFilter fleetKpiFilter);
     }
 }
