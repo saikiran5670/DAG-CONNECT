8cee3a7c26dfbed525b5501fd411cd5555a50a11
Pratap, Ram <ram.pratap@atos.net>
2021-07-30 11:44:27 +0000

    Add method in controller for fleetKPI in dashboard

--- dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/DashBoardController.cs
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/DashBoardController.cs
@@ -6,8 +6,9 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using net.atos.daf.ct2.dashboardservice;
 using net.atos.daf.ct2.portalservice.Common;
-using net.atos.daf.ct2.subscription.entity;
+using net.atos.daf.ct2.portalservice.Entity.Dashboard;
 using Newtonsoft.Json;
 using DashboardService = net.atos.daf.ct2.dashboardservice;
 
@@ -18,31 +19,50 @@ namespace net.atos.daf.ct2.portalservice.Controllers
     [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
     public class DashBoardController : BaseController
     {
-        #region Private Variable
 
         private readonly ILog _logger;
-        private readonly DashboardService.DashboardService.DashboardServiceClient _subscribeClient;
+        private readonly DashboardService.DashboardService.DashboardServiceClient _dashboarClient;
         private readonly AuditHelper _auditHelper;
 
-        #endregion
 
-        #region Constructor
-        public DashBoardController(DashboardService.DashboardService.DashboardServiceClient subscribeClient, AuditHelper auditHelper, IHttpContextAccessor httpContextAccessor, SessionHelper sessionHelper) : base(httpContextAccessor, sessionHelper)
+        public DashBoardController(DashboardService.DashboardService.DashboardServiceClient dashboardClient, AuditHelper auditHelper, IHttpContextAccessor httpContextAccessor, SessionHelper sessionHelper) : base(httpContextAccessor, sessionHelper)
         {
-            _subscribeClient = subscribeClient;
+            _dashboarClient = dashboardClient;
             _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
             _auditHelper = auditHelper;
         }
-        #endregion
-
 
         [HttpGet]
         [Route("fleetkpi")]
-        public async Task<IActionResult> GetFleetKpi()
+        public async Task<IActionResult> GetFleetKpi([FromBody] Entity.Dashboard.DashboardFilter request)
         {
-            throw NotImplementedException();
+            try
+            {
+                if (!(request.StartDateTime > 0)) { return BadRequest(DashboardConstant.GET_DASBHOARD_VALIDATION_STARTDATE_MSG); }
+                if (!(request.EndDateTime > 0)) { return BadRequest(DashboardConstant.GET_DASBHOARD_VALIDATION_ENDDATE_MSG); }
+                if (request.VINs.Count <= 0) { return BadRequest(DashboardConstant.GET_DASBHOARD_VALIDATION_VINREQUIRED_MSG); }
+                if (request.StartDateTime > request.EndDateTime) { return BadRequest(DashboardConstant.GET_DASBHOARD_VALIDATION_DATEMISMATCH_MSG); }
+
+                string filters = JsonConvert.SerializeObject(request);
+                FleetKpiFilterRequest objDashboardFilter = JsonConvert.DeserializeObject<FleetKpiFilterRequest>(filters);
+                _logger.Info("GetFleetKpi method in dashboard API called.");
+                var data = await _dashboarClient.GetFleetKPIDetailsAsync(objDashboardFilter);
+                if (data != null)
+                {
+                    data.Message = DashboardConstant.GET_DASBHOARD_SUCCESS_MSG;
+                    return Ok(data);
+                }
+                else
+                {
+                    return StatusCode(404, DashboardConstant.GET_DASBHOARD_FAILURE_MSG);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(null, ex);
+                return StatusCode(500, ex.Message + " " + ex.StackTrace);
+            }
         }
 
-        private Exception NotImplementedException() => throw new NotImplementedException();
     }
 }
---
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Entity/Dashboard/DashboardConstant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net.atos.daf.ct2.portalservice.Entity.Dashboard
+{
+    public static class DashboardConstant
+    {
+        public const string GET_DASBHOARD_VALIDATION_STARTDATE_MSG = "Invalid start date.";
+        public const string GET_DASBHOARD_VALIDATION_ENDDATE_MSG = "Invalid end date.";
+        public const string GET_DASBHOARD_VALIDATION_VINREQUIRED_MSG = "Invalid VIN details.";
+        public const string GET_DASBHOARD_VALIDATION_DATEMISMATCH_MSG = "Start Date should be less than End Date.";
+        public const string GET_DASBHOARD_SUCCESS_MSG = "Dashboard details fetched successfully";
+        public const string GET_DASBHOARD_FAILURE_MSG = "No Result Found";
+    }
+}
