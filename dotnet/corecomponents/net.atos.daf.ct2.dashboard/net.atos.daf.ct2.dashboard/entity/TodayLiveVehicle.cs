e143538bda028700ca9649e44c8de8a216f6c581
Manoj Nandamuri <manoj.nandamuri@atos.net>
2021-07-30 16:08:56 +0000

    Commit for repo dashboard

---
+++ dotnet/corecomponents/net.atos.daf.ct2.dashboard/net.atos.daf.ct2.dashboard/entity/TodayLiveVehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.atos.daf.ct2.dashboard.entity
+{
+    public class TodayLiveVehicleResponse
+    {
+        public double Distance { get; set; }
+        public long DrivingTime { get; set; }
+        public int VehicleCount { get; set; }
+        public int DriverCount { get; set; }
+        public int CriticleAlertCount { get; set; }
+        public int ActiveVehicles { get; set; }
+        public int TimeBaseUtilization { get; set; }
+        public int DistanceBaseUtilization { get; set; }
+    }
+
+    public class TodayLiveVehicleRequest
+    {
+        public List<string> VINs { get; set; }
+    }
+}
