df7bcee85d4703c79969ad1ff67a0c63c52ed5ee
tushar.gawande@atos.net <tushar.gawande@atos.net>
2021-07-30 15:46:06 +0530

    Dashboard controller changes

---
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/DashBoardController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using log4net;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using net.atos.daf.ct2.portalservice.Common;
+using net.atos.daf.ct2.subscription.entity;
+using Newtonsoft.Json;
+using DashboardService = net.atos.daf.ct2.dashboardservice;
+namespace net.atos.daf.ct2.portalservice.Controllers
+{
+    [ApiController]
+    [Route("subscribe")]
+    [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
+    public class DashBoardController : BaseController
+    {
+        #region Private Variable
+
+        private readonly ILog _logger;
+        private readonly DashboardService.DashboardService.DashboardServiceClient _subscribeClient;
+        private readonly AuditHelper _auditHelper;
+
+        #endregion
+
+        #region Constructor
+        public DashBoardController(DashboardService.DashboardService.DashboardServiceClient subscribeClient, AuditHelper auditHelper, IHttpContextAccessor httpContextAccessor, SessionHelper sessionHelper) : base(httpContextAccessor, sessionHelper)
+        {
+            _subscribeClient = subscribeClient;
+            _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            _auditHelper = auditHelper;
+        }
+        #endregion
+
+
+
+
+    }
+}
