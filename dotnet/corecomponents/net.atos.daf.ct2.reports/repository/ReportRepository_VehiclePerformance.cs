be743773d98a63d27bb987540a6f95fd394be9f2
Prajwal Patil <prajwal.patil@atos.net>
2021-07-31 17:32:12 +0530

    Vehicle performance repo added.

---
+++ dotnet/corecomponents/net.atos.daf.ct2.reports/repository/ReportRepository_VehiclePerformance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using net.atos.daf.ct2.reports.entity;
+
+namespace net.atos.daf.ct2.reports.repository
+{
+    public partial class ReportRepository : IReportRepository
+    {
+
+    }
+}
