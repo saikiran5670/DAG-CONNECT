a6dc822d1e86889d7307ba67121e034c3ed2d1c3
Pranali <pranali.chavan@atos.net>
2021-07-31 13:47:59 +0530

    charts updated in live vehicles

--- ui/src/app/dashboard/today-live-vehicle/today-live-vehicle.component.ts
+++ ui/src/app/dashboard/today-live-vehicle/today-live-vehicle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { BaseChartDirective, Color, Label, MultiDataSet, PluginServiceGlobalRegistrationAndOptions } from 'ng2-charts';
@@ -10,10 +10,12 @@ import { BaseChartDirective, Color, Label, MultiDataSet, PluginServiceGlobalRegi
 })
 export class TodayLiveVehicleComponent implements OnInit {
   @Input() translationData : any;
+  @ViewChild('chart1') chart1 : ElementRef;
+  liveVehicleData:any;
 
+  //Active Vehicles Chart
   doughnutChartLabels: Label[] = [('Target'), '', ''];
-
-  doughnutChartActiveVehicleData: MultiDataSet = [ [35, 65] ];
+  doughnutChartActiveVehicleData: MultiDataSet = [ [20, 80] ];
   doughnutChartType: ChartType = 'doughnut';
   doughnutColors: Color[] = [
     {
@@ -37,7 +39,7 @@ export class TodayLiveVehicleComponent implements OnInit {
     legend: {
       display: false
     },
-    cutoutPercentage: 65,
+    cutoutPercentage: 80,
     tooltips: {
       filter: function(item, data) {
         var label = data.labels[item.index];
@@ -50,10 +52,132 @@ export class TodayLiveVehicleComponent implements OnInit {
       display: false
     }
   };
+  public doughnutChartPlugins: PluginServiceGlobalRegistrationAndOptions[] = [{
+    beforeDraw(chart) {
+      const ctx = chart.ctx;
+
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      const centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+      const centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+
+      ctx.font = '500 14px Roboto, "Helvetica Neue", sans-serif';
+      ctx.fillStyle = 'black';
+
+      var text = chart.config.options.title.text;
+      // Draw text in center
+      ctx.fillText("20%", centerX, centerY);
+    }
+  }];
+
+  //Time based chart
+  doughnutChartTimeBasedLabels: Label[] = [('Target'), '', ''];
+  doughnutChartTimeBasedData: MultiDataSet = [ [0, 100] ];
+  
+   doughnutChartTimeOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+    tooltips: {
+      filter: function(item, data) {
+        var label = data.labels[item.index];
+        if (label) return true;
+        return false;
+      }
+    },
+    title:{
+      text: "15",
+      display: false
+    }
+  };
+  public doughnutChartTimePlugins: PluginServiceGlobalRegistrationAndOptions[] = [{
+    beforeDraw(chart) {
+      const ctx = chart.ctx;
+
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      const centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+      const centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+
+      ctx.font = '500 14px Roboto, "Helvetica Neue", sans-serif';
+      ctx.fillStyle = 'black';
+
+      var text = chart.config.options.title.text;
+      // Draw text in center
+      ctx.fillText("0%", centerX, centerY);
+    }
+  }];
+
+//Distance based chart
+doughnutChartDistanceBasedLabels: Label[] = [('Target'), '', ''];
+doughnutChartDistanceBasedData: MultiDataSet = [ [2, 98] ];
+
+ doughnutChartDistanceOptions: ChartOptions = {
+  responsive: true,
+  legend: {
+    display: false
+  },
+  cutoutPercentage: 80,
+  tooltips: {
+    filter: function(item, data) {
+      var label = data.labels[item.index];
+      if (label) return true;
+      return false;
+    }
+  },
+  title:{
+    text: "15",
+    display: false
+  }
+};
+public doughnutChartDistancePlugins: PluginServiceGlobalRegistrationAndOptions[] = [{
+  beforeDraw(chart) {
+    const ctx = chart.ctx;
+
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    const centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+    const centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+
+    ctx.font = '500 14px Roboto, "Helvetica Neue", sans-serif';
+    ctx.fillStyle = 'black';
+
+    var text = chart.config.options.title.text;
+    // Draw text in center
+    ctx.fillText("2%", centerX, centerY);
+  }
+}];
+
   constructor(private router : Router) { }
 
   ngOnInit(): void {
+    this.getLiveVehicleData();
+    //this.setChartData();
+
   }
+
+  getLiveVehicleData(){
+    this.liveVehicleData = {
+      "VechicleCount": 0,
+      "Distance": 0,
+      "DrivingTime": 0,
+      "Drivers": 0,
+      "CriticalAlert": 0,
+      "ActiveVehicles": 10,
+      "TimeUtilization": 0,
+      "DistanceUtilization": 20
+    }
+  }
+
+  setChartData(){
+    this.chart1.nativeElement.style.height = '10px';
+    this.chart1.nativeElement.style.width = '10px';
+
+  }
+
+  
 
   navigateToReport(){
     this.router.navigate(['/report/fleetutilisation']);
