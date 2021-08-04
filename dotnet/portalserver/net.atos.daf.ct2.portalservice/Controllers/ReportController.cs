f72158d484b15b7fdec8ed23c7b2bb5a754f24de
Prajwal Patil <prajwal.patil@atos.net>
2021-08-04 14:33:52 +0530

    Changes in getVeh performance type

--- dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/ReportController.cs
+++ dotnet/portalserver/net.atos.daf.ct2.portalservice/Controllers/ReportController.cs
@@ -1624,12 +1624,12 @@ namespace net.atos.daf.ct2.portalservice.Controllers
 
                 string filters = JsonConvert.SerializeObject(vehiclePerformanceFilter);
                 BubbleChartDataRequest objVehPerformanceFilter = JsonConvert.DeserializeObject<BubbleChartDataRequest>(filters);
-                var data = await _reportServiceClient.GetVehPerformanceBubbleChartDataAsync(objVehPerformanceFilter);
+                var response = await _reportServiceClient.GetVehPerformanceBubbleChartDataAsync(objVehPerformanceFilter);
 
-                if (data?.BubbleChartData != null)
+                if (response != null)
                 {
-                    data.Message = ReportConstants.GET_VEHICLE_PERFORMANCE_SUCCESS_MSG;
-                    return Ok(data);
+                    response.Message = ReportConstants.GET_VEHICLE_PERFORMANCE_SUCCESS_MSG;
+                    return Ok(response);
                 }
                 else
                 {
