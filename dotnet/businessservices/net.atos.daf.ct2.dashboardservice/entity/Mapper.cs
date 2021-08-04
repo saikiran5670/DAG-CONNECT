60d64df4ef104f9abbff89fb58015a885c779175
Amar Nath Roy <amar-nath.roy@atos.net>
2021-08-04 09:57:47 +0000

    Add the User Preference create request for dashboard

---
+++ dotnet/businessservices/net.atos.daf.ct2.dashboardservice/entity/Mapper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using net.atos.daf.ct2.dashboard.entity;
+using net.atos.daf.ct2.reports.ENUM;
+
+namespace net.atos.daf.ct2.dashboardservice.entity
+{
+    public class Mapper
+    {
+        internal dashboard.entity.DashboardUserPreferenceCreateRequest MapCreateDashboardUserPreferences(DashboardUserPreferenceCreateRequest request)
+        {
+
+            dashboard.entity.DashboardUserPreferenceCreateRequest objRequest
+                   = new dashboard.entity.DashboardUserPreferenceCreateRequest
+                   {
+                       Attributes = new List<dashboard.entity.DashboardUserPreferenceAttribute>(),
+                       OrganizationId = request.OrganizationId,
+                       ReportId = request.ReportId,
+                       AccountId = request.AccountId
+                   };
+
+            foreach (var attribute in request.Attributes)
+            {
+                objRequest.Attributes.Add(new dashboard.entity.DashboardUserPreferenceAttribute
+                {
+                    DataAttributeId = attribute.DataAttributeId,
+                    State = (dashboard.entity.ReportUserPreferenceState)(char)attribute.State,
+                    Type = (dashboard.entity.ReportPreferenceType)(char)attribute.Type,
+                    ChartType = attribute.ChartType > 0 ? (dashboard.entity.ReportPreferenceChartType)(char)attribute.ChartType : new dashboard.entity.ReportPreferenceChartType?(),
+                    ThresholdType = attribute.ThresholdType > 0 ? (dashboard.entity.ReportPreferenceThresholdType?)(char)attribute.ThresholdType : new dashboard.entity.ReportPreferenceThresholdType?(),
+                    ThresholdValue = attribute.ThresholdValue,
+                });
+            }
+            return objRequest;
+
+        }
+    }
+
+
+}
