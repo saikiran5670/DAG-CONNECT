ee1943f6437b8dd36d8c59a1ec199750b14ac575
Manoj Nandamuri <manoj.nandamuri@atos.net>
2021-08-04 13:32:11 +0000

    Dashboard: Today New Modification to point multiple table

--- dotnet/corecomponents/net.atos.daf.ct2.dashboard/net.atos.daf.ct2.dashboard/entity/TodayLiveVehicle.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.dashboard/net.atos.daf.ct2.dashboard/entity/TodayLiveVehicle.cs
@@ -6,14 +6,18 @@ namespace net.atos.daf.ct2.dashboard.entity
 {
     public class TodayLiveVehicleResponse
     {
+        public string TodayVin { get; set; }
         public double Distance { get; set; }
         public long DrivingTime { get; set; }
-        public int VehicleCount { get; set; }
         public int DriverCount { get; set; }
+        public int TodayActiveVinCount { get; set; }
+        public long TodayTimeBasedUtilizationRate { get; set; }
+        public int TodayDistanceBasedUtilization { get; set; }
         public int CriticleAlertCount { get; set; }
-        public int ActiveVehicles { get; set; }
-        public int TimeBaseUtilization { get; set; }
-        public int DistanceBaseUtilization { get; set; }
+        public string YesterdayVin { get; set; }
+        public int YesterdayActiveVinCount { get; set; }
+        public long YesterDayTimeBasedUtilizationRate { get; set; }
+        public int YesterDayDistanceBasedUtilization { get; set; }
     }
 
     public class TodayLiveVehicleRequest
