83fdbba6aef87659ced4197e83c39a734d78eedd
Kumari, Archana <archana.kumari@atos.net>
2021-08-05 13:14:58 +0000

    Vehicle chart sum logic

--- dotnet/corecomponents/net.atos.daf.ct2.reports/entity/VehPerformanceChartData.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.reports/entity/VehPerformanceChartData.cs
@@ -1,4 +1,6 @@
-﻿namespace net.atos.daf.ct2.reports.entity
+﻿using System.Collections.Generic;
+
+namespace net.atos.daf.ct2.reports.entity
 {
     public class VehPerformanceChartData
     {
@@ -9,7 +11,25 @@
         public string MatrixValue { get; set; }
         public string CountPerIndex { get; set; }
         public string ColumnIndex { get; set; }
-
+        public List<KPIs> ListKPIs { get; set; }
+        public long TripDuration { get; set; }
 
     }
+    public class KPIs
+    {
+        public string Label { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class KpiDataRange
+    {
+        public string PerformanceType { get; set; }
+        public int Index { get; set; }
+        public string Kpi { get; set; }
+        public int LowerVal { get; set; }
+        public int UpperVal { get; set; }
+        public int Value { get; set; }
+    }
+
+
 }
