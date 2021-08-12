058e9d26af6b10d83e950105974417522232a8e2
Tushar Gawande <tushar.gawande@atos.net>
2021-08-12 09:30:13 +0000

    Engine load collective

--- dotnet/corecomponents/net.atos.daf.ct2.reports/PerformanceChartMatrix.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.reports/PerformanceChartMatrix.cs
@@ -118,8 +118,6 @@ namespace net.atos.daf.ct2.reports
                 matdata.RowsCount = matdata.DMatrix.GetUpperBound(0) - matdata.DMatrix.GetLowerBound(0) + 1;
                 matrix.Add(matdata);
             }
-            int mAxColsCount = matrix.Max().ColumnsCount;
-            int maxRowsCount = matrix.Max().RowsCount;
             result.ChartData = new List<IndexWiseChartData>();
             foreach (var item in matrix)
             {
@@ -217,6 +215,175 @@ namespace net.atos.daf.ct2.reports
             return result;
         }
 
+        public VehiclePerformanceData GetcombinedmatrixRoadSpeed(List<VehPerformanceChartData> chartData, string performanceType, List<KpiDataRange> rangedata, double tripDuration)
+        {
+            //Console.WriteLine("Hello World!");
+            VehiclePerformanceData result = new VehiclePerformanceData();
+            List<ChartMatrix> matrix = new List<ChartMatrix>();
+            foreach (var item in chartData)
+            {
+                ChartMatrix matdata = new ChartMatrix();
+                matdata.A = item.MatrixValue.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+                matdata.IA = item.CountPerIndex.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+                matdata.JA = item.ColumnIndex.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+                matdata.A = performanceType == "B" ? matdata.A.Select(x => -x).ToArray() : matdata.A;
+
+                matdata.DMatrix = ParseSparseMatrix(matdata.A, matdata.IA);//, JA);
+                matdata.ColumnsCount = matdata.DMatrix.GetUpperBound(1) - matdata.DMatrix.GetLowerBound(1) + 1;
+                matdata.RowsCount = matdata.DMatrix.GetUpperBound(0) - matdata.DMatrix.GetLowerBound(0) + 1;
+                matrix.Add(matdata);
+            }
+            //int mAxColsCount = matrix.Max().ColumnsCount;
+            //int maxRowsCount = matrix.Max().RowsCount;
+            result.ChartData = new List<IndexWiseChartData>();
+            foreach (var item in matrix)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        IndexWiseChartData data = new IndexWiseChartData();
+                        try
+                        {
+                            int value = item.DMatrix[i, j];
+                            int y = 0;
+                            foreach (var range in rangedata)
+                            {
+                                switch (range.Index)
+                                {
+                                    case 0:// " ":
+                                        if (value >= range.LowerVal && value <= range.UpperVal)
+                                            range.Value += value;
+                                        break;
+                                    case 1: // "O":
+                                        if (value > range.LowerVal && value < range.UpperVal)
+                                        {
+                                            range.Value += value;
+                                            if (value > -0.25)
+                                            {
+                                                y = 0;
+                                            }
+                                            else
+                                            {
+                                                y = 1;
+                                            }
+                                        }
+
+                                        break;
+                                    case 2: // "A":
+                                        if (value >= range.LowerVal && value < range.UpperVal)
+                                        {
+                                            range.Value += value;
+                                            if (value >= -0.75)
+                                            {
+                                                y = 2;
+                                            }
+                                            else
+                                            {
+                                                y = 3;
+                                            }
+                                        }
+                                        break;
+                                    case 3:// "P":
+                                        if (value >= range.LowerVal && value < range.UpperVal)
+                                        {
+                                            range.Value += value;
+                                            if (value >= -1.25)
+                                            {
+                                                y = 4;
+                                            }
+                                            else
+                                            {
+                                                y = 5;
+                                            }
+                                        }
+                                        break;
+                                    case 4:// "E":
+                                        if (value >= range.LowerVal && value < range.UpperVal)
+                                        {
+                                            range.Value += value;
+                                            if (value >= -2)
+                                            {
+                                                y = 6;
+                                            }
+                                            else
+                                            {
+                                                y = 7;
+                                            }
+                                        }
+                                        break;
+                                    case 5:// "N":
+                                        if (value < range.UpperVal)
+                                        {
+                                            range.Value += value;
+                                            if (value >= -3)
+                                            {
+                                                y = 8;
+                                            }
+                                            else
+                                            {
+                                                y = 9;
+                                            }
+                                        }
+                                        break;
+                                    case 6:// "I":
+                                        if (value >= range.LowerVal && value < range.UpperVal)
+                                            range.Value += value;
+                                        break;
+                                    case 7:// "D":
+                                        if (value >= range.LowerVal && value <= range.UpperVal)
+                                            range.Value += value;
+                                        break;
+                                    case 8:// "U":
+                                        if (value > range.LowerVal && value < range.UpperVal)
+                                            range.Value += value;
+                                        break;
+                                    case 9:// " ":
+                                        if (value > range.LowerVal && value < range.UpperVal)
+                                            range.Value += value;
+                                        break;
+
+                                    default:
+                                        break;
+                                }
+
+                            }
+                            if (result.ChartData.Any(k => k.Xindex == i && k.Yindex == y))
+                            {
+                                result.ChartData.First(k => k.Xindex == i && k.Yindex == y).Value += value;
+                            }
+                            else
+                            {
+                                data.Xindex = i;
+                                //Skip records which dont fall in KPI
+                                data.Yindex = y;
+                                data.Value += value;
+                                result.ChartData.Add(data);
+                            }
+
+                        }
+                        catch (Exception)
+                        {
+                            // If index is not present in matrix it will throw exception we will put 0 in that case.
+                            if (result.ChartData.Any(k => k.Xindex == i && k.Yindex == j))
+                            {
+                                result.ChartData.First(k => k.Xindex == i && k.Yindex == j).Value += 0;
+                            }
+                            else
+                            {
+                                data.Xindex = i;
+                                data.Yindex = j;
+                                data.Value += 0;
+                                result.ChartData.Add(data);
+                            }
+                        }
+
+                    }
+                }
+            }
+            result.PieChartData = CalculateKPIData(result.ChartData, tripDuration, rangedata);
+            return result;
+        }
         public VehiclePerformanceData GetcombinedmatrixBrake(List<VehPerformanceChartData> chartData, string performanceType, List<KpiDataRange> rangedata, double tripDuration)
         {
             //Console.WriteLine("Hello World!");
