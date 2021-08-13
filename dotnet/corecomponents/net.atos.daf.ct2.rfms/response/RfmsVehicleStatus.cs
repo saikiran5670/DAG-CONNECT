a9df6d4df6f05fc4e18c72aec23cfe0ab29edd63
Prajwal Patil <prajwal.patil@atos.net>
2021-08-13 11:36:20 +0000

    rfms vehicle status request added.

--- dotnet/corecomponents/net.atos.daf.ct2.rfms/response/RfmsVehicleStatus.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.rfms/response/RfmsVehicleStatus.cs
@@ -1,46 +1,9 @@
-/*
+
 using System;
 using System.Collections.Generic;
 
 namespace net.atos.daf.ct2.rfms.response
 {
-        public class TachoDriverIdentification
-    {
-        public string DriverIdentification { get; set; }
-        public string CardIssuingMemberState { get; set; }
-        public string DriverAuthenticationEquipment { get; set; }
-        public string CardReplacementIndex { get; set; }
-        public string CardRenewalIndex { get; set; }
-    }
-
-    public class OemDriverIdentification
-    {
-        public string IdType { get; set; }
-        public string OemDriverIdentification { get; set; }
-    }
-
-    public class DriverId
-    {
-        public TachoDriverIdentification TachoDriverIdentification { get; set; }
-        public OemDriverIdentification OemDriverIdentification { get; set; }
-    }
-
-    public class TellTaleInfo
-    {
-        public string TellTale { get; set; }
-        public string OemTellTale { get; set; }
-        public string State { get; set; }
-    }
-
-    public class TriggerType
-    {
-        public string TriggerType { get; set; }
-        public string Context { get; set; }
-        public List<string> TriggerInfo { get; set; }
-        public DriverId DriverId { get; set; }
-        public string PtoId { get; set; }
-        public TellTaleInfo TellTaleInfo { get; set; }
-    }
 
     public class Driver1Id
     {
@@ -209,16 +172,6 @@ namespace net.atos.daf.ct2.rfms.response
         public int PramRequestCounter { get; set; }
     }
 
-    public class GnssPosition
-    {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public int Heading { get; set; }
-        public int Altitude { get; set; }
-        public double Speed { get; set; }
-        public DateTime PositionDateTime { get; set; }
-    }
-
     public class Driver2Id
     {
         public TachoDriverIdentification TachoDriverIdentification { get; set; }
@@ -289,7 +242,7 @@ namespace net.atos.daf.ct2.rfms.response
     }
 
     public class RfmsVehicleStatus
-    {      
+    {
         public VehicleStatusResponse VehicleStatusResponse { get; set; }
         public bool MoreDataAvailable { get; set; }
         public string MoreDataAvailableLink { get; set; }
@@ -297,5 +250,3 @@ namespace net.atos.daf.ct2.rfms.response
 
     }
 }
-*/
-
