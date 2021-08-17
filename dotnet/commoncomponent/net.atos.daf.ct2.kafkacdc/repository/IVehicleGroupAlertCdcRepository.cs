0c7a591578c2c54beae2da964e4299ae14c8c528
Amar Nath Roy <amar-nath.roy@atos.net>
2021-08-17 12:36:28 +0000

    Add Vehicle Group CDC

---
+++ dotnet/commoncomponent/net.atos.daf.ct2.kafkacdc/repository/IVehicleGroupAlertCdcRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using net.atos.daf.ct2.kafkacdc.entity;
+
+namespace net.atos.daf.ct2.kafkacdc.repository
+{
+    public interface IVehicleGroupAlertCdcRepository
+    {
+        Task<List<VehicleAlertRef>> GetVehicleGroupAlertRefByAlertIds(List<int> alertId);
+        Task<List<VehicleAlertRef>> GetVehiclesGroupFromAlertConfiguration(int vehicleGroupId);
+
+    }
+}
