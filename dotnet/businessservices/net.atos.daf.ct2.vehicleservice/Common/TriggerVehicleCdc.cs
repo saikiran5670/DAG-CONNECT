7d172a9a9b723c9643cf7deff6ed79d46092b794
Amar Nath Roy <amar-nath.roy@atos.net>
2021-08-17 14:17:21 +0000

    Addd Trigger VehicleCdc for vehicle service.

--- dotnet/businessservices/net.atos.daf.ct2.vehicleservice/Common/TriggerVehicleCdc.cs
+++ dotnet/businessservices/net.atos.daf.ct2.vehicleservice/Common/TriggerVehicleCdc.cs
@@ -8,11 +8,14 @@ namespace net.atos.daf.ct2.vehicleservice.Common
 {
     public class VehicleCdcHelper
     {
-        private readonly IVehicleGroupAlertCdcManager _vehicleGroupAlertCdsManager;
+        private readonly IVehicleGroupAlertCdcManager _vehicleGroupAlertCdcManager;
         public VehicleCdcHelper(IVehicleGroupAlertCdcManager vehicleGroupAlertCdcManager)
         {
-            _vehicleGroupAlertCdsManager = vehicleGroupAlertCdcManager;
+            _vehicleGroupAlertCdcManager = vehicleGroupAlertCdcManager;
         }
-
+        public async Task TriggerVehicleCdc(int vehicleGroupId, string vehicleState)
+        {
+            _ = await Task.Run(() => _vehicleGroupAlertCdcManager.GetVehicleGroupAlertConfiguration(vehicleGroupId, vehicleState));
+        }
     }
 }
