3d3dffd692092464e2bef1ae6a6d9bb2514f5127
Amar Nath Roy <amar-nath.roy@atos.net>
2021-08-17 13:55:51 +0000

    Addd Trigger Vehicle CDC for vehicle service.

---
+++ dotnet/businessservices/net.atos.daf.ct2.vehicleservice/Common/TriggerVehicleCdc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using net.atos.daf.ct2.kafkacdc;
+
+namespace net.atos.daf.ct2.vehicleservice.Common
+{
+    public class VehicleCdcHelper
+    {
+        private readonly IVehicleGroupAlertCdcManager _vehicleGroupAlertCdsManager;
+        public VehicleCdcHelper(IVehicleGroupAlertCdcManager vehicleGroupAlertCdcManager)
+        {
+            _vehicleGroupAlertCdsManager = vehicleGroupAlertCdcManager;
+        }
+
+    }
+}
