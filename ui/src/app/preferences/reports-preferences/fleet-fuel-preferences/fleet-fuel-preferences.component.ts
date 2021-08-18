3e86e16084dcfd90b32eac1a830e0fcf999c969c
G <hemanth-kumar.g@atos.net>
2021-08-18 12:10:28 +0530

    code change for 14880

--- ui/src/app/preferences/reports-preferences/fleet-fuel-preferences/fleet-fuel-preferences.component.ts
+++ ui/src/app/preferences/reports-preferences/fleet-fuel-preferences/fleet-fuel-preferences.component.ts
@@ -274,7 +274,7 @@ export class FleetFuelPreferencesComponent implements OnInit {
 
   if(key === "rp_ff_report_vehicle_vehicledetails_cruisecontroldistance5075"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h (%)';
     }
@@ -282,7 +282,7 @@ export class FleetFuelPreferencesComponent implements OnInit {
 
   if(key === "rp_ff_report_vehicle_vehicledetails_cruisecontroldistance75"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h (%)';
     }
@@ -290,7 +290,7 @@ export class FleetFuelPreferencesComponent implements OnInit {
   
   if(key === "rp_ff_report_vehicle_singlevehicledetails_cruisecontroldistance5075"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h (%)';
     }
@@ -299,7 +299,7 @@ export class FleetFuelPreferencesComponent implements OnInit {
 
   if(key === "rp_ff_report_vehicle_singlevehicledetails_cruisecontroldistance3050"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h(%)';
     }
@@ -307,7 +307,7 @@ export class FleetFuelPreferencesComponent implements OnInit {
 
   if(key === "rp_ff_report_vehicle_singlevehicledetails_cruisecontroldistance75"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h(%)';
     }
@@ -315,7 +315,7 @@ export class FleetFuelPreferencesComponent implements OnInit {
 
   if(key === "rp_ff_report_driver_vehicledetails_cruisecontroldistance3050"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h(%)';
     }
@@ -323,7 +323,7 @@ export class FleetFuelPreferencesComponent implements OnInit {
 
   if(key === "rp_ff_report_driver_vehicledetails_cruisecontroldistance75"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h(%)';
     }
@@ -331,7 +331,7 @@ export class FleetFuelPreferencesComponent implements OnInit {
 
   if(key === "rp_ff_report_driver_vehicledetails_cruisecontroldistance5075"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h(%)';
     }
@@ -339,7 +339,7 @@ export class FleetFuelPreferencesComponent implements OnInit {
 
   if(key === "rp_ff_report_driver_singlevehicledetails_cruisecontroldistance3050"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h(%)';
     }
@@ -347,14 +347,14 @@ export class FleetFuelPreferencesComponent implements OnInit {
 
   if(key === "rp_ff_report_driver_singlevehicledetails_cruisecontroldistance5075"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h(%)';
     }
   }
   if(key === "rp_ff_report_driver_singlevehicledetails_cruisecontroldistance75"){
     if(unitObj[0].value == 'Imperial'){
-      return '(mph)';
+      return '(mph)(%)';
     } else {
       return 'km/h(%)';
     }
