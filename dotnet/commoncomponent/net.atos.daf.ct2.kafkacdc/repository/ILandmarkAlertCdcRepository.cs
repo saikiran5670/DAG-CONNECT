e7999f6ac4fd9da1b0d3e6e0b0e4b73b51b388d7
Tushar Gawande <tushar.gawande@atos.net>
2021-08-20 13:53:53 +0000

    Alert CDC for poi and geofence

---
+++ dotnet/commoncomponent/net.atos.daf.ct2.kafkacdc/repository/ILandmarkAlertCdcRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using net.atos.daf.ct2.kafkacdc.entity;
+
+namespace net.atos.daf.ct2.kafkacdc.repository
+{
+    public interface ILandmarkAlertCdcRepository
+    {
+        Task<List<VehicleAlertRef>> GetVehiclesFromAlertConfiguration(List<int> alertId);
+        Task<List<VehicleAlertRef>> GetVehicleAlertRefByAlertIds(List<int> alertId);
+        Task<List<int>> GetAlertsbyLandmarkId(int landmarkid, string landmarktype);
+    }
+}
