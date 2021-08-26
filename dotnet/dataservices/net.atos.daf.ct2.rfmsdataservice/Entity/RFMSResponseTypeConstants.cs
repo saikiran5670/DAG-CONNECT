f34d2b504d03e009e1371801aa218f26ec23999b
Kumari, Archana <archana.kumari@atos.net>
2021-08-26 13:40:41 +0000

    rfms code optimization

--- dotnet/dataservices/net.atos.daf.ct2.rfmsdataservice/Entity/RFMSResponseTypeConstants.cs
+++ dotnet/dataservices/net.atos.daf.ct2.rfmsdataservice/Entity/RFMSResponseTypeConstants.cs
@@ -11,6 +11,7 @@ namespace net.atos.daf.ct2.rfmsdataservice.Entity
         public static string ACCPET_TYPE_VEHICLE_JSON = "application/vnd.fmsstandard.com.vehicles.v3.0+json; UTF-8";
         public static string ACCEPT_TYPE_VEHICLE_POSITION_JSON = "application/vnd.fmsstandard.com.vehiclepositions.v3.0+json; UTF-8";
         public static string ACCEPT_TYPE_VEHICLE_STATUS_JSON = "application/vnd.fmsstandard.com.vehiclestatuses.v3.0+json; UTF-8";
+        public const string GET_VIN_VISIBILITY_FAILURE_MSG = "No vehicle found for Account Id {0} and Organization Id {1}";
 
     }
 }
