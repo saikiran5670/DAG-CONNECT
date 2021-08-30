be51f45a69e5ca68c8554b2355bd1e0e22b607c1
Vishal Bangar <vishal.bangar@atos.net>
2021-08-30 14:12:30 +0530

    Timezone changes in reports

--- ui/src/app/report/fuel-benchmarking/fuel-benchmarking.component.ts
+++ ui/src/app/report/fuel-benchmarking/fuel-benchmarking.component.ts
@@ -681,8 +681,8 @@ export class FuelBenchmarkingComponent implements OnInit {
     // this.resetChartData(); // reset chart data
     // let _startTime = Util.convertDateToUtc(this.startDateValue); // this.startDateValue.getTime();
     // let _endTime = Util.convertDateToUtc(this.endDateValue); // this.endDateValue.getTime();
-    let _startTime = Util.getMillisecondsToUTCDate(this.startDateValue.getTime(), this.prefTimeZone); 
-    let _endTime = Util.getMillisecondsToUTCDate(this.endDateValue.getTime(), this.prefTimeZone); 
+    let _startTime = Util.getMillisecondsToUTCDate(this.startDateValue, this.prefTimeZone); 
+    let _endTime = Util.getMillisecondsToUTCDate(this.endDateValue, this.prefTimeZone); 
   
     let selectedVehicleGroup = this.fuelBenchmarkingForm.controls.vehicleGroup.value;
     if(selectedVehicleGroup!==0){
