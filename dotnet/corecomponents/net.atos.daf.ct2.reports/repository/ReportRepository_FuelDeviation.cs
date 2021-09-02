c6e44dc6069b94f43709bc20e816e6c3334eae7d
Tushar Gawande <tushar.gawande@atos.net>
2021-09-02 10:40:51 +0000

    #16654

--- dotnet/corecomponents/net.atos.daf.ct2.reports/repository/ReportRepository_FuelDeviation.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.reports/repository/ReportRepository_FuelDeviation.cs
@@ -32,8 +32,8 @@ namespace net.atos.daf.ct2.reports.repository
 	                    , trpst.end_time_stamp as EndTimeStamp
 	                    , trpst.etl_gps_distance as Distance
 	                    , trpst.idle_duration as IdleDuration
-	                    , ROUND(trpst.average_speed,2) as AverageSpeed
-	                    , ROUND(trpst.average_weight,2) as AverageWeight
+	                    , ROUND(trpst.average_speed,5) as AverageSpeed
+	                    , ROUND(trpst.average_weight,5) as AverageWeight
                         , startgeoaddr.id AS StartPositionId
                         , endgeoaddr.id AS EndPositionId
                         , coalesce(startgeoaddr.address,'') AS StartPosition
