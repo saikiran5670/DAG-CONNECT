99208559a5f8b89872fbed0f09b0d3fe59d6b550
Anshu <anshu.rijhwani@atos.net>
2021-09-08 15:29:02 +0530

    AverageSpeed Changes

--- java/corecomponents/net.atos.daf.ct2.alerts/src/main/java/net/atos/daf/ct2/service/realtime/ExcessiveAverageSpeedService.java
+++ java/corecomponents/net.atos.daf.ct2.alerts/src/main/java/net/atos/daf/ct2/service/realtime/ExcessiveAverageSpeedService.java
@@ -13,43 +13,56 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-public class ExcessiveAverageSpeedService extends ProcessWindowFunction<Index, Index, String, TimeWindow> implements Serializable {
-    private static final Logger logger = LoggerFactory.getLogger(ExcessiveAverageSpeedService.class);
-    private static final long serialVersionUID = 1L;
+public class ExcessiveAverageSpeedService extends ProcessWindowFunction<Index, Index, String, TimeWindow>
+		implements Serializable {
+	private static final Logger logger = LoggerFactory.getLogger(ExcessiveAverageSpeedService.class);
+	private static final long serialVersionUID = 1L;
 
-    @Override
-    public void process(String arg0, ProcessWindowFunction<Index, Index, String, TimeWindow>.Context arg1,
-                        Iterable<Index> indexMsg, Collector<Index> arg3) throws Exception {
-        try {
-            List<Index> indexList = StreamSupport.stream(indexMsg.spliterator(), false)
-                    .collect(Collectors.toList());
-            if (!indexList.isEmpty()) {
-                logger.info("list size :{}", indexList.size());
-                Index startIndex = indexList.get(0);
-                if (indexList.size() == 1) {
-                    if (null != indexList.get(0).getDocument().getVTachographSpeed()) {
-                        startIndex.setVDist(Long.valueOf(
-                                (((indexList.get(0).getDocument().getVTachographSpeed()) * 1000)
-                                        / 3600)));
-                    } else {
-                        startIndex.setVDist(0L);
-                    }
-                    startIndex.setVIdleDuration(indexList.get(0).getVIdleDuration());
-                } else {
+	@Override
+	public void process(String arg0, ProcessWindowFunction<Index, Index, String, TimeWindow>.Context arg1,
+			Iterable<Index> indexMsg, Collector<Index> arg3) throws Exception {
+		try {
+			List<Index> indexList = StreamSupport.stream(indexMsg.spliterator(), false).collect(Collectors.toList());
+			if (!indexList.isEmpty()) {
+				logger.info("list size :{}", indexList.size());
+				net.atos.daf.ct2.models.Index idx = new net.atos.daf.ct2.models.Index();
+				Index startIndex = indexList.get(0);
+				idx.setVin(startIndex.getVin());
+				idx.setVid(startIndex.getVid());
+				idx.setDocument(startIndex.getDocument());
+				idx.getIndexList().add(startIndex);
 
-                    Index endIndex = indexList.get(indexList.size() - 1);
-                    logger.info("startIndex: {}", startIndex);
-                    logger.info("endIndex: {}", endIndex);
-                    Long average = Utils.calculateAverage(startIndex, endIndex);
-                    startIndex.setVDist(average);
-                    Long idleDuration = Utils.calculateIdleDuration(indexMsg);
-                    startIndex.setVIdleDuration(idleDuration);
-                }
+				if (indexList.size() == 1) {
 
-                arg3.collect(startIndex);
-            }
-        } catch (Exception e) {
-            logger.error("Issue while preparing data for ExcessiveAvgSpeed :{} , error {}", indexMsg, e);
-        }
-    }
+					if (null != indexList.get(0).getDocument().getVTachographSpeed()) {
+
+						Double techoSpeed = Double.valueOf(indexList.get(0).getDocument().getVTachographSpeed());
+						Double averageSpeed = (techoSpeed * 1000 / 3600);
+
+						// rounded value till 2 digits
+						double roundOffAverageSpeed = (double) Math.round(averageSpeed * 100) / 100;
+
+						idx.setAverageSpeed(roundOffAverageSpeed);
+						logger.info("Final averageSpeed--" + idx.getAverageSpeed());
+					} else {
+						idx.setAverageSpeed(0.0);
+					}
+					idx.setIdleDuration(indexList.get(0).getVIdleDuration());
+				} else {
+
+					Index endIndex = indexList.get(indexList.size() - 1);
+					logger.info("startIndex: {}", startIndex);
+					logger.info("endIndex: {}", endIndex);
+					Double average = Utils.calculateAverage(startIndex, endIndex);
+					idx.setAverageSpeed(average);
+					Long idleDuration = Utils.calculateIdleDuration(indexMsg);
+					idx.setIdleDuration(idleDuration);
+				}
+
+				arg3.collect(idx);
+			}
+		} catch (Exception e) {
+			logger.error("Issue while preparing data for ExcessiveAvgSpeed :{} , error {}", indexMsg, e);
+		}
+	}
 }
