2d1986f4feba2ccbbbd2be00236f7cdf105ab780
Harneet Rekhi <harneet.rekhi@atos.net>
2021-09-29 13:32:45 +0000

    OTA Software Update Core Component

---
+++ dotnet/corecomponents/net.atos.daf.ct2.otasoftwareupdate/OTASoftwareUpdateManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using net.atos.daf.ct2.otasoftwareupdate.entity;
+using net.atos.daf.ct2.otasoftwareupdate.repository;
+
+namespace net.atos.daf.ct2.otasoftwareupdate
+{
+    public class OTASoftwareUpdateManager : IOTASoftwareUpdateManager
+    {
+        readonly IOTASoftwareUpdateRepository _otaSoftwareUpdateRepository;
+        public OTASoftwareUpdateManager(IOTASoftwareUpdateRepository otaSoftwareUpdateRepository)
+        {
+            _otaSoftwareUpdateRepository = otaSoftwareUpdateRepository;
+        }
+
+        #region Get GetVehicleSoftwareStatus List
+        public async Task<IEnumerable<VehicleSoftwareStatus>> GetVehicleSoftwareStatus()
+        {
+
+            return await _otaSoftwareUpdateRepository.GetVehicleSoftwareStatus();
+
+        }
+        #endregion
+    }
+}
---
+++ dotnet/corecomponents/net.atos.daf.ct2.otasoftwareupdate/entity/VehicleSoftwareStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace net.atos.daf.ct2.otasoftwareupdate.entity
+{
+    public class VehicleSoftwareStatus
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public string Enum { get; set; }
+        public string ParentEnum { get; set; }
+        public string Key { get; set; }
+        public int FeatureId { get; set; }
+    }
+}
