91de91b388e25562bd893183bc1400161f488dec
Kumari, Archana <archana.kumari@atos.net>
2021-09-30 14:45:10 +0000

    rfms status api fix

--- dotnet/corecomponents/net.atos.daf.ct2.rfms/RfmsVehicleStatusExtension.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.rfms/RfmsVehicleStatusExtension.cs
@@ -40,7 +40,7 @@ namespace net.atos.daf.ct2.rfms
             rfmsVehicleStatus.VehicleStatusResponse.VehicleStatuses = rfmsVehicleStatus.VehicleStatusResponse.VehicleStatuses.Take(thresholdValue).ToList();
             string lastVin = rfmsVehicleStatus.VehicleStatusResponse.VehicleStatuses.Last().Vin;
             string lastReceivedDateTime = rfmsVehicleStatus.VehicleStatusResponse.VehicleStatuses.Last().ReceivedDateTime;//.ToString("yyyy-MM-ddThh:mm:ss.fffZ");
-            rfmsVehicleStatus.MoreDataAvailableLink = "//vehiclestatuses?starttime=" + lastReceivedDateTime + "&lastVin=" + lastVin;
+            rfmsVehicleStatus.MoreDataAvailableLink = "//vehiclestatus?starttime=" + lastReceivedDateTime + "&lastVin=" + lastVin;
             rfmsVehicleStatus.MoreDataAvailable = true;
         }
         internal void SetStatusMoreDataAvailableLink(RfmsVehicleStatusRequest rfmsVehicleStatusRequest, RfmsVehicleStatus rfmsVehicleStatus)
