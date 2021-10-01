217f343dde752cd16df0bfcc9a652a118510d7c4
Amar Nath Roy <amar-nath.roy@atos.net>
2021-10-01 05:31:49 +0000

    OTA 17 changes for repository

--- dotnet/corecomponents/net.atos.daf.ct2.notificationengine/entity/TripAlertOtaConfigParam.cs
+++ dotnet/corecomponents/net.atos.daf.ct2.notificationengine/entity/TripAlertOtaConfigParam.cs
@@ -7,14 +7,19 @@ namespace net.atos.daf.ct2.notificationengine.entity
     public class TripAlertOtaConfigParam
     {
         public int Id { get; set; }
-        public int Trip_Alert_Id { get; set; }
+        public int TripAlertId { get; set; }
         public string Vin { get; set; }
         public string Compaign { get; set; }
         public string Baseline { get; set; }
-        public string Status_Code { get; set; }
+        public string StatusCode { get; set; }
+        //public enum StatusCode
+        //{
+        //    UPDATE_FAILURE = 8,
+        //    WAITING_FOR_MANAGER_APPROVAL = 10
+        //}
         public string Status { get; set; }
-        public string Comapign_Id { get; set; }
+        public string ComapignId { get; set; }
         public string Subject { get; set; }
-        public long Time_stamp { get; set; }
+        public long TimeStamp { get; set; }
     }
 }
