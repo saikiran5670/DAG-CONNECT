480282dd822403a03f193e70f854299e6babb123
Harneet Rekhi <harneet.rekhi@atos.net>
2021-10-01 06:39:45 +0000

    Http Client Common Grpc service

---
+++ dotnet/commoncomponent/net.atos.daf.ct2.httpclient/IOTA22HttpClientManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using net.atos.daf.ct2.httpclient.entity;
+using net.atos.daf.ct2.httpclient.ENUM;
+
+namespace net.atos.daf.ct2.httpclient
+{
+    public interface IOTA22HttpClientManager
+    {
+        Task<VehiclesStatusOverviewResponse> GetVehiclesStatusOverview(VehiclesStatusOverviewRequest request);
+    }
+}
