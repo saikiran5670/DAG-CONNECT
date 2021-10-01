96300afc8460485faec4830e8b6a84a4d032d431
Ravi Ellapalayam Murugasamy <ravi.ellapalayammurugasamy@atos.net>
2021-10-01 19:25:50 +0530

    code changes for #16699

--- ui/src/app/report/eco-score-report/eco-score-report-driver/eco-score-report-driver.component.ts
+++ ui/src/app/report/eco-score-report/eco-score-report-driver/eco-score-report-driver.component.ts
@@ -499,7 +499,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
   formatData(data, isBrushChart){
     let result = [];
     for (var i in data) {      
-      let val = (new Date(i)).getTime();
+      let val = Util.convertUtcToDateTZ((new Date(i)).getTime(), this.prefObj.prefTimeZone);
       this.calMinMaxValue(val);
       let temp = Number.parseFloat(data[i]);
       if(isBrushChart)
@@ -515,7 +515,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
     for (var i in data) {
       let arr = data[i].substr(0, data[i].lastIndexOf(":"));
       let temp = Number.parseFloat((arr.split(":").join(".")));
-      let val = (new Date(i)).getTime();
+      let val = Util.convertUtcToDateTZ((new Date(i)).getTime(), this.prefObj.prefTimeZone);
       this.calMinMaxValue(val);
       if(isBrushChart)
         result.push([val, 0]);
