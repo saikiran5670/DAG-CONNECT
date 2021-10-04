bf19f9170d8d3a99fec0cdb734007f7d0abff621
Harneet Rekhi <harneet.rekhi@atos.net>
2021-10-04 13:53:41 +0000

    HttpClient Factory implementation

---
+++ dotnet/commoncomponent/net.atos.daf.ct2.httpclient/entity/ota22/VehicleUpdateDetailsRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace net.atos.daf.ct2.httpclientfactory.entity.ota22
+{
+    public class VehicleUpdateDetailsRequest
+    {
+        [JsonProperty("vin")]
+        public string Vin { get; set; }
+        [JsonProperty("retention")]
+        public string Retention { get; set; }
+    }
+}
