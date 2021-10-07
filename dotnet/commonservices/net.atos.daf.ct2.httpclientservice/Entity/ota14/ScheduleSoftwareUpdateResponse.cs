4dbaabc7f4018db30068cc9ab30f17cf5b15bd34
Amar Nath Roy <amar-nath.roy@atos.net>
2021-10-07 07:13:34 +0000

    ota 14 changes

---
+++ dotnet/commonservices/net.atos.daf.ct2.httpclientservice/Entity/ota14/ScheduleSoftwareUpdateResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net.atos.daf.ct2.httpclientservice.Entity.ota14
+{
+    public class ScheduleSoftwareUpdateResponse
+    {
+    }
+}
