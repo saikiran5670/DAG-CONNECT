c1333bfaf6f88d194f89b8d14c782a830a0b6a80
Kumari, Archana <archana.kumari@atos.net>
2021-10-11 04:33:05 +0000

    fixed fms auth issue

--- dotnet/dataservices/net.atos.daf.ct2.fmsdataservice/CustomAttributes/InterfaceAccessPolicies.cs
+++ dotnet/dataservices/net.atos.daf.ct2.fmsdataservice/CustomAttributes/InterfaceAccessPolicies.cs
@@ -5,6 +5,7 @@
         public const string FMS_VEHICLE_VEHICLES_ACCESS_POLICY = "api.fms3.vehicles";
         public const string FMS_VEHICLE_POSITION_ACCESS_POLICY = "api.fms3.positions";
         public const string FMS_VEHICLE_STATUS_ACCESS_POLICY = "api.fms3.status";
+        public const string MAIN_ACCESS_POLICY = "api.fms3-data";
     }
 
 }
