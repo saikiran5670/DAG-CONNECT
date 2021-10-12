8cb3747aa4d690f53e0d70e02650c300e62d3315
Ravi Ellapalayam Murugasamy <ravi.ellapalayammurugasamy@atos.net>
2021-10-12 16:42:00 +0530

    code changes for #19282

--- ui/src/app/report/driver-time-management/driver-time-management.component.ts
+++ ui/src/app/report/driver-time-management/driver-time-management.component.ts
@@ -931,6 +931,9 @@ export class DriverTimeManagementComponent implements OnInit, OnDestroy {
           if(this.driverListData.length>1){
           this.driverListData.unshift({ driverID: 0, firstName: this.translationData.lblAll || 'All' });
           }
+          if(this.vehicleListData.length>0){
+            this.vehicleListData.unshift({ vehicleId: 0, vehicleName: this.translationData.lblAll || 'All' });
+          }
           let vehicleData = this.vehicleListData.slice();
           this.vehicleDD = this.getUniqueVINs([...this.singleVehicle, ...vehicleData]);
           this.driverDD = this.driverListData;
