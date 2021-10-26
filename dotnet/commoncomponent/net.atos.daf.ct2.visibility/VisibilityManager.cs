91124900bfeb30dc7a701a8242a866c20ea74953
Hemanshu Waghulde <hemanshu.waghulde@atos.net>
2021-10-26 10:52:20 +0000

    Visibility changes for Alerts

--- dotnet/commoncomponent/net.atos.daf.ct2.visibility/VisibilityManager.cs
+++ dotnet/commoncomponent/net.atos.daf.ct2.visibility/VisibilityManager.cs
@@ -76,7 +76,7 @@ namespace net.atos.daf.ct2.visibility
 
             if (reportFeatureId > 0 && vehicles.Count() > 0)
             {
-                var vehiclePackages = await GetSubscribedVehicleByFeature(reportFeatureId, contextOrgId);
+                var vehiclePackages = await _visibilityRepository.GetSubscribedVehicleByFeature(reportFeatureId, contextOrgId);
 
                 //Filter owned vehicles based on package features
                 //If not found Org packages then filter vehicles based on subscribed vehicles
@@ -148,9 +148,120 @@ namespace net.atos.daf.ct2.visibility
             return resultDict;
         }
 
-        public async Task<IEnumerable<VehiclePackage>> GetSubscribedVehicleByFeature(int featureid, int organizationid)
+        /// <summary>
+        /// Filtering visible and owned vehicle as per feature id 
+        /// </summary>
+        /// <param name="resultDict">Visibile vehicles received from account visibility</param>
+        /// <param name="reportFeatureId">Report feature Id recieved from report click</param>
+        /// <param name="contextOrgId">organizatin id</param>
+        /// <returns></returns>
+        private async Task<Dictionary<VehicleGroupDetails, List<VisibilityVehicle>>> FilterVehiclesByfeaturesForAlert(Dictionary<VehicleGroupDetails, List<VisibilityVehicle>> resultDict, int[] featureIds, int contextOrgId)
         {
-            return await _visibilityRepository.GetSubscribedVehicleByFeature(featureid, organizationid);
+            var vehicles = resultDict.Values.SelectMany(x => x).Distinct(new ObjectComparer()).ToList();
+
+            if (vehicles.Count() > 0)
+            {
+                var vehiclePackages = await _visibilityRepository.GetSubscribedVehicleByFeatureForAlert(featureIds, contextOrgId);
+
+                //Filter owned vehicles based on package features
+                //If not found Org packages then filter vehicles based on subscribed vehicles
+                var ownedVehicles = vehicles.Where(e => e.HasOwned == true);
+                if (ownedVehicles.Count() > 0)
+                {
+                    if (vehiclePackages.Any(e => e.PackageType == "O"))
+                    {
+                        foreach (var vehicle in ownedVehicles)
+                        {
+                            vehicle.FeatureIds = new List<long>();
+                            vehicle.FeatureIds.AddRange(vehiclePackages.Where(e => e.PackageType == "O").FirstOrDefault()?.FeatureIds ?? new long[] { });
+                            vehicle.SubscriptionType = "O";
+                        }
+                    }
+
+                    if (vehiclePackages.Any(e => (e.PackageType == "V" && e.HasOwned == true) || e.PackageType == "N")) // check if any v type and owned subscription available
+                    {
+                        //Step 1 - Take subscribed vehicle id org+vin
+                        var subscriptionVehicleIds = vehiclePackages.Where(e => e.HasOwned == true && e.PackageType == "V").Select(e => e.Vehicle_Id);
+                        var ownedVehiclesFromV = ownedVehicles.Where(x => subscriptionVehicleIds.Contains(x.Id));//v1, v2, v3
+
+                        //Step 2 - Take vin type vehicle id vin
+                        var vinPackageVehicleIds = vehiclePackages.Where(e => e.PackageType == "N").Select(e => e.Vehicle_Id);
+                        var ownedVehiclesFromN = ownedVehicles.Where(x => vinPackageVehicleIds.Contains(x.Id));//v2, v4
+
+                        //Step 3 - Union and assigned to owned vehicle
+                        var ownedVehiclesFromNV = ownedVehiclesFromV.Union(ownedVehiclesFromN, new ObjectComparer());
+
+                        ownedVehicles = ownedVehicles.Union(ownedVehiclesFromNV, new ObjectComparer());
+
+                        //Step 4 - Assign subscribed features to owned vehicles
+                        foreach (var vehicle in ownedVehicles)
+                        {
+                            vehicle.FeatureIds = vehicle.FeatureIds ?? new List<long>();
+                            vehicle.FeatureIds = vehicle.FeatureIds.Union(vehiclePackages.Where(e => e.Vehicle_Id == vehicle.Id).SelectMany(x => x.FeatureIds)).ToList();
+                            vehicle.SubscriptionType = ownedVehiclesFromNV.Any(e => e.Id == vehicle.Id) ? "V" : vehicle.SubscriptionType;
+                        }
+
+                        ownedVehicles = ownedVehicles.Where(x => x.FeatureIds != null && x.FeatureIds.Count() > 0);
+                    }
+                }
+
+                //Filter visible vehicles based on org relationship features and package features
+                var visibleVehicles = vehicles.Where(e => e.HasOwned == false).ToList();
+
+                if (visibleVehicles.Count() > 0)
+                {
+                    //Fetch visible relationship vehicles of having reportFeatureId in it's allowed features list
+                    //Intersect those vehicles with Org+VIN package subscribed vehicles where reportFeatureId is present in the subscription
+                    //Filter vehicles out those are not in relationship vehicles and subscribed vehicles.
+                    if (vehiclePackages.Any(e => (e.HasOwned == false && e.PackageType == "V") || e.PackageType == "N"))
+                    {
+                        //Step 1 - Take subscribed vehicle ids org+vin
+                        var subscriptionVehicleIds = vehiclePackages.Where(e => e.HasOwned == false && e.PackageType == "V").Select(e => e.Vehicle_Id);
+
+                        //Step 2 - Take relationship vehicle ids
+                        var relationshipVehicles = await _visibilityRepository.GetRelationshipVehiclesByFeatureForAlert(featureIds, contextOrgId);
+                        var relationshipVehicleIds = relationshipVehicles.Select(x => x.Vehicle_Id);
+
+                        //Fetch vehicles records to be removed from visible vehicles list
+                        var filteredVisibleVehicleIds = relationshipVehicleIds.Intersect(subscriptionVehicleIds);
+
+                        //Step 3 - Take VIN type vehicle ids
+                        var vinPackageVehicleIds = vehiclePackages.Where(e => e.PackageType == "N").Select(e => e.Vehicle_Id).ToList();
+
+                        //Fetch vehicles records to be removed from visible vehicles list from Vin package
+                        var filteredVinPackageVisibleVehicleIds = relationshipVehicleIds.Intersect(vinPackageVehicleIds);
+
+                        //Step 4 - Union and assigned to visible vehicles
+                        var visibleVehiclesFromVR = visibleVehicles.Where(x => filteredVisibleVehicleIds.Contains(x.Id));//v1, v2, v3
+                        var visibleVehiclesFromN = visibleVehicles.Where(x => filteredVinPackageVisibleVehicleIds.Contains(x.Id));//v2, v4
+                        visibleVehicles = visibleVehiclesFromVR.Union(visibleVehiclesFromN, new ObjectComparer()).ToList();
+
+                        //Step 5 - Assign subscribed features to visible vehicles
+                        foreach (var vehicle in visibleVehicles)
+                        {
+                            vehicle.FeatureIds = new List<long>();
+                            vehicle.FeatureIds = vehiclePackages.Where(e => e.Vehicle_Id == vehicle.Id).SelectMany(x => x.FeatureIds)
+                                                    .Intersect(relationshipVehicles.Where(e => e.Vehicle_Id == vehicle.Id).FirstOrDefault()?.FeatureIds ?? new long[] { }).ToList();
+                            vehicle.SubscriptionType = "V";
+                        }
+                    }
+                    else
+                    {
+                        visibleVehicles.Clear();
+                    }
+                }
+
+                //Concatenate both lists
+                vehicles = ownedVehicles.Concat(visibleVehicles.AsEnumerable()).ToList();
+
+                foreach (var vg_kv in resultDict.ToList())
+                {
+                    var resultVehicles = vg_kv.Value;
+                    resultDict[vg_kv.Key] = vehicles.Where(x => resultVehicles.Any(y => y.Id == x.Id)).ToList();
+                }
+            }
+
+            return resultDict;
         }
 
         public async Task<IEnumerable<VehicleDetailsAccountVisibility>> GetVehicleByAccountVisibilityTemp(int accountId, int orgId, int contextOrgId, int reportFeatureId)
@@ -170,6 +281,25 @@ namespace net.atos.daf.ct2.visibility
             resultDict = await FilterVehiclesByfeatures(resultDict, reportFeatureId, contextOrgId);
 
             return MapVehicleDetailsTemp(accountId, contextOrgId, resultDict);
+        }
+
+        public async Task<IEnumerable<VehicleDetailsAccountVisibilityForAlert>> GetVehicleByAccountVisibilityForAlert(int accountId, int orgId, int contextOrgId, int[] featureIds)
+        {
+            Dictionary<VehicleGroupDetails, List<VisibilityVehicle>> resultDict;
+            //If context switched then find vehicle visibility for the organization
+            if (orgId != contextOrgId)
+            {
+                resultDict = await _vehicleManager.GetVisibilityVehiclesByOrganization(contextOrgId);
+            }
+            else
+            {
+                resultDict = await _vehicleManager.GetVisibilityVehicles(accountId, orgId);
+            }
+
+            // vehicle filtering based on features
+            resultDict = await FilterVehiclesByfeaturesForAlert(resultDict, featureIds, contextOrgId);
+
+            return MapVehicleDetailsForAlert(accountId, contextOrgId, resultDict);
         }
 
         public Task<IEnumerable<VehicleDetailsFeatureAndSubsction>> GetVehicleByFeatureAndSubscription(int accountId, int orgId, int contextOrgId, int roleId,
@@ -355,6 +485,59 @@ namespace net.atos.daf.ct2.visibility
             return result;
         }
 
+        private static IEnumerable<VehicleDetailsAccountVisibilityForAlert> MapVehicleDetailsForAlert(int accountId, int contextOrgId, Dictionary<VehicleGroupDetails, List<VisibilityVehicle>> resultDict)
+        {
+            List<VehicleDetailsAccountVisibilityForAlert> vehicleDetails = new List<VehicleDetailsAccountVisibilityForAlert>();
+            foreach (var vg_kv in resultDict)
+            {
+                var vehicleGroup = vg_kv.Key;
+                var visibleVehicles = vg_kv.Value;
+                //if (!vehicleGroup.GroupType.Equals("S"))
+                {
+                    foreach (var vehicle in visibleVehicles)
+                    {
+                        vehicleDetails.Add(new VehicleDetailsAccountVisibilityForAlert
+                        {
+                            AccountId = accountId,
+                            VehicleId = vehicle.Id,
+                            Vin = vehicle.VIN,
+                            VehicleName = vehicle.Name ?? string.Empty,
+                            RegistrationNo = vehicle.RegistrationNo ?? string.Empty,
+                            ObjectType = "V",
+                            VehicleGroupId = vehicleGroup.Id,
+                            VehicleGroupName = vehicleGroup.Name,
+                            GroupType = vehicleGroup.GroupType,
+                            FunctionEnum = vehicleGroup.GroupMethod ?? string.Empty,
+                            OrganizationId = contextOrgId,
+                            SubscriptionType = vehicle.SubscriptionType,
+                            FeatureIds = vehicle.FeatureIds
+                        });
+                    }
+                }
+            }
+
+            var result = vehicleDetails.GroupBy(x => new
+            {
+                x.VehicleId,
+                x.VehicleName,
+                x.Vin,
+                x.RegistrationNo,
+                x.OrganizationId
+            }).Select(x => new VehicleDetailsAccountVisibilityForAlert
+            {
+                VehicleId = x.Key.VehicleId,
+                Vin = x.Key.Vin,
+                VehicleName = x.Key.VehicleName,
+                RegistrationNo = x.Key.RegistrationNo,
+                OrganizationId = x.Key.OrganizationId,
+                VehicleGroupIds = x.Select(x => x.VehicleGroupId).ToArray(),
+                VehicleGroupDetails = x.Select(x => $"{ x.VehicleGroupId }~{ x.VehicleGroupName }~{ x.GroupType }")
+                .Aggregate((s1, s2) => s1 + "," + s2),
+                SubscriptionType = x.Select(x => x.SubscriptionType).Any(x => x.Equals("V")) ? "V" : "O",
+                FeatureIds = x.SelectMany(x => x.FeatureIds).Distinct().ToList()
+            });
+            return result;
+        }
 
         private async Task<IEnumerable<VehicleDetailsAccountVisibilityForOTA>> MapVehicleDetailsForOTA(Dictionary<VehicleGroupDetails, List<VisibilityVehicle>> resultDict, int adminFeatureId, int orgId, int contextOrgId)
         {
@@ -439,7 +622,6 @@ namespace net.atos.daf.ct2.visibility
                 return idHashCode ^ vinHashCode;
             }
         }
-
 
         public async Task<IEnumerable<VehicleDetailsVisibiltyAndFeatureTemp>> GetSubscribedVehicleByAlertFeature(List<int> featureid, int organizationid)
         {
