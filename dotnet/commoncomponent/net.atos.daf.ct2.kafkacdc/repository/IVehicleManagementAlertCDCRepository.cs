3940dfb48167fe9ef1d5526370af21884cb12a9a
Harneet Rekhi <harneet.rekhi@atos.net>
2021-11-19 13:18:59 +0000

    Alert CDC Vehicle Update

--- dotnet/commoncomponent/net.atos.daf.ct2.kafkacdc/repository/IVehicleManagementAlertCDCRepository.cs
+++ dotnet/commoncomponent/net.atos.daf.ct2.kafkacdc/repository/IVehicleManagementAlertCDCRepository.cs
@@ -8,7 +8,9 @@ namespace net.atos.daf.ct2.kafkacdc.repository
 {
     public interface IVehicleManagementAlertCDCRepository
     {
-        Task<List<VehicleAlertRef>> GetVehicleAlertRefFromvehicleId(IEnumerable<int> vehicleIds);
+        Task<List<VehicleAlertRef>> GetVehicleAlertRefFromvehicleId(IEnumerable<int> alertIds, IEnumerable<int> vehicleIds);
         Task<List<VehicleAlertRef>> GetVehicleAlertByvehicleId(IEnumerable<int> vehicleIds, int organizationId);
+        Task<List<VehicleGroupAlertRef>> GetAlertByVehicleAndFeatures(List<int> vehicleGroupIds, List<int> featureIds);
+        Task<bool> DeleteAndInsertVehicleAlertRef(List<int> alertIds, IEnumerable<int> vehicleIds, List<VehicleAlertRef> vehicleAlertRefs);
     }
 }
