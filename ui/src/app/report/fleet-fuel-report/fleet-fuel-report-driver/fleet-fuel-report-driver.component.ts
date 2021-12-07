1c6fa6b376da21f287c6c4f4bf5307056a85e903
Tauheed <tauheed@ascra.com>
2021-12-07 13:21:06 +0530

    fixed for 20704

--- ui/src/app/report/fleet-fuel-report/fleet-fuel-report-driver/fleet-fuel-report-driver.component.ts
+++ ui/src/app/report/fleet-fuel-report/fleet-fuel-report-driver/fleet-fuel-report-driver.component.ts
@@ -2053,7 +2053,7 @@ setVehicleGroupAndVehiclePreSelection() {
 
     const header =  ['Driver Name','Driver ID','Vehicle Name', 'VIN', 'Vehicle Registration No', 'Distance('+unitValkm+')', 'Average Distance Per Day('+unitValkm+')', 'Average Speed('+unitValkmh+')',
     'Max Speed('+unitValkmh+')', 'Number Of Trips', 'Average Gross Weight Comb('+unitValkg2+')','FuelConsumed('+unitValuekm+')', 'FuelConsumption('+unitVal100km+')',  'CO2 Emission('+unitValkg2+')',
-    'Idle Duration','PTO Duration%','HarshBrakeDuration%','Heavy Throttle Duration%','Cruise Control Distance '+ccdOne+'('+unitValkmh+')%',
+    'Idle Duration(%)','PTO Duration(%)','HarshBrakeDuration(%)','Heavy Throttle Duration(%)','Cruise Control Distance '+ccdOne+'('+unitValkmh+')%',
     'Cruise Control Distance '+ccdTwo+'('+unitValkmh+')%','Cruise Control Distance'+ccdThree+'('+unitValkmh+')%', 'Average Traffic Classification',
     'CC Fuel Consumption('+unitVal100km+')','fuel Consumption CC Non Active('+unitVal100km+')','Idling Consumption','Dpa Score','DPA Anticipation Score%','DPA Breaking Score%', 'Idling PTO Score (hh:mm:ss)','Idling With PTO%','Idling Without PTO (hh:mm:ss)','Idling Without PTO%','Foot Brake',
     'CO2 Emmision(gr/km)', 'Idling Consumption With PTO('+unitVal100km+')'];
