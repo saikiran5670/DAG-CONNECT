7d039fcdfe29a2389180d7bd6220fdb18701a946
Harneet Rekhi <harneet.rekhi@atos.net>
2021-12-20 11:11:16 +0000

    20725: Error handling fix FeatureManagementService

---
+++ dotnet/businessservices/net.atos.daf.ct2.featureservice/entity/FeatureConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net.atos.daf.ct2.featureservice.entity
+{
+    public static class FeatureConstants
+    {
+        public const string INTERNAL_SERVER_MSG = "Error occured, Please check with DAF IT team for more info.";
+    }
+}
