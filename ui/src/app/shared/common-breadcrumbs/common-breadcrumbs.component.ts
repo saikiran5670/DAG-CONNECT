e2bbbcf622f182af4774e15aac2aedcf103b3444
Tarak <narisetty.tarakram@atos.net>
2021-12-28 12:33:23 +0530

    css breadcrumbs

--- ui/src/app/shared/common-breadcrumbs/common-breadcrumbs.component.ts
+++ ui/src/app/shared/common-breadcrumbs/common-breadcrumbs.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class CommonBreadcrumbsComponent implements OnInit {
   @Input() breadcrumb;
+  @Input() topRightElements;
   constructor() { }
 
   ngOnInit(): void {
