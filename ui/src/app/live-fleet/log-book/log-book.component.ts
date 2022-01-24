c8b14b55f16d63e7fd18b988fa3c48ce57498a33
Tauheed <tauheed@ascra.com>
2022-01-24 18:56:11 +0530

    logbook fix

--- ui/src/app/live-fleet/log-book/log-book.component.ts
+++ ui/src/app/live-fleet/log-book/log-book.component.ts
@@ -1542,13 +1542,13 @@ let prepare = []
 
   getLastMonthDate(){
     var date = Util.getUTCDate(this.prefTimeZone);
-    date.setMonth(date.getMonth()-1);
+    date.setDate(date.getDate()-30);
     return date;
   }
 
   getLast3MonthDate(){
     var date = Util.getUTCDate(this.prefTimeZone);
-    date.setMonth(date.getMonth()-3);
+    date.setDate(date.getDate()-90);
     date.setHours(0);
     date.setMinutes(0);
     date.setSeconds(0);
