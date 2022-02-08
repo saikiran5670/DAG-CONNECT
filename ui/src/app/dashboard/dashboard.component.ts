4da68dda8bc8ad0d73a2fafed4f59eac276ac891
Siddhartha Gupta <siddhartha.gupta.external@atos.net>
2022-02-08 16:42:48 +0530

    Dashboard code refactoring.

--- ui/src/app/dashboard/dashboard.component.ts
+++ ui/src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,5 @@
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpHeaders,
-} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
-import { MultiDataSet, Label, Color } from 'ng2-charts';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 import { ReportService } from '../services/report.service';
 import { TranslationService } from '../services/translation.service';
 import { OrganizationService } from '../services/organization.service';
@@ -30,105 +22,6 @@ export class DashboardComponent implements OnInit {
   preference : any;
   noDataFound: boolean = false;
   dashboardPrefData: any;
-  //--------- Pie chart ------------------//
-  // doughnutChartData: MultiDataSet = [[55, 25, 20]];
-  // doughnutChartLabels: Label[] = ['BMW', 'Ford', 'Tesla'];
-  // doughnutChartType: ChartType = 'doughnut';
-  // barChartColors: Color[] = [
-  //   {
-  //     backgroundColor: ['#eb8171', '#f7d982', '#54a9d8'],
-  //   },
-  // ];
-  // public doughnut_barOptions: ChartOptions = {
-  //   responsive: true,
-  //   legend: {
-  //     position: 'bottom',
-  //     labels: {
-  //       //fontSize: 10,
-  //       usePointStyle: true,
-  //     },
-  //   },
-  //   cutoutPercentage: 50,
-  // };
-  // public barChartLegend = true;
-
-  // //--------------- bar graph ----------//
-  // public stack_barChartData: ChartDataSets[] = [
-  //   {
-  //     data: [20, 10, 20, 10, 20],
-  //     label: 'BMW',
-  //     stack: 'a',
-  //     backgroundColor: '#eb8171',
-  //     hoverBackgroundColor: '#eb8171',
-  //     barThickness: 30,
-  //   },
-  //   {
-  //     data: [10, 10, 10, 10, 10],
-  //     label: 'Ford',
-  //     stack: 'a',
-  //     backgroundColor: '#f7d982',
-  //     hoverBackgroundColor: '#f7d982',
-  //     barThickness: 30,
-  //   },
-  //   {
-  //     data: [10, 20, 10, 20, 10],
-  //     label: 'Tesla',
-  //     stack: 'a',
-  //     backgroundColor: '#54a9d8',
-  //     hoverBackgroundColor: '#54a9d8',
-  //     barThickness: 30,
-  //   },
-  // ];
-  // public stack_barChartLabels: Label[] = [
-  //   '2016',
-  //   '2017',
-  //   '2018',
-  //   '2019',
-  //   '2020',
-  // ];
-  // public stack_barChartOptions: ChartOptions = {
-  //   responsive: true,
-  //   legend: {
-  //     position: 'bottom',
-  //   },
-  //   scales: {
-  //     xAxes: [
-  //       {
-  //         stacked: true,
-  //         time: {
-  //           unit: 'month',
-  //         },
-  //         // gridLines: {
-  //         //   display: false,
-  //         // }
-  //       },
-  //     ],
-  //     yAxes: [
-  //       {
-  //         stacked: true,
-  //         ticks: {
-  //           beginAtZero: true,
-  //           //    max: 0
-  //         },
-  //       },
-  //     ],
-  //   },
-  // };
-  // public stack_barChartPlugins = [];
-  // public stack_barChartLegend = true;
-  // public stack_barChartType: ChartType = 'bar';
-  //----- bar graph 2 -------------------------//
-  // public stack_barChartData_overdue: ChartDataSets[] = [
-  //   {
-  //     data: [15],
-  //     label: 'BMW',
-  //     stack: 'a',
-  //     backgroundColor: '#eb8171',
-  //     hoverBackgroundColor: '#eb8171',
-  //     barThickness: 30,
-  //   },
-  // ];
-  // public stack_barChartLabels_overdue: Label[] = ['2020'];
   translationData: any = {};
 
   constructor(public httpClient: HttpClient,private translationService: TranslationService,private reportService : ReportService, private organizationService: OrganizationService,
@@ -157,7 +50,6 @@ export class DashboardComponent implements OnInit {
     }
    
     this.globalSearchFilterData = JSON.parse(localStorage.getItem("globalSearchFilterData"));
-   // this.localStLanguage = JSON.parse(localStorage.getItem("language"));
     this.accountId = localStorage.getItem('accountId') ? parseInt(localStorage.getItem('accountId')) : 0;
     this.accountPrefObj = JSON.parse(localStorage.getItem('accountInfo'));
     this.showLoadingIndicator = true;
@@ -176,7 +68,6 @@ export class DashboardComponent implements OnInit {
         }
       });
     });
-   
   }
 
   loadReportData() {
@@ -204,11 +95,9 @@ export class DashboardComponent implements OnInit {
       this.hideloader();
       reportListData = [];
     });
-
   }
   
   sendMessage(): void {
-    // send message to subscribers via observable subject
     this.messageService.sendMessage('refreshTimer');
   }
 
@@ -242,8 +131,6 @@ export class DashboardComponent implements OnInit {
   }
 
   hideloader() {
-    // Setting display of spinner
     this.showLoadingIndicator = false;
   }
-
 }
