609e17d20010d8776a384b7f20ddb6ad98dbeca8
Vishal Bangar <vishal.bangar@atos.net>
2022-02-14 20:57:16 +0530

    Code refactoring - Eco-score report

--- ui/src/app/report/eco-score-report/eco-score-report-driver/eco-score-report-driver.component.ts
+++ ui/src/app/report/eco-score-report/eco-score-report-driver/eco-score-report-driver.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
-import { ChartComponent, ApexAxisChartSeries, ApexChart, ApexFill, ApexTooltip, ApexXAxis, ApexLegend, ApexDataLabels,
-        ApexTitleSubtitle, ApexYAxis } from "ng-apexcharts";
+import {
+  ChartComponent, ApexAxisChartSeries, ApexChart, ApexFill, ApexTooltip, ApexXAxis, ApexLegend, ApexDataLabels,
+  ApexTitleSubtitle, ApexYAxis
+} from "ng-apexcharts";
 import { AngularGridInstance, Column, FieldType, GridOption, Formatter, } from 'angular-slickgrid';
 import { ChartOptions, ChartType } from 'chart.js';
 import { Color, Label, MultiDataSet, PluginServiceGlobalRegistrationAndOptions, SingleDataSet } from 'ng2-charts';
 import * as Chart from 'chart.js';
-import * as ApexCharts from 'apexcharts';
 import { Util } from 'src/app/shared/util';
 import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
@@ -34,7 +35,6 @@ export type ChartOptionsApex = {
 })
 export class EcoScoreReportDriverComponent implements OnInit {
   @Input() ecoScoreDriverInfo: any;
-  // @Input() ecoScoreForm: any;
   @Input() ecoScoreDriverDetails: any;
   @Input() generalGraphColumnData: any;
   generalGraphPrefObj: any = {
@@ -77,7 +77,6 @@ export class EcoScoreReportDriverComponent implements OnInit {
   showLoadingIndicator: boolean = false;
   translationDataLocal: any = [];
   translationDataTrendLineLocal: any = [];
-  //performance table
   angularGrid!: AngularGridInstance;
   dataViewObj: any;
   gridObj: any;
@@ -86,7 +85,6 @@ export class EcoScoreReportDriverComponent implements OnInit {
   datasetHierarchical: any[] = [];
   driverCount: number = 0;
   columnPerformance: any = [];
-  //General table
   angularGridGen!: AngularGridInstance;
   dataViewObjGen: any;
   gridObjGen: any;
@@ -94,7 +92,6 @@ export class EcoScoreReportDriverComponent implements OnInit {
   columnDefinitionsGen!: Column[];
   datasetGen: any[] = [];
   columnGeneral: any = [];
-  //common details
   driverDetails: any = [];
   driverDetailsGen: any = [];
   driver1: string = '';
@@ -146,13 +143,13 @@ export class EcoScoreReportDriverComponent implements OnInit {
     right: { style: 'thin' },
   };
   titleStyle: any = { name: 'sans-serif', family: 4, size: 11, bold: true };
-  constructor() {}
 
-  ngOnInit(): void {
+  constructor() { }
+
+  ngOnInit() {
     this.checkGraphprefences();
     this.loadBarCharOptions();
     this.loadBarChartPerformanceOptions();
-    this.translationUpdate();
     this.getSeriesData();
     this.fromDisplayDate = this.ecoScoreDriverInfo.startDate;
     this.toDisplayDate = this.ecoScoreDriverInfo.endDate;
@@ -397,7 +394,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
           show: false,
         },
         events: {
-          beforeMount: function (chartContext, options) {},
+          beforeMount: function (chartContext, options) { },
           animationEnd: function (chartContext, options) {
             // this.hideSeries()
             this.hideloader();
@@ -487,15 +484,10 @@ export class EcoScoreReportDriverComponent implements OnInit {
   getSeriesData() {
     let dataSeries = [];
     this.ecoScoreDriverDetailsTrendLine.trendlines.forEach((vehicle, index) => {
-      // if(index == 0){
       for (var key in vehicle.kpiInfo) {
         if (vehicle.kpiInfo.hasOwnProperty(key)) {
           let _key = vehicle.kpiInfo[key].key;
-          // if(this.prefUnitFormat.indexOf("Imperial") !== -1 && (_key.indexOf("30") !== -1 || _key.indexOf("50") !== -1 || _key.indexOf("75") !== -1))
-          //   _key += '_I';
           let name = this.translationData[_key];
-          // let _name = this.translationData.filter(obj=>obj.key === _key); || this.translationDataLocal.filter(obj=>obj.key === _key);
-          // let name = _name[0].value;
           if (
             _key.indexOf('rp_CruiseControlUsage') !== -1 ||
             _key.indexOf('rp_cruisecontroldistance') !== -1
@@ -550,19 +542,10 @@ export class EcoScoreReportDriverComponent implements OnInit {
               show: true,
             },
             axisBorder: {
-              show: true,
-              // color: "#008FFB"
+              show: true
             },
-            // labels: {
-            //   style: {
-            //     // color: "#008FFB"
-            //   }
-            // },
             title: {
-              text: seriesName,
-              // style: {
-              //   color: "#008FFB"
-              // }
+              text: seriesName
             },
             tooltip: {
               enabled: false,
@@ -571,7 +554,6 @@ export class EcoScoreReportDriverComponent implements OnInit {
           this.kpiName.push(seriesName);
         }
       }
-      // }
     });
     this.kpiList = [...new Set(this.kpiName)];
     this.seriesDataFull = dataSeries;
@@ -624,6 +606,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
       else if (val > this.maxValue) this.maxValue = val;
     }
   }
+
   checkGraphprefences() {
     this.generalGraphColumnData.forEach((element) => {
       if (element.name == 'EcoScore.GeneralGraph.PieChart') {
@@ -781,7 +764,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
       this.ecoScoreDriverDetails.singleDriverKPIInfo &&
       this.ecoScoreDriverDetails.singleDriverKPIInfo.subSingleDriver &&
       this.ecoScoreDriverDetails.singleDriverKPIInfo.subSingleDriver.length >
-        0 &&
+      0 &&
       this.driverPerformanceColumnData &&
       this.driverPerformanceColumnData.length > 0
     ) {
@@ -829,7 +812,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
                       // no child -> (others)
                       if (
                         _elem.name ==
-                          'EcoScore.DriverPerformance.FuelConsumption' &&
+                        'EcoScore.DriverPerformance.FuelConsumption' &&
                         this.driverPerformanceColumnData[1]
                       ) {
                         let _p =
@@ -844,7 +827,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
                         }
                       } else if (
                         _elem.name ==
-                          'EcoScore.DriverPerformance.BrakingScore' &&
+                        'EcoScore.DriverPerformance.BrakingScore' &&
                         this.driverPerformanceColumnData[2]
                       ) {
                         let _p =
@@ -907,42 +890,6 @@ export class EcoScoreReportDriverComponent implements OnInit {
     this.loadData();
   }
 
-  //General Table
-  translationUpdate() {
-    // this.translationDataLocal = [
-    //   { key:'rp_general' , value:'General' },
-    //   { key:'rp_averagegrossweight' , value:'Average Gross Weight' },
-    //   { key:'rp_distance' , value:'Distance' },
-    //   { key:'rp_numberoftrips' , value:'Number of Trips' },
-    //   { key:'rp_numberofvehicles' , value:'Number of vehicles' },
-    //   { key:'rp_averagedistanceperday' , value:'Average distance per day' },
-    //   { key:'rp_driverperformance' , value:'Driver Performance' },
-    //   { key:'rp_ecoscore' , value:'Eco Score' },
-    //   { key:'rp_fuelconsumption' , value:'Fuel Consumption' },
-    //   { key:'rp_braking' , value:'Braking' },
-    //   { key:'rp_anticipationscore' , value:'Anticipation Score' },
-    //   { key:'rp_averagedrivingspeed' , value:'Average Driving Speed' },
-    //   { key:'rp_idleduration' , value:'Idle Duration' },
-    //   { key:'rp_idling' , value:'Idling' },
-    //   { key:'rp_heavythrottleduration' , value:'Heavy Throttle Duration' },
-    //   { key:'rp_heavythrottling' , value:'Heavy Throttling' },
-    //   { key:'rp_averagespeed' , value:'Average Speed' },
-    //   { key:'rp_ptoduration' , value:'PTO Duration' },
-    //   { key:'rp_ptousage' , value:'PTO Usage' },
-    //   { key:'rp_CruiseControlUsage30' , value:'Cruise Control Usage' },
-    //   { key:'rp_CruiseControlUsage75' , value:'Cruise Control Usage' },
-    //   { key:'rp_CruiseControlUsage50' , value:'Cruise Control Usage' },
-    //   { key:'rp_cruisecontrolusage' , value:'Cruise Control Usage' },
-    //   { key:'rp_cruisecontroldistance50' , value:'Cruise Control Usage' },
-    //   { key:'rp_cruisecontroldistance30' , value:'Cruise Control Usage' },
-    //   { key:'rp_cruisecontroldistance75' , value:'Cruise Control Usage' },
-    //   { key:'rp_harshbraking' , value:'Harsh Braking' },
-    //   { key:'rp_harshbrakeduration' , value:'Harsh Brake Duration' },
-    //   { key:'rp_brakeduration' , value:'Brake Duration' },
-    //   { key:'rp_brakingscore' , value:'Braking Score' }
-    //  ];
-  }
-
   tableColumns() {
     this.columnDefinitions = [
       {
@@ -982,7 +929,6 @@ export class EcoScoreReportDriverComponent implements OnInit {
     if (this.driverDetails !== undefined && this.driverDetails !== null) {
       for (var i = 0; i < this.driverDetails.length; i++) {
         this.columnPerformance.push({ columnId: 'driver_' + i });
-        // this.columnGeneral.push({columnId: 'driverG_'+i});
       }
       this.driverDetails.sort((col1, col2) => {
         let vin1 = col1.vin.toLowerCase();
@@ -1157,9 +1103,8 @@ export class EcoScoreReportDriverComponent implements OnInit {
       .replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
       .replace(/>/g, '&gt;');
-    const spacer = `<span style="display:inline-block; width:${
-      15 * dataContext[treeLevelPropName]
-    }px;"></span>`;
+    const spacer = `<span style="display:inline-block; width:${15 * dataContext[treeLevelPropName]
+      }px;"></span>`;
 
     if (
       data[idx + 1] &&
@@ -1182,7 +1127,6 @@ export class EcoScoreReportDriverComponent implements OnInit {
   getScore: Formatter = (row, cell, value, columnDef, dataContext, grid) => {
     if (value !== undefined && value !== null && value.length > 0) {
       value.sort((col1: any, col2: any) => {
-        // if(col1.columnGroup && col2.columnGroup){
         let vin1 = col1.vin.toLowerCase();
         let vin2 = col2.vin.toLowerCase();
         if (vin1 < vin2) return -1;
@@ -1268,6 +1212,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
     }
     return val;
   }
+
   getColor(dataContext: any, val: string) {
     if (dataContext.limitType && val) {
       let valTemp = Number.parseFloat(val);
@@ -1502,7 +1447,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
           data: element.data,
           label:
             element.label &&
-            element.label.toLowerCase().indexOf('overall driver') !== -1
+              element.label.toLowerCase().indexOf('overall driver') !== -1
               ? this.translationData.lblOverallDriver
               : element.label,
         });
@@ -1610,7 +1555,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
           data: element.data,
           label:
             element.label &&
-            element.label.toLowerCase().indexOf('overall driver') !== -1
+              element.label.toLowerCase().indexOf('overall driver') !== -1
               ? this.translationData.lblOverallDriver
               : element.label,
         });
@@ -1727,13 +1672,11 @@ export class EcoScoreReportDriverComponent implements OnInit {
   }
 
   exportAsExcelFile() {
-    //Create workbook and worksheet
     let workbook = new Workbook();
     let worksheet = workbook.addWorksheet(
       this.translationData.lblEcoScoreReport
     );
     const title = this.translationData.lblEcoScoreReport;
-    //Add Row and formatting
     let titleRow = worksheet.addRow([title]);
     worksheet.addRow([]);
     titleRow.font = {
@@ -1931,15 +1874,6 @@ export class EcoScoreReportDriverComponent implements OnInit {
     let generalBar = document.getElementById('generalChart');
     let performanceBar = document.getElementById('performanceChart');
     let summaryArea = document.getElementById('summaryCard');
-
-    // let chartKPI = document.getElementById('apexchartschart2');
-    // let innerClass = document.getElementsByClassName('apexcharts-inner');
-    // let yAxis = document.getElementsByClassName('apexcharts-yaxis');
-    // let svg = chartKPI.getElementsByTagName('svg')[0];
-    // svg.removeChild(innerClass[0]);
-    // svg.removeChild(yAxis[1]);
-    // svg.removeChild(yAxis[0]);
-
     let src;
     let ohref;
     let oheight;
@@ -1968,16 +1902,8 @@ export class EcoScoreReportDriverComponent implements OnInit {
       src = canvas.toDataURL();
       performanceBarHref = canvas.toDataURL('image/png');
     });
-    // html2canvas(chartKPI).then(canvas => {
-    //   performanceBarHeight= canvas.height * oWidth/canvas.width;
-    //   //oWidth= canvas.width;
-    //   src = canvas.toDataURL();
-    //   chartKPIHref = canvas.toDataURL('image/png');
-    // });
 
     let trendLineChart = document.getElementById('trendLineChart');
-    // let chartKPIs = document.getElementsByClassName('apexcharts-legend')[0];
-    // trendLineChart.getElementsByTagName('foreignObject')[0].removeChild(chartKPIs);
     var fileTitle = this.translationData.lblPdfTitle;
     html2canvas(trendLineChart, { scale: 2 }).then((canvas) => {
       (doc as any).autoTable({
@@ -1986,10 +1912,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
           fontSize: 12,
         },
         didDrawPage: function (data) {
-          // Header
           doc.setFontSize(14);
-          // var fileTitle = this.translationData.lblPdfTitle;
-          // var fileTitle = 'pdf';
           var img = '/assets/logo.png';
           doc.addImage(img, 'JPEG', 10, 10, 0, 0);
 
@@ -2009,7 +1932,6 @@ export class EcoScoreReportDriverComponent implements OnInit {
 
       const FILEURI = canvas.toDataURL('image/png');
       doc.addImage(FILEURI, 'PNG', 10, 40, fileWidth, fileHeight);
-      // doc.addImage(chartKPIHref, 'PNG', 150, 40, fileWidth, fileHeight);
       doc.addPage('a2', 'p');
 
       let perfVinList = ['', '', 'Overall', 'Overall'];
@@ -2090,7 +2012,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
         theme: 'striped',
         startX: 15,
         startY: 15,
-        didDrawCell: (data) => {},
+        didDrawCell: (data) => { },
       });
       doc.addPage('a4', 'p');
 
@@ -2103,7 +2025,7 @@ export class EcoScoreReportDriverComponent implements OnInit {
         theme: 'striped',
         startX: 15,
         startY: 15,
-        didDrawCell: (data) => {},
+        didDrawCell: (data) => { },
       });
       doc.addPage('a4', 'p');
       doc.addImage(
