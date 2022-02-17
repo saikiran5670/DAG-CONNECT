e3d34c15b69effaca203c50c1684664cd88a589e
Siddhartha Gupta <siddhartha.gupta.external@atos.net>
2022-02-18 00:03:29 +0530

    Feature management & Driver management code refactoring done.

--- ui/src/app/admin/feature-management/create-edit-view-features/create-edit-view-features.component.spec.ts
+++ ui/src/app/admin/feature-management/create-edit-view-features/create-edit-view-features.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CreateEditViewFeaturesComponent } from './create-edit-view-features.component';
 
 describe('CreateEditViewFeaturesComponent', () => {
