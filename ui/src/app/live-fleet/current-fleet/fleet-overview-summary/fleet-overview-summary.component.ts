ddb544adb9e88208bdb3397c5b486474bb0b05bd
Tauheed <tauheed@ascra.com>
2022-03-03 14:15:19 +0530

    24078

--- ui/src/app/live-fleet/current-fleet/fleet-overview-summary/fleet-overview-summary.component.ts
+++ ui/src/app/live-fleet/current-fleet/fleet-overview-summary/fleet-overview-summary.component.ts
@@ -138,7 +138,7 @@ export class FleetOverviewSummaryComponent implements OnInit {
       this.prefTimeFormat = Number(prefData.timeformat[0].name.split("_")[1].substring(0,2));    
     }
     this.unitValkm = (this.prefUnitFormat == 'dunit_Metric') ? (this.translationData.lblkm) : (this.prefUnitFormat == 'dunit_Imperial') ? (this.translationData.lblmile || 'mile') : (this.translationData.lblmile);
-    this.stepForword(this.detailsData, true);
+    // this.stepForword(this.detailsData, true);
     this.setFleetThreshold();
   }
 
@@ -379,11 +379,11 @@ export class FleetOverviewSummaryComponent implements OnInit {
         // if (element.tripDistance) {
         //   tripDistance += element.tripDistance;
         // }
-        if (flag) {
+        // if (flag) {
           // if (element.drivingTime)
           //   this.totalDriveTime += element.drivingTime;
 
-          if (element.vehicleDrivingStatusType && element.vehicleDrivingStatusType != 'N') {
+          if (flag && (element.vehicleDrivingStatusType && element.vehicleDrivingStatusType != 'N')) {
             this.movedVehicle += 1;
           }
 
@@ -403,7 +403,7 @@ export class FleetOverviewSummaryComponent implements OnInit {
           //   }
           // }
 
-        }
+        // }
         // if(element.drivingTime)
         //   totalDriveTime += element.drivingTime;
         // if(element.tripDistance){
