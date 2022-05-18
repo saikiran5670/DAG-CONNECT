1c69a2f8996521cf1c786b07276c8fc156b82c13
Gaonkar <sushil.gaonkar@atos.net>
2022-05-18 12:51:07 +0530

    #22337 & #30224 Fix

--- ui/src/app/report/fleet-fuel-report/fleet-fuel-report-vehicle/fleet-fuel-report-vehicle.component.ts
+++ ui/src/app/report/fleet-fuel-report/fleet-fuel-report-vehicle/fleet-fuel-report-vehicle.component.ts
@@ -1627,7 +1627,7 @@ export class FleetFuelReportVehicleComponent implements OnInit, OnDestroy {
         this.selectedEndTime = "23:59";
       } else {
         this.startTimeDisplay = '12:00:00 AM';
-        this.endTimeDisplay = '11:59:00 PM';
+        this.endTimeDisplay = '11:59:59 PM';
         this.selectedStartTime = "12:00 AM";
         this.selectedEndTime = "11:59 PM";
       }
